9ce5deb7aff84e17b61ee0959fb14998d085744b
Lance Willett <lance@fautrever.com>
2012-01-20 18:18:59 +0000

    _s fixes
    Spacing, trailing slash; no need to translate the footer generator URI.

--- image.php
+++ image.php
@@ -34,8 +34,8 @@ get_header(); ?>
 						</div><!-- .entry-meta -->
 
 						<nav id="image-navigation">
-							<span class="previous-image"><?php previous_image_link( false, __( '&larr; Previous' , '_s' ) ); ?></span>
-							<span class="next-image"><?php next_image_link( false, __( 'Next &rarr;' , '_s' ) ); ?></span>
+							<span class="previous-image"><?php previous_image_link( false, __( '&larr; Previous', '_s' ) ); ?></span>
+							<span class="next-image"><?php next_image_link( false, __( 'Next &rarr;', '_s' ) ); ?></span>
 						</nav><!-- #image-navigation -->
 					</header><!-- .entry-header -->
 
