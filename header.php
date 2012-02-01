88b7c401e636ddc49d38aaae141edb8fa09d30f6
Ian Stewart <ian@iandanielstewart.com>
2012-02-01 16:49:05 +0000

    _s: using a simpler and more consistent site-header class

--- header.php
+++ header.php
@@ -47,7 +47,7 @@
 <body <?php body_class(); ?>>
 <div id="page" class="hfeed">
 	<?php do_action( 'before' ); ?>
-	<header id="masthead" role="banner">
+	<header id="masthead" class="site-header" role="banner">
 		<hgroup>
 			<h1 class="site-title"><a href="<?php echo home_url( '/' ); ?>" title="<?php echo esc_attr( get_bloginfo( 'name', 'display' ) ); ?>" rel="home"><?php bloginfo( 'name' ); ?></a></h1>
 			<h2 class="site-description"><?php bloginfo( 'description' ); ?></h2>
@@ -59,6 +59,6 @@
 
 			<?php wp_nav_menu( array( 'theme_location' => 'primary' ) ); ?>
 		</nav>
-	</header><!-- #masthead -->
+	</header><!-- #masthead .site-header -->
 
 	<div id="main">
