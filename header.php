e1f35edffbcc61a2630313e660498dff8b0afd19
Ian Stewart <ian@iandanielstewart.com>
2012-02-03 20:26:25 +0000

    _s: We won't need comment replies if comments are closed
    git-svn-id: https://wpcom-themes.svn.automattic.com/_s/@8854 d957f892-c61d-0410-b221-f235e6eecf30

--- header.php
+++ header.php
@@ -35,7 +35,7 @@
 	?></title>
 <link rel="profile" href="http://gmpg.org/xfn/11" />
 <link rel="stylesheet" type="text/css" media="all" href="<?php bloginfo( 'stylesheet_url' ); ?>" />
-<?php if ( is_singular() && get_option( 'thread_comments' ) ) wp_enqueue_script( 'comment-reply' ); ?>
+<?php if ( is_singular() && comments_open() && get_option( 'thread_comments' ) ) wp_enqueue_script( 'comment-reply' ); ?>
 <link rel="pingback" href="<?php bloginfo( 'pingback_url' ); ?>" />
 <!--[if lt IE 9]>
 <script src="<?php echo get_template_directory_uri(); ?>/js/html5.js" type="text/javascript"></script>
