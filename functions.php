be34e27d0411da7035906b71c0499f7d492bb18b
Michael Fields <michael@mfields.org>
2012-03-06 14:03:39 +0000

    _s: Script dependancies need to be given as an array. Remove unneeded jQuery enqueue.
    git-svn-id: https://wpcom-themes.svn.automattic.com/_s/@9086 d957f892-c61d-0410-b221-f235e6eecf30

--- functions.php
+++ functions.php
@@ -104,9 +104,7 @@ function _s_scripts() {
 
 	wp_enqueue_style( 'style', get_stylesheet_uri() );
 
-	wp_enqueue_script( 'jquery' );
-
-	wp_enqueue_script( 'small-menu', get_template_directory_uri() . '/js/small-menu.js', 'jquery', '20120206', true );
+	wp_enqueue_script( 'small-menu', get_template_directory_uri() . '/js/small-menu.js', array( 'jquery' ), '20120206', true );
 
 	if ( is_singular() && comments_open() && get_option( 'thread_comments' ) ) {
 		wp_enqueue_script( 'comment-reply' );
