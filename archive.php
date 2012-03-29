43ea44cb447dbbc653f0d592671fc2ff92dfca6c
Ian Stewart <ian@iandanielstewart.com>
2012-03-29 15:55:33 +0000

    _s: Simplifying search, index, and archive, with a common no posts found template part, no-results.php; Closes #1187
    git-svn-id: https://wpcom-themes.svn.automattic.com/_s/@9340 d957f892-c61d-0410-b221-f235e6eecf30

--- archive.php
+++ archive.php
@@ -88,16 +88,7 @@ get_header(); ?>
 
 			<?php else : ?>
 
-				<article id="post-0" class="post no-results not-found">
-					<header class="entry-header">
-						<h1 class="entry-title"><?php _e( 'Nothing Found', '_s' ); ?></h1>
-					</header><!-- .entry-header -->
-
-					<div class="entry-content">
-						<p><?php _e( 'It seems we can&rsquo;t find what you&rsquo;re looking for. Perhaps searching can help.', '_s' ); ?></p>
-						<?php get_search_form(); ?>
-					</div><!-- .entry-content -->
-				</article><!-- #post-0 -->
+				<?php get_template_part( 'no-results', 'archive' ); ?>
 
 			<?php endif; ?>
 
--- search.php
+++ search.php
@@ -30,16 +30,7 @@ get_header(); ?>
 
 			<?php else : ?>
 
-				<article id="post-0" class="post no-results not-found">
-					<header class="entry-header">
-						<h1 class="entry-title"><?php _e( 'Nothing Found', '_s' ); ?></h1>
-					</header><!-- .entry-header -->
-
-					<div class="entry-content">
-						<p><?php _e( 'Sorry, but nothing matched your search terms. Please try again with some different keywords.', '_s' ); ?></p>
-						<?php get_search_form(); ?>
-					</div><!-- .entry-content -->
-				</article><!-- #post-0 -->
+				<?php get_template_part( 'no-results', 'search' ); ?>
 
 			<?php endif; ?>
 
