2997c2c59e18d209ded1fcc4fa60b7791b2976ed
Philip Arthur Moore <work@philiparthurmoore.com>
2012-05-15 01:08:33 +0000

    _s: In comments.php there's a line that reads "if comments are closed and no comments are...". Let's get rid of the "no" since comments must be present in order for the .nocomments text to be shown.
    git-svn-id: https://wpcom-themes.svn.automattic.com/_s/@9560 d957f892-c61d-0410-b221-f235e6eecf30

--- comments.php
+++ comments.php
@@ -65,7 +65,7 @@
 	<?php endif; // have_comments() ?>
 
 	<?php
-		// If comments are closed and there are no comments, let's leave a little note, shall we?
+		// If comments are closed and there are comments, let's leave a little note, shall we?
 		if ( ! comments_open() && '0' != get_comments_number() && post_type_supports( get_post_type(), 'comments' ) ) :
 	?>
 		<p class="nocomments"><?php _e( 'Comments are closed.', '_s' ); ?></p>
