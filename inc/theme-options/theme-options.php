c4a02323c61708abc7519e7e39ae03b4eec5b3b8
Michael Fields <michael@mfields.org>
2012-05-26 23:39:53 +0000

    _s: Remove notice in Theme Options screen in WordPress 3.4. props @corvannoorloos  * Replace deprecated get_current_theme() with wp_get_theme()  * Providing back-compat with prior versions of WordPress.
    git-svn-id: https://wpcom-themes.svn.automattic.com/_s/@9647 d957f892-c61d-0410-b221-f235e6eecf30

--- inc/theme-options/theme-options.php
+++ inc/theme-options/theme-options.php
@@ -253,7 +253,8 @@ function _s_theme_options_render_page() {
 	?>
 	<div class="wrap">
 		<?php screen_icon(); ?>
-		<h2><?php printf( __( '%s Theme Options', '_s' ), get_current_theme() ); ?></h2>
+		<?php $theme_name = function_exists( 'wp_get_theme' ) ? wp_get_theme() : get_current_theme(); ?>
+		<h2><?php printf( __( '%s Theme Options', '_s' ), $theme_name ); ?></h2>
 		<?php settings_errors(); ?>
 
 		<form method="post" action="options.php">
