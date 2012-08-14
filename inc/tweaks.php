f2fea05e7c76f6a77b20094716f95a3b04295510
Konstantin Kovshenin <kovshenin@gmail.com>
2012-08-14 10:15:46 +0400

    Use a filter for wp_title instead of hard-coding things in header.php
    Instead of hard-coding things like blog name, description and page number in the header.php template, use a simple call to wp_title() and make the rest in tweaks.php using the wp_title filter. This allows others to easily override the <title> tag without having to parse the output from buffer. SEO plugins will love this.

--- inc/tweaks.php
+++ inc/tweaks.php
@@ -50,3 +50,27 @@ function _s_enhanced_image_navigation( $url, $id ) {
 	return $url;
 }
 add_filter( 'attachment_link', '_s_enhanced_image_navigation', 10, 2 );
+
+/**
+ * Filters wp_title to print a neat <title> tag based on what is being viewed.
+ *
+ * @since _s 1.1
+ */
+function _s_wp_title( $title, $sep ) {
+	global $page, $paged;
+
+	// Add the blog name
+	$title .= get_bloginfo( 'name' );
+
+	// Add the blog description for the home/front page.
+	$site_description = get_bloginfo( 'description', 'display' );
+	if ( $site_description && ( is_home() || is_front_page() ) )
+		$title .= " $sep $site_description";
+
+	// Add a page number if necessary:
+	if ( $paged >= 2 || $page >= 2 )
+		$title .= " $sep " . sprintf( __( 'Page %s', '_s' ), max( $paged, $page ) );
+
+	return $title;
+}
+add_filter( 'wp_title', '_s_wp_title', 10, 2 );
