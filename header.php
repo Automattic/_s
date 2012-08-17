e70fac0e9c20278aa005044a8c8999388973eb50
Piet Bos <info@senlinonline.com>
2012-08-17 11:31:42 +0800

    Update header.php
    add initial scale property to viewport meta tag

--- header.php
+++ header.php
@@ -11,7 +11,7 @@
 <html <?php language_attributes(); ?>>
 <head>
 <meta charset="<?php bloginfo( 'charset' ); ?>" />
-<meta name="viewport" content="width=device-width" />
+<meta name="viewport" content="width=device-width, initial-scale=1.0" />
 <title><?php
 	/*
 	 * Print the <title> tag based on what is being viewed.
