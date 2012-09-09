258e8acaa9da8a49576a956f0de19537f29474a1
Ashfame <mail@ashfame.com>
2012-09-10 01:55:54 +0530

    changing H1 tags usage to make html output semantically correct, which inturn improves SEO

--- content-page.php
+++ content-page.php
@@ -9,7 +9,11 @@
 
 <article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
 	<header class="entry-header">
-		<h1 class="entry-title"><?php the_title(); ?></h1>
+		<?php if ( is_front_page() ) { ?>
+			<div class="entry-title"><?php the_title(); ?></div>
+		<?php } else { ?>
+			<h1 class="entry-title"><?php the_title(); ?></h1>
+		<?php } ?>
 	</header><!-- .entry-header -->
 
 	<div class="entry-content">
--- functions.php
+++ functions.php
@@ -90,8 +90,8 @@ function _s_widgets_init() {
 		'id' => 'sidebar-1',
 		'before_widget' => '<aside id="%1$s" class="widget %2$s">',
 		'after_widget' => '</aside>',
-		'before_title' => '<h1 class="widget-title">',
-		'after_title' => '</h1>',
+		'before_title' => '<div class="widget-title">',
+		'after_title' => '</div>',
 	) );
 }
 add_action( 'widgets_init', '_s_widgets_init' );
