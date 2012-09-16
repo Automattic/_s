fe09edc0ac7a57d8e5bcd03d7df7e3dfff02d1cf
Ashfame <mail@ashfame.com>
2012-09-16 20:45:27 +0530

    minor changes to increase code's readability

--- image.php
+++ image.php
@@ -49,20 +49,25 @@ get_header();
 									 * Grab the IDs of all the image attachments in a gallery so we can get the URL of the next adjacent image in a gallery,
 									 * or the first image (if we're looking at the last image in a gallery), or, in a gallery of one, just the link to that image file
 									 */
-									$attachments = array_values( get_children( array( 'post_parent' => $post->post_parent, 'post_status' => 'inherit', 'post_type' => 'attachment', 'post_mime_type' => 'image', 'order' => 'ASC', 'orderby' => 'menu_order ID' ) ) );
+									$attachments = array_values( get_children( array(
+										'post_parent' => $post->post_parent,
+										'post_status' => 'inherit',
+										'post_type' => 'attachment',
+										'post_mime_type' => 'image',
+										'order' => 'ASC',
+										'orderby' => 'menu_order ID'
+									)));
+
+									// Find out the index of current attachment among all the attachments of the post to which it belongs
 									foreach ( $attachments as $k => $attachment ) {
 										if ( $attachment->ID == $post->ID )
 											break;
 									}
-									$k++;
+
 									// If there is more than 1 attachment in a gallery
 									if ( count( $attachments ) > 1 ) {
-										if ( isset( $attachments[ $k ] ) )
-											// get the URL of the next image attachment
-											$next_attachment_url = get_attachment_link( $attachments[ $k ]->ID );
-										else
-											// or get the URL of the first image attachment
-											$next_attachment_url = get_attachment_link( $attachments[ 0 ]->ID );
+										// get the URL of the next image attachment if it exists, else of the first image attachment
+										$next_attachment_url = isset( $attachments[ $k + 1 ] ) ? get_attachment_link( $attachments[ $k + 1 ]->ID ) : get_attachment_link( $attachments[ 0 ]->ID );
 									} else {
 										// or, if there's only 1 image, get the URL of the image
 										$next_attachment_url = wp_get_attachment_url();
