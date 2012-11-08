ab3c5fdf3fbf6a58ad31b0a7a512b99703ecebeb
Chris Schmitz <ccschmitz@gmail.com>
2012-11-08 01:43:45 -0600

    Make use of divs and sections consistent
    In a few places #primary is a <div>, and in others it's a <section>.
    This commit moves them all over to <section>'s, which I think is more
    appropriate.

--- 404.php
+++ 404.php
@@ -8,40 +8,40 @@
 
 get_header(); ?>
 
-	<div id="primary" class="content-area">
-		<div id="content" class="site-content" role="main">
+		<section id="primary" class="content-area">
+			<div id="content" class="site-content" role="main">
 
-			<article id="post-0" class="post error404 not-found">
-				<header class="entry-header">
-					<h1 class="entry-title"><?php _e( 'Oops! That page can&rsquo;t be found.', '_s' ); ?></h1>
-				</header><!-- .entry-header -->
+				<article id="post-0" class="post error404 not-found">
+					<header class="entry-header">
+						<h1 class="entry-title"><?php _e( 'Oops! That page can&rsquo;t be found.', '_s' ); ?></h1>
+					</header><!-- .entry-header -->
 
-				<div class="entry-content">
-					<p><?php _e( 'It looks like nothing was found at this location. Maybe try one of the links below or a search?', '_s' ); ?></p>
+					<div class="entry-content">
+						<p><?php _e( 'It looks like nothing was found at this location. Maybe try one of the links below or a search?', '_s' ); ?></p>
 
-					<?php get_search_form(); ?>
+						<?php get_search_form(); ?>
 
-					<?php the_widget( 'WP_Widget_Recent_Posts' ); ?>
+						<?php the_widget( 'WP_Widget_Recent_Posts' ); ?>
 
-					<div class="widget">
-						<h2 class="widgettitle"><?php _e( 'Most Used Categories', '_s' ); ?></h2>
-						<ul>
-						<?php wp_list_categories( array( 'orderby' => 'count', 'order' => 'DESC', 'show_count' => 1, 'title_li' => '', 'number' => 10 ) ); ?>
-						</ul>
-					</div><!-- .widget -->
+						<div class="widget">
+							<h2 class="widgettitle"><?php _e( 'Most Used Categories', '_s' ); ?></h2>
+							<ul>
+							<?php wp_list_categories( array( 'orderby' => 'count', 'order' => 'DESC', 'show_count' => 1, 'title_li' => '', 'number' => 10 ) ); ?>
+							</ul>
+						</div><!-- .widget -->
 
-					<?php
-					/* translators: %1$s: smilie */
-					$archive_content = '<p>' . sprintf( __( 'Try looking in the monthly archives. %1$s', '_s' ), convert_smilies( ':)' ) ) . '</p>';
-					the_widget( 'WP_Widget_Archives', 'dropdown=1', "after_title=</h2>$archive_content" );
-					?>
+						<?php
+						/* translators: %1$s: smilie */
+						$archive_content = '<p>' . sprintf( __( 'Try looking in the monthly archives. %1$s', '_s' ), convert_smilies( ':)' ) ) . '</p>';
+						the_widget( 'WP_Widget_Archives', 'dropdown=1', "after_title=</h2>$archive_content" );
+						?>
 
-					<?php the_widget( 'WP_Widget_Tag_Cloud' ); ?>
+						<?php the_widget( 'WP_Widget_Tag_Cloud' ); ?>
 
-				</div><!-- .entry-content -->
-			</article><!-- #post-0 .post .error404 .not-found -->
+					</div><!-- .entry-content -->
+				</article><!-- #post-0 .post .error404 .not-found -->
 
-		</div><!-- #content .site-content -->
-	</div><!-- #primary .content-area -->
+			</div><!-- #content .site-content -->
+		</section><!-- #primary .content-area -->
 
 <?php get_footer(); ?>
