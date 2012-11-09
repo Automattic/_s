57b75470fb710a653733177a42af874a555f17c9
Erick Hitter <ehitter@gmail.com>
2012-11-09 14:19:30 -0500

    Include the ID of the footer in IS settings so we can match the IS footer's width to it.

--- functions.php
+++ functions.php
@@ -81,7 +81,10 @@ function _s_setup() {
 	 *
 	 * For more options, see http://jetpack.me/support/infinite-scroll/
 	 */
-	add_theme_support( 'infinite-scroll', array( 'container' => 'content' ) );
+	add_theme_support( 'infinite-scroll', array(
+		'container' => 'content',
+		'footer'    => 'page'
+	) );
 }
 endif; // _s_setup
 add_action( 'after_setup_theme', '_s_setup' );
