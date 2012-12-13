a7ce5279ae692beb375bf3b0e52d3172cb668435
BinaryMoon <binarymoon@gmail.com>
2012-12-13 12:46:02 +0000

    only display sub heading if it's set
    remove empty heading tags

--- header.php
+++ header.php
@@ -28,7 +28,9 @@
 	<header id="masthead" class="site-header" role="banner">
 		<hgroup>
 			<h1 class="site-title"><a href="<?php echo esc_url( home_url( '/' ) ); ?>" title="<?php echo esc_attr( get_bloginfo( 'name', 'display' ) ); ?>" rel="home"><?php bloginfo( 'name' ); ?></a></h1>
-			<h2 class="site-description"><?php bloginfo( 'description' ); ?></h2>
+			<?php if ( get_bloginfo( 'description' ) ) { ?>
+				<h2 class="site-description"><?php bloginfo( 'description' ); ?></h2>			
+			<?php } ?>
 		</hgroup>
 
 		<nav role="navigation" class="site-navigation main-navigation">
