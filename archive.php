3bf329683dbed4318bd1e61f9ffbd36150577a2e
Jason Manheim <jason@designpx.com>
2012-12-29 22:21:20 -0800

    Condence code and add custom taxonomy support for descriptions.

--- archive.php
+++ archive.php
@@ -52,18 +52,10 @@ get_header(); ?>
 						?>
 					</h1>
 					<?php
-						if ( is_category() ) {
-							// show an optional category description
-							$category_description = category_description();
-							if ( ! empty( $category_description ) )
-								echo apply_filters( 'category_archive_meta', '<div class="taxonomy-description">' . $category_description . '</div>' );
-
-						} elseif ( is_tag() ) {
-							// show an optional tag description
-							$tag_description = tag_description();
-							if ( ! empty( $tag_description ) )
-								echo apply_filters( 'tag_archive_meta', '<div class="taxonomy-description">' . $tag_description . '</div>' );
-						}
+						// show an optional term description
+						$term_description = term_description();
+						if ( ! empty( $term_description ) )
+							echo ( '<div class="entry-content">' . $term_description . '</div>' );
 					?>
 				</header><!-- .page-header -->
 
