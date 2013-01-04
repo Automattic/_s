cfb7f7fcdf6d0d6d11dc4dee8d04258e92090821
David Ensinger <ensinger.4@osu.edu>
2013-01-04 16:09:37 -0500

    Load comments template only when comments are open or there's at least one comment
    To be consistent with single.php and to ensure that an empty div (<div
    id="comments" class="comments-area">) isn't outputted when both
    comments are closed and there aren't any comments.

--- image.php
+++ image.php
@@ -109,7 +109,11 @@ get_header();
 					</footer><!-- .entry-meta -->
 				</article><!-- #post-<?php the_ID(); ?> -->
 
-				<?php comments_template(); ?>
+				<?php
+					// If comments are open or we have at least one comment, load up the comment template
+					if ( comments_open() || '0' != get_comments_number() )
+						comments_template( '', true );
+				?>
 
 			<?php endwhile; // end of the loop. ?>
 
