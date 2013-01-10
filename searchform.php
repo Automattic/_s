0cfa0e1da5f85ce24372495db03fc00e58e92424
Piero Bellomo <piero.bellomo@viewlab.net>
2013-01-10 23:43:29 +0100

    Bootstrap style search box

--- searchform.php
+++ searchform.php
@@ -6,8 +6,7 @@
  * @since _s 1.0
  */
 ?>
-	<form method="get" id="searchform" action="<?php echo esc_url( home_url( '/' ) ); ?>" role="search">
+	<form method="get" id="searchform" action="<?php echo esc_url( home_url( '/' ) ); ?>" role="search" class="navbar-search pull-right">
 		<label for="s" class="assistive-text"><?php _e( 'Search', '_s' ); ?></label>
-		<input type="text" class="field" name="s" value="<?php echo esc_attr( get_search_query() ); ?>" id="s" placeholder="<?php esc_attr_e( 'Search &hellip;', '_s' ); ?>" />
-		<input type="submit" class="submit" name="submit" id="searchsubmit" value="<?php esc_attr_e( 'Search', '_s' ); ?>" />
+		<input type="text" class="search-query" name="s" value="<?php echo esc_attr( get_search_query() ); ?>" id="s" placeholder="<?php esc_attr_e( 'Search &hellip;', '_s' ); ?>" />
 	</form>
