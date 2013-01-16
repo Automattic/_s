1b62626426f8b9ee41aebc2701aeb27950bba12c
Piero Bellomo <piero.bellomo@viewlab.net>
2013-01-16 02:13:51 +0100

    added appended button to search form

--- searchform.php
+++ searchform.php
@@ -6,7 +6,10 @@
  * @since _s 1.0
  */
 ?>
-	<form method="get" id="searchform" action="<?php echo esc_url( home_url( '/' ) ); ?>" role="search" class="navbar-search pull-right">
+	<form method="get" id="searchform" action="<?php echo esc_url( home_url( '/' ) ); ?>" role="search" class="navbar-search form-search pull-right">
 		<label for="s" class="assistive-text"><?php _e( 'Search', '_s' ); ?></label>
-		<input type="text" class="search-query" name="s" value="<?php echo esc_attr( get_search_query() ); ?>" id="s" placeholder="<?php esc_attr_e( 'Search &hellip;', '_s' ); ?>" />
+        <div class="input-append">
+		    <input type="text" class="search-query span2" name="s" value="<?php echo esc_attr( get_search_query() ); ?>" id="s" placeholder="<?php esc_attr_e( 'Search &hellip;', '_s' ); ?>" />
+            <button type="submit" class="btn"><i class="icon-search"></i></button>
+        </div>
 	</form>
