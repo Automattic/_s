e04732c5a3ac22fe9c29aba6e72b9ddbd5248616
Michael Fields <michael@mfields.org>
2013-02-27 15:08:26 -0800

    Remove _s_content_nav( 'nav-above' ); from the top of all templates. Fixes #148

--- archive.php
+++ archive.php
@@ -67,8 +67,6 @@ get_header(); ?>
 					?>
 				</header><!-- .page-header -->
 
-				<?php _s_content_nav( 'nav-above' ); ?>
-
 				<?php /* Start the Loop */ ?>
 				<?php while ( have_posts() ) : the_post(); ?>
 
