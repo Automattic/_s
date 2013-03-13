1ba2f627521385f91110f89b6d457d57c96a6d12
obenland <obenland@gmx.de>
2013-03-13 15:39:05 -0700

    _s: Use .screen-reader-text rather than .assistive-text.
    With 3.6 introducing many HTML5 improvements the need for custom
    searchform and comment templates will vanish. To cope with core's
    .screen-reader-text class, let's make the switch early.
    
    Commit also adds core's focus styles for screen reader texts, so they
    can actually be used.

--- searchform.php
+++ searchform.php
@@ -7,7 +7,7 @@
  */
 ?>
 	<form method="get" id="searchform" class="searchform" action="<?php echo esc_url( home_url( '/' ) ); ?>" role="search">
-		<label for="s" class="assistive-text"><?php _ex( 'Search', 'assistive text', '_s' ); ?></label>
+		<label for="s" class="screen-reader-text"><?php _ex( 'Search', 'assistive text', '_s' ); ?></label>
 		<input type="search" class="field" name="s" value="<?php echo esc_attr( get_search_query() ); ?>" id="s" placeholder="<?php echo esc_attr_x( 'Search &hellip;', 'placeholder', '_s' ); ?>" />
 		<input type="submit" class="submit" id="searchsubmit" value="<?php echo esc_attr_x( 'Search', 'submit button', '_s' ); ?>" />
 	</form>
