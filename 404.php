153c76c92b39ef3de9cb3aae52584c4aa9fb9379
Slobodan Manic <slobodan.manic@gmail.com>
2013-03-21 06:42:55 +0100

    Skip printing categories widget in 404 template if there's only one category
    Addresses issue 183 - https://github.com/Automattic/_s/issues/183

--- 404.php
+++ 404.php
@@ -22,12 +22,14 @@ get_header(); ?>
 
 					<?php the_widget( 'WP_Widget_Recent_Posts' ); ?>
 
+					<?php if ( _s_categorized_blog() ) { // Only print the widget if blog has multiple categories ?>					
 					<div class="widget">
 						<h2 class="widgettitle"><?php _e( 'Most Used Categories', '_s' ); ?></h2>
 						<ul>
 						<?php wp_list_categories( array( 'orderby' => 'count', 'order' => 'DESC', 'show_count' => 1, 'title_li' => '', 'number' => 10 ) ); ?>
 						</ul>
 					</div><!-- .widget -->
+					<?php } // end check for multiple categories ?>
 
 					<?php
 					/* translators: %1$s: smiley */
