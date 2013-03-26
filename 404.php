32d243248746062a0635236be340bb1cd994561c
obenland <obenland@gmx.de>
2013-03-26 15:55:49 -0700

    _s: Minor updates to categories widget. Closes #183.

--- 404.php
+++ 404.php
@@ -22,14 +22,14 @@ get_header(); ?>
 
 					<?php the_widget( 'WP_Widget_Recent_Posts' ); ?>
 
-					<?php if ( _s_categorized_blog() ) : // Only print the widget if blog has multiple categories ?>					
-					<div class="widget">
-						<h2 class="widgettitle"><?php _e( 'Most Used Categories', '_s' ); ?></h2>
+					<?php if ( _s_categorized_blog() ) : // Only show the widget if site has multiple categories. ?>
+					<div class="widget widget_categories">
+						<h1 class="widgettitle"><?php _e( 'Most Used Categories', '_s' ); ?></h2>
 						<ul>
-						<?php wp_list_categories( array( 'orderby' => 'count', 'order' => 'DESC', 'show_count' => 1, 'title_li' => '', 'number' => 10 ) ); ?>
+							<?php wp_list_categories( array( 'orderby' => 'count', 'order' => 'DESC', 'show_count' => 1, 'title_li' => '', 'number' => 10 ) ); ?>
 						</ul>
 					</div><!-- .widget -->
-					<?php endif; // end check for multiple categories ?>
+					<?php endif; ?>
 
 					<?php
 					/* translators: %1$s: smiley */
