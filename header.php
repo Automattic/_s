bb3366f2d1e4e6c7fed3f2ff358173a6f7b8cb6c
Eli McMakin <elimc184@hotmail.com>
2013-04-04 09:27:42 -0500

    Force latest IE rendering engine and Google Chrome Frame

--- header.php
+++ header.php
@@ -9,6 +9,7 @@
 ?><!DOCTYPE html>
 <html <?php language_attributes(); ?>>
 <head>
+<meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
 <meta charset="<?php bloginfo( 'charset' ); ?>" />
 <meta name="viewport" content="width=device-width" />
 <title><?php wp_title( '|', true, 'right' ); ?></title>
