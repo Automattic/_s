07c44bbeefd536f143c702f44c5bbf0a399fb6fb
Philip Arthur Moore <philip@automattic.com>
2013-04-24 15:08:06 +0900

    Restore site-header class to header element and give site-branding class to title and description wrapper div. See #206.

--- header.php
+++ header.php
@@ -24,8 +24,8 @@
 <body <?php body_class(); ?>>
 <div id="page" class="hfeed site">
 	<?php do_action( 'before' ); ?>
-	<header id="masthead" role="banner">
-		<div class="site-header">
+	<header id="masthead" class="site-header" role="banner">
+		<div class="site-branding">
 			<h1 class="site-title"><a href="<?php echo esc_url( home_url( '/' ) ); ?>" title="<?php echo esc_attr( get_bloginfo( 'name', 'display' ) ); ?>" rel="home"><?php bloginfo( 'name' ); ?></a></h1>
 			<h2 class="site-description"><?php bloginfo( 'description' ); ?></h2>
 		</div>
