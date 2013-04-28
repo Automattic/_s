456cd3dd01b5e66a15c8c6d20b484aec7a6d1a14
obenland <obenland@gmx.de>
2013-04-28 04:13:55 -0700

    _s: Removes unnecessary switch in and out of PHP.

--- comments.php
+++ comments.php
@@ -9,16 +9,14 @@
  *
  * @package _s
  */
-?>
 
-<?php
-	/*
-	 * If the current post is protected by a password and
-	 * the visitor has not yet entered the password we will
-	 * return early without loading the comments.
-	 */
-	if ( post_password_required() )
-		return;
+/*
+ * If the current post is protected by a password and
+ * the visitor has not yet entered the password we will
+ * return early without loading the comments.
+ */
+if ( post_password_required() )
+	return;
 ?>
 
 	<div id="comments" class="comments-area">
