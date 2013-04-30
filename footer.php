23c12d9595840e2b400c933e0810b712c66e843a
vmnlopes <vmnlop@gmail.com>
2013-04-30 09:50:22 +0100

    adding sidebar to the footer and changing disigner name

--- footer.php
+++ footer.php
@@ -6,16 +6,18 @@
  *
  * @package _s
  */
+get_sidebar('footer');
 ?>
 
-	</div><!-- #main -->
-
+</div><!-- #main -->
 	<footer id="colophon" class="site-footer" role="contentinfo">
 		<div class="site-info">
 			<?php do_action( '_s_credits' ); ?>
-			<a href="http://wordpress.org/" title="<?php esc_attr_e( 'A Semantic Personal Publishing Platform', '_s' ); ?>" rel="generator"><?php printf( __( 'Proudly powered by %s', '_s' ), 'WordPress' ); ?></a>
+			<a href="http://wordpress.org/" title="<?php esc_attr_e( 'A Semantic Personal Publishing Platform', '_s' ); ?>
+			" rel="generator"><?php printf( __( 'Proudly powered by %s', '_s' ), 'WordPress' ); ?></a>
 			<span class="sep"> | </span>
-			<?php printf( __( 'Theme: %1$s by %2$s.', '_s' ), '_s', '<a href="http://automattic.com/" rel="designer">Automattic</a>' ); ?>
+			<?php printf( __( 'Theme: %1$s by %2$s.', 'Maquina' ), 'Maquina',
+			 'Vitor Lopes <a href="http://vnlweb.com" rel="designer">vnlweb.com</a>' ); ?>
 		</div><!-- .site-info -->
 	</footer><!-- #colophon -->
 </div><!-- #page -->
