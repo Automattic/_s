f28c7c4ec6a6f932e99bbfe4462db6cc54ea407b
vmnlopes <vmnlop@gmail.com>
2013-05-03 19:20:51 +0100

    removing testing sylesheet

--- header.php
+++ header.php
@@ -19,7 +19,6 @@
 <script src="<?php echo get_template_directory_uri(); ?>/js/html5.js" type="text/javascript"></script>
 <!--[endif]-->
 <?php wp_head(); ?>
-<link rel="stylesheet" href="<?php echo get_template_directory_uri(); ?>/layouts/vnlweb.css" type="text/css" />
 </head>
 <body <?php body_class(); ?>>
 <div id="page" class="hfeed site">
