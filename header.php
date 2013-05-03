a85087c9c4de4444448409da9517194145c1e347
vmnlopes <vmnlop@gmail.com>
2013-05-03 19:19:22 +0100

    DOS and UNIX style line endings This causes a problem with SVN repositories removing for now

--- header.php
+++ header.php
@@ -17,7 +17,6 @@
 <link rel="pingback" href="<?php bloginfo( 'pingback_url' ); ?>" />
 <!--[if lt IE 9]>
 <script src="<?php echo get_template_directory_uri(); ?>/js/html5.js" type="text/javascript"></script>
-<script src="<?php echo get_template_directory_uri(); ?>/js/css3-mediaqueries.js" type="text/javascript"></script>
 <!--[endif]-->
 <?php wp_head(); ?>
 <link rel="stylesheet" href="<?php echo get_template_directory_uri(); ?>/layouts/vnlweb.css" type="text/css" />
