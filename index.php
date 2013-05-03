9aa49ee153f06752dcb1148ed9e80f6f03f1097c
vmnlopes <vmnlop@gmail.com>
2013-05-03 21:52:26 +0100

    add post trumbnails

--- index.php
+++ index.php
@@ -11,24 +11,34 @@
  * @Maquina
  */
 get_header();
-get_sidebar('left'); ?>
+get_sidebar('left'); 
+?>
 	<div id="primary" class="content-area">
 		<div id="content" class="site-content" role="main">
 
 		<?php if ( have_posts() ) : ?>
 
 			<?php /* Start the Loop */ ?>
-			<?php while ( have_posts() ) : the_post(); ?>
+			
+			<?php if ( has_post_thumbnail() ) {
+				
+			 /* check if the post has a Post Thumbnail assigned to it. */
+			 
+				the_post_thumbnail(); 
+			} 
+				the_content();
+			?>
+		<?php while ( have_posts() ) : the_post(); ?>
 
-				<?php
-					/* Include the Post-Format-specific template for the content.
-					 * If you want to overload this in a child theme then include a file
-					 * called content-___.php (where ___ is the Post Format name) and that will be used instead.
-					 */
-					get_template_part( 'content', get_post_format() );
-				?>
+			<?php
+				/* Include the Post-Format-specific template for the content.
+				 * If you want to overload this in a child theme then include a file
+				 * called content-___.php (where ___ is the Post Format name) and that will be used instead.
+				 */
+				get_template_part( 'content', get_post_format() );
+			?>
 
-			<?php endwhile; ?>
+		<?php endwhile; ?>
 
 			<?php _s_content_nav( 'nav-below' ); ?>
 
