62b0b2cc9f3adcafa5a3289dd8c8ad91c2e083fa
vmnlopes <vmnlop@gmail.com>
2013-05-04 21:53:23 +0100

    add footer credits

--- footer.php
+++ footer.php
@@ -12,12 +12,9 @@
 <?php get_sidebar('footer');?>
 	<footer id="colophon" class="site-footer" role="contentinfo">
 		<div class="site-info">
-			<?php do_action( '_s_credits' ); ?>
-			<a href="http://wordpress.org/" title="<?php esc_attr_e( 'A Semantic Personal Publishing Platform', '_s' ); ?>
-			" rel="generator"><?php printf( __( 'Proudly powered by %s', '_s' ), 'WordPress' ); ?></a>
-			<span class="sep"> | </span>
-			<?php printf( __( 'Theme: %1$s by %2$s.', 'Maquina' ), 'Maquina',
-			 'Vitor Lopes <a href="http://vnlweb.com" rel="designer">vnlweb.com</a>' ); ?>
+			<?php do_action( 'Maquina_credits' ); ?>
+			<?php printf( __( 'Maquina Theme by %2$s.', 'Maquina' ), 'Maquina',
+			 '<a href="http://vnlweb.com/my-projects/maquina/" rel="designer">vnlweb</a>' ); ?>
 		</div><!-- .site-info -->
 	</footer><!-- #colophon -->
 </div><!-- #page -->
