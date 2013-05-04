e2fcf74aa3171926c75b103305938a61ac0f48ee
vmnlopes <vmnlop@gmail.com>
2013-05-04 16:57:11 +0100

    removed google analytics

--- footer.php
+++ footer.php
@@ -23,12 +23,5 @@
 </div><!-- #page -->
 
 <?php wp_footer(); ?>
-<!-- Google Analytics: change UA-XXXXX-X to be your site's ID. -->
-        <script>
-            var _gaq=[['_setAccount','UA-XXXXX-X'],['_trackPageview']];
-            (function(d,t){var g=d.createElement(t),s=d.getElementsByTagName(t)[0];
-            g.src='//www.google-analytics.com/ga.js';
-            s.parentNode.insertBefore(g,s)}(document,'script'));
-        </script>
 </body>
 </html>
