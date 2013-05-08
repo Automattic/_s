f367fefd725a4a1fa8777b9bd153806e53d2ffe8
Takashi Irie <takashi@automattic.com>
2013-05-08 12:27:48 +0100

    Fix unlinked site title in the Customizer

--- js/customizer.js
+++ js/customizer.js
@@ -8,7 +8,7 @@
 	// Site title and description.
 	wp.customize( 'blogname', function( value ) {
 		value.bind( function( to ) {
-			$( '.site-title' ).text( to );
+			$( '.site-title a' ).text( to );
 		} );
 	} );
 	wp.customize( 'blogdescription', function( value ) {
