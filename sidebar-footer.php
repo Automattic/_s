33c145020de34e86adafde2dafce8d7453d36e67
vmnlopes <vmnlop@gmail.com>
2013-05-10 18:55:44 +0100

    minor corrections

--- sidebar-footer.php
+++ sidebar-footer.php
@@ -6,28 +6,10 @@
  * @based on Twenty Eleven 1.0
  */
 ?>
-<div id="footer-widgets-container" <?php maquina_footer_sidebar_class(); ?>>
-	<?php if ( is_active_sidebar( 'footer-sidebar-1' ) ) : ?>
-	<div id="footer-widget-1" class="footer-widget-1" role="complementary">
-		<?php dynamic_sidebar( 'footer-sidebar-1' ); ?>
+<div id="footer-widgets-container" class="footer-widgets-container">
+	<?php if ( is_active_sidebar( 'footer-sidebar' ) ) : ?>
+	<div id="footer-widget" class="footer-widget widgetleft" role="complementary">
+		<?php dynamic_sidebar( 'footer-sidebar' ); ?>
 	</div><!-- #first .widget-area -->
-	<?php endif; ?>
-
-	<?php if ( is_active_sidebar( 'footer-sidebar-2' ) ) : ?>
-	<div id="footer-widget-2" class="footer-widget-2" role="complementary">
-		<?php dynamic_sidebar( 'footer-sidebar-2' ); ?>
-	</div><!-- #second .widget-area -->
-	<?php endif; ?>
-
-	<?php if ( is_active_sidebar( 'footer-sidebar-3' ) ) : ?>
-	<div id="footer-widget-3" class="footer-widget-3" role="complementary">
-		<?php dynamic_sidebar( 'footer-sidebar-3' ); ?>
-	</div><!-- #third .widget-area -->
-	<?php endif; ?>
-	
-	<?php if ( is_active_sidebar( 'footer-sidebar-4' ) ) : ?>
-	<div id="footer-widget-4" class="footer-widget-4" role="complementary">
-		<?php dynamic_sidebar( 'footer-sidebar-4' ); ?>
-	</div><!-- #fourth .widget-area -->	
 	<?php endif; ?>
 </div><!-- #footer-widgets-container -->
--- sidebar-header.php
+++ sidebar-header.php
@@ -6,28 +6,10 @@
  * @based on Twenty Eleven 1.0
  */
 ?>
-<div id="header-widgets-container" <?php maquina_header_sidebar_class(); ?>>
-	<?php if ( is_active_sidebar( 'header-sidebar-1' ) ) : ?>
-	<div id="header-widget-1" class="header-widget-1" role="complementary">
-		<?php dynamic_sidebar( 'header-sidebar-1' ); ?>
+<div id="header-widgets-container" class="header-widgets-container">
+	<?php if ( is_active_sidebar( 'header-sidebar' ) ) : ?>
+	<div id="header-widget" class="header-widget widgetleft" role="complementary">
+		<?php dynamic_sidebar( 'header-sidebar' ); ?>
 	</div><!-- #first .widget-area -->
-	<?php endif; ?>
-
-	<?php if ( is_active_sidebar( 'header-sidebar-2' ) ) : ?>
-	<div id="header-widget-2" class="header-widget-2" role="complementary">
-		<?php dynamic_sidebar( 'header-sidebar-2' ); ?>
-	</div><!-- #second .widget-area -->
-	<?php endif; ?>
-
-	<?php if ( is_active_sidebar( 'header-sidebar-3' ) ) : ?>
-	<div id="header-widget-3" class="header-widget-3" role="complementary">
-		<?php dynamic_sidebar( 'header-sidebar-3' ); ?>
-	</div><!-- #third .widget-area -->
-	<?php endif; ?>
-	
-	<?php if ( is_active_sidebar( 'header-sidebar-4' ) ) : ?>
-	<div id="header-widget-4" class="header-widget-4" role="complementary">
-		<?php dynamic_sidebar( 'header-sidebar-4' ); ?>
-	</div><!-- #fourth .widget-area -->	
 	<?php endif; ?>
 </div><!-- #header-widgets-container -->
