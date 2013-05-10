85caa8018983b128fc49432225a68e3762935ad2
vmnlopes <vmnlop@gmail.com>
2013-05-10 18:54:06 +0100

    modifing analitycs and adding translation to logo

--- functions.php
+++ functions.php
@@ -120,8 +120,6 @@ add_action( 'after_setup_theme', 'maquina_register_custom_background' );
  
 function maquina_scripts() {
 	    
-	wp_enqueue_script( 'modernizr', get_template_directory_uri() . '/js/modernizr.custom.20146.js', array(), '2.6.1', true );
-		
 	wp_enqueue_script( 'Maquina-prefixfree', get_template_directory_uri() . '/js/prefixfree.min.js' );
 
 	wp_enqueue_script( 'Maquina-navigation', get_template_directory_uri() . '/js/navigation.js', array(), '20120206', true );
@@ -179,7 +177,34 @@ function maquina_load_stylesheets () {
 
 add_action('wp_head', 'maquina_load_stylesheets');
 
-/*  add_action('wp_footer', 'add_googleanalytics');
+// Include the Google Analytics Tracking Code (ga.js)
+// @ http://code.google.com/apis/analytics/docs/tracking/asyncUsageGuide.html
+function google_analytics_tracking_code(){
+
+	$maquina_ga_code = 'UA-XXXXX-X'; // GA Property ID
+
+	if ($options['ga_enable']) { ?>
+
+		<script type="text/javascript">
+		  var _gaq = _gaq || [];
+		  _gaq.push(['_setAccount', '<?php echo $maquina_ga_code; ?>']);
+		  _gaq.push(['_trackPageview']);
+
+		  (function() {
+		    var ga = document.createElement('script'); ga.type = 'text/javascript'; ga.async = true;
+		    ga.src = ('https:' == document.location.protocol ? 'https://ssl' : 'http://www') + '.google-analytics.com/ga.js';
+		    var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(ga, s);
+		  })();
+		</script>
+
+<?php }
+}
+
+//  tracking code before the closing body tag
+add_action('wp_footer', 'google_analytics_tracking_code');
+ 
+  
+ /*
  *
  * uncomment to activate
  *
@@ -199,7 +224,7 @@ function add_googleanalytics() { ?>
     $wp_customize->add_section( 'maquina_logo_section' , array(
     'title'       => __( 'Logo', 'maquina' ),
     'priority'    => 30,
-    'description' => __( 'Upload a logo to replace the default site name and description in the header' ),
+    'description' => __( 'Upload a logo to replace the default site name and description in the header','maquina' ),
 ) );
 	$wp_customize->add_setting( 'maquina_logo' );
 	
