5014af54a4237b4d1770d0e5d49a2f6dc22afc0a
Konstantin Kovshenin <kovshenin@gmail.com>
2013-05-17 09:34:30 +0400

    Remove trailing whitespace.

--- content.php
+++ content.php
@@ -7,7 +7,7 @@
 <article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
 	<header class="entry-header">
 		<h1 class="entry-title"><a href="<?php the_permalink(); ?>" rel="bookmark"><?php the_title(); ?></a></h1>
-		
+
 		<?php if ( 'post' == get_post_type() ) : ?>
 		<div class="entry-meta">
 			<?php _s_posted_on(); ?>
