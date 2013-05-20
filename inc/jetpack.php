c7bf07061d8ba2d4e8cc0240c1b50b914ed8f2d6
obenland <obenland@gmx.de>
2013-05-20 13:02:30 +0200

    _s: Let's not limit the setup function to IS.

--- inc/jetpack.php
+++ inc/jetpack.php
@@ -10,10 +10,10 @@
  * Add theme support for Infinite Scroll.
  * See: http://jetpack.me/support/infinite-scroll/
  */
-function _s_infinite_scroll_setup() {
+function _s_jetpack_setup() {
 	add_theme_support( 'infinite-scroll', array(
 		'container' => 'content',
 		'footer'    => 'page',
 	) );
 }
-add_action( 'after_setup_theme', '_s_infinite_scroll_setup' );
+add_action( 'after_setup_theme', '_s_jetpack_setup' );
