9afe82b900bc626864ee1bf573cf3ca994bba7ac
obenland <obenland@gmx.de>
2013-05-25 12:35:51 +0200

    _s: Require is a statement, not a function. Props @drrobotnik. Fixes #213.

--- functions.php
+++ functions.php
@@ -125,29 +125,29 @@ add_action( 'wp_enqueue_scripts', '_s_scripts' );
 /**
  * Implement the Custom Header feature.
  */
-//require( get_template_directory() . '/inc/custom-header.php' );
+//require get_template_directory() . '/inc/custom-header.php';
 
 /**
  * Custom template tags for this theme.
  */
-require( get_template_directory() . '/inc/template-tags.php' );
+require get_template_directory() . '/inc/template-tags.php';
 
 /**
  * Custom functions that act independently of the theme templates.
  */
-require( get_template_directory() . '/inc/extras.php' );
+require get_template_directory() . '/inc/extras.php';
 
 /**
  * Customizer additions.
  */
-require( get_template_directory() . '/inc/customizer.php' );
+require get_template_directory() . '/inc/customizer.php';
 
 /**
  * Load Jetpack compatibility file.
  */
-require( get_template_directory() . '/inc/jetpack.php' );
+require get_template_directory() . '/inc/jetpack.php';
 
 /**
  * WordPress.com-specific functions and definitions.
  */
-//require( get_template_directory() . '/inc/wpcom.php' );
+//require get_template_directory() . '/inc/wpcom.php';
