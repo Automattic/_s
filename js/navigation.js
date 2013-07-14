4c99b2aba895834d4551eb03c790ecc337351ca7
Shea Bunge <info@bungeshea.com>
2013-07-14 16:38:08 +1000

    Use === and !== instead of == and !=

--- js/navigation.js
+++ js/navigation.js
@@ -11,22 +11,22 @@
 		return;
 
 	button = container.getElementsByTagName( 'h1' )[0];
-	if ( 'undefined' == typeof button )
+	if ( 'undefined' === typeof button )
 		return;
 
 	menu = container.getElementsByTagName( 'ul' )[0];
 
 	// Hide menu toggle button if menu is empty and return early.
-	if ( 'undefined' == typeof menu ) {
+	if ( 'undefined' === typeof menu ) {
 		button.style.display = 'none';
 		return;
 	}
 
-	if ( -1 == menu.className.indexOf( 'nav-menu' ) )
+	if ( -1 === menu.className.indexOf( 'nav-menu' ) )
 		menu.className += ' nav-menu';
 
 	button.onclick = function() {
-		if ( -1 != container.className.indexOf( 'toggled' ) )
+		if ( -1 !== container.className.indexOf( 'toggled' ) )
 			container.className = container.className.replace( ' toggled', '' );
 		else
 			container.className += ' toggled';
