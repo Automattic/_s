5c3a219888fb8142e16d9b4055c51211c2760d3b
Thomas Guillot <thomas@automattic.com>
2013-08-20 10:29:59 +0100

    _s: Changed the elements to match arrow key codes with navigation
    Previous JS was not working properly

--- js/keyboard-image-navigation.js
+++ js/keyboard-image-navigation.js
@@ -2,10 +2,10 @@ jQuery( document ).ready( function( $ ) {
 	$( document ).keydown( function( e ) {
 		var url = false;
 		if ( e.which === 37 ) {  // Left arrow key code
-			url = $( '.previous-image a' ).attr( 'href' );
+			url = $( '.image-navigation .nav-previous a' ).attr( 'href' );
 		}
 		else if ( e.which === 39 ) {  // Right arrow key code
-			url = $( '.entry-attachment a' ).attr( 'href' );
+			url = $( '.image-navigation .nav-next a' ).attr( 'href' );
 		}
 		if ( url && ( !$( 'textarea, input' ).is( ':focus' ) ) ) {
 			window.location = url;
