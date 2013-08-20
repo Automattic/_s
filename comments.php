3d98e794d329b25ab3251bc23a394fb5e03821c3
Patrick Rauland <bftrick@gmail.com>
2013-08-20 07:27:46 -0500

    also changing overload to override in comments.php & archive.php

--- comments.php
+++ comments.php
@@ -43,7 +43,7 @@ if ( post_password_required() )
 			<?php
 				/* Loop through and list the comments. Tell wp_list_comments()
 				 * to use _s_comment() to format the comments.
-				 * If you want to overload this in a child theme then you can
+				 * If you want to override this in a child theme then you can
 				 * define _s_comment() and that will be used instead.
 				 * See _s_comment() in inc/template-tags.php for more.
 				 */
