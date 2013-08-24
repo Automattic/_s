48ac0da4e149f2576af7bc415d689fd515e8640e
David A. Kennedy <me@davidakennedy.com>
2013-08-24 02:06:06 -0400

    Second round of removing title attributes.

--- image.php
+++ image.php
@@ -20,14 +20,13 @@ get_header();
 					<div class="entry-meta">
 						<?php
 							$metadata = wp_get_attachment_metadata();
-							printf( __( 'Published <span class="entry-date"><time class="entry-date" datetime="%1$s">%2$s</time></span> at <a href="%3$s" title="Link to full-size image">%4$s &times; %5$s</a> in <a href="%6$s" title="Return to %7$s" rel="gallery">%8$s</a>', '_s' ),
+							printf( __( 'Published <span class="entry-date"><time class="entry-date" datetime="%1$s">%2$s</time></span> at <a href="%3$s">%4$s &times; %5$s</a> in <a href="%6$s" rel="gallery">%7$s</a>', '_s' ),
 								esc_attr( get_the_date( 'c' ) ),
 								esc_html( get_the_date() ),
 								esc_url( wp_get_attachment_url() ),
 								$metadata['width'],
 								$metadata['height'],
 								esc_url( get_permalink( $post->post_parent ) ),
-								esc_attr( strip_tags( get_the_title( $post->post_parent ) ) ),
 								get_the_title( $post->post_parent )
 							);
 
@@ -66,11 +65,11 @@ get_header();
 				<footer class="entry-meta">
 					<?php
 						if ( comments_open() && pings_open() ) : // Comments and trackbacks open
-							printf( __( '<a class="comment-link" href="#respond" title="Post a comment">Post a comment</a> or leave a trackback: <a class="trackback-link" href="%s" title="Trackback URL for your post" rel="trackback">Trackback URL</a>.', '_s' ), esc_url( get_trackback_url() ) );
+							printf( __( '<a class="comment-link" href="#respond">Post a comment</a> or leave a trackback: <a class="trackback-link" href="%s" rel="trackback">Trackback URL</a>.', '_s' ), esc_url( get_trackback_url() ) );
 						elseif ( ! comments_open() && pings_open() ) : // Only trackbacks open
-							printf( __( 'Comments are closed, but you can leave a trackback: <a class="trackback-link" href="%s" title="Trackback URL for your post" rel="trackback">Trackback URL</a>.', '_s' ), esc_url( get_trackback_url() ) );
+							printf( __( 'Comments are closed, but you can leave a trackback: <a class="trackback-link" href="%s" rel="trackback">Trackback URL</a>.', '_s' ), esc_url( get_trackback_url() ) );
 						elseif ( comments_open() && ! pings_open() ) : // Only comments open
-							 _e( 'Trackbacks are closed, but you can <a class="comment-link" href="#respond" title="Post a comment">post a comment</a>.', '_s' );
+							 _e( 'Trackbacks are closed, but you can <a class="comment-link" href="#respond">post a comment</a>.', '_s' );
 						elseif ( ! comments_open() && ! pings_open() ) : // Comments and trackbacks closed
 							_e( 'Both comments and trackbacks are currently closed.', '_s' );
 						endif;
