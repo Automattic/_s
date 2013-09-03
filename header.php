129c2382392e3d5253d02e8baefb5344b946e3ee
obenland <obenland@gmx.de>
2013-09-02 17:33:32 -0700

    _s: Define initial-scale for a better mobile experience when changing device orientation. Fixes: #285.

--- header.php
+++ header.php
@@ -10,7 +10,7 @@
 <html <?php language_attributes(); ?>>
 <head>
 <meta charset="<?php bloginfo( 'charset' ); ?>">
-<meta name="viewport" content="width=device-width">
+<meta name="viewport" content="width=device-width, initial-scale=1">
 <title><?php wp_title( '|', true, 'right' ); ?></title>
 <link rel="profile" href="http://gmpg.org/xfn/11">
 <link rel="pingback" href="<?php bloginfo( 'pingback_url' ); ?>">
