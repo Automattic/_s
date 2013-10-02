3ae7a5e310922c1d563f758c3bf3d6a8af92d5b9
David A. Kennedy <me@davidakennedy.com>
2013-10-01 21:30:53 -0400

    Make skip link focusable.

--- header.php
+++ header.php
@@ -29,7 +29,7 @@
 
 		<nav id="site-navigation" class="main-navigation" role="navigation">
 			<h1 class="menu-toggle"><?php _e( 'Menu', '_s' ); ?></h1>
-			<div class="screen-reader-text skip-link"><a href="#content"><?php _e( 'Skip to content', '_s' ); ?></a></div>
+			<div class="skip-link"><a class="screen-reader-text" href="#content"><?php _e( 'Skip to content', '_s' ); ?></a></div>
 
 			<?php wp_nav_menu( array( 'theme_location' => 'primary' ) ); ?>
 		</nav><!-- #site-navigation -->
