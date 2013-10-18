9b086fec62239988cad883291f2280bca62df094
Blaine Robison <brobison@elevatoragency.com>
2013-10-18 11:47:30 -0700

    Added required js files

---
+++ js/main.js
@@ -0,0 +1,11 @@
+;(function ($, window, document, undefined) {
+    'use strict';
+	$(document).ready( function() {
+		var Elevator = {
+			init : function() {
+
+			}
+		};
+		Elevator.init();
+	});
+})(jQuery, window, document);
