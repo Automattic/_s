6e5888ec199a67c4b4945b218195c1b363c1e930
Kazue Igarashi <gatespace.k@gmail.com>
2013-12-04 11:15:02 +0900

    archive.php: syntax error fix.

--- archive.php
+++ archive.php
@@ -39,10 +39,10 @@ get_header(); ?>
 							printf( __( 'Day: %s', '_s' ), '<span>' . get_the_date() . '</span>' );
 
 						elseif ( is_month() ) :
-							printf( __( 'Month: %s', '_s' ), '<span>' . get_the_date( _x( 'F Y', 'monthly archives date format', '_s' ) . '</span>' );
+							printf( __( 'Month: %s', '_s' ), '<span>' . get_the_date( _x( 'F Y', 'monthly archives date format', '_s' ) . '</span>' ) );
 
 						elseif ( is_year() ) :
-							printf( __( 'Year: %s', '_s' ), '<span>' . get_the_date( _x( 'Y', 'yearly archives date format', '_s' ) . '</span>' );
+							printf( __( 'Year: %s', '_s' ), '<span>' . get_the_date( _x( 'Y', 'yearly archives date format', '_s' ) . '</span>' ) );
 
 						elseif ( is_tax( 'post_format', 'post-format-aside' ) ) :
 							_e( 'Asides', '_s' );
