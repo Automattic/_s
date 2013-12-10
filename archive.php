ba8b39b62612f6364ffe823be91d6a3631ba60b6
Ulrich Pogson <grapplerulrich@gmail.com>
2013-12-10 23:50:07 +0100

    Add braces around conditionals in archive.php

--- archive.php
+++ archive.php
@@ -12,18 +12,18 @@ get_header(); ?>
 	<section id="primary" class="content-area">
 		<main id="main" class="site-main" role="main">
 
-		<?php if ( have_posts() ) : ?>
+		<?php if ( have_posts() ) { ?>
 
 			<header class="page-header">
 				<h1 class="page-title">
 					<?php
-						if ( is_category() ) :
+						if ( is_category() ) {
 							single_cat_title();
 
-						elseif ( is_tag() ) :
+						} elseif ( is_tag() ) {
 							single_tag_title();
 
-						elseif ( is_author() ) :
+						} elseif ( is_author() ) {
 							/* Queue the first post, that way we know
 							 * what author we're dealing with (if that is the case).
 							*/
@@ -35,65 +35,65 @@ get_header(); ?>
 							 */
 							rewind_posts();
 
-						elseif ( is_day() ) :
+						} elseif ( is_day() ) {
 							printf( __( 'Day: %s', '_s' ), '<span>' . get_the_date() . '</span>' );
 
-						elseif ( is_month() ) :
+						} elseif ( is_month() ) {
 							printf( __( 'Month: %s', '_s' ), '<span>' . get_the_date( _x( 'F Y', 'monthly archives date format', '_s' ) ) . '</span>' );
 
-						elseif ( is_year() ) :
+						} elseif ( is_year() ) {
 							printf( __( 'Year: %s', '_s' ), '<span>' . get_the_date( _x( 'Y', 'yearly archives date format', '_s' ) ) . '</span>' );
 
-						elseif ( is_tax( 'post_format', 'post-format-aside' ) ) :
+						} elseif ( is_tax( 'post_format', 'post-format-aside' ) ) {
 							_e( 'Asides', '_s' );
 
-						elseif ( is_tax( 'post_format', 'post-format-image' ) ) :
+						} elseif ( is_tax( 'post_format', 'post-format-image' ) ) {
 							_e( 'Images', '_s');
 
-						elseif ( is_tax( 'post_format', 'post-format-video' ) ) :
+						} elseif ( is_tax( 'post_format', 'post-format-video' ) ) {
 							_e( 'Videos', '_s' );
 
-						elseif ( is_tax( 'post_format', 'post-format-quote' ) ) :
+						} elseif ( is_tax( 'post_format', 'post-format-quote' ) ) {
 							_e( 'Quotes', '_s' );
 
-						elseif ( is_tax( 'post_format', 'post-format-link' ) ) :
+						} elseif ( is_tax( 'post_format', 'post-format-link' ) ) {
 							_e( 'Links', '_s' );
 
-						else :
+						} else {
 							_e( 'Archives', '_s' );
 
-						endif;
+						}
 					?>
 				</h1>
 				<?php
 					// Show an optional term description.
 					$term_description = term_description();
-					if ( ! empty( $term_description ) ) :
+					if ( ! empty( $term_description ) ) {
 						printf( '<div class="taxonomy-description">%s</div>', $term_description );
-					endif;
+					}
 				?>
 			</header><!-- .page-header -->
 
-			<?php /* Start the Loop */ ?>
-			<?php while ( have_posts() ) : the_post(); ?>
+			<?php /* Start the Loop */
+			while ( have_posts() ) {
 
-				<?php
-					/* Include the Post-Format-specific template for the content.
-					 * If you want to override this in a child theme, then include a file
-					 * called content-___.php (where ___ is the Post Format name) and that will be used instead.
-					 */
-					get_template_part( 'content', get_post_format() );
-				?>
+				the_post();
 
-			<?php endwhile; ?>
+				/* Include the Post-Format-specific template for the content.
+				 * If you want to override this in a child theme, then include a file
+				 * called content-___.php (where ___ is the Post Format name) and that will be used instead.
+				 */
+				get_template_part( 'content', get_post_format() );
 
-			<?php _s_paging_nav(); ?>
+			}
 
-		<?php else : ?>
+			_s_paging_nav();
 
-			<?php get_template_part( 'content', 'none' ); ?>
+		} else {
 
-		<?php endif; ?>
+			get_template_part( 'content', 'none' );
+
+		} ?>
 
 		</main><!-- #main -->
 	</section><!-- #primary -->
