643df0aa3cdfa5972c667ca41c862f11dbbf3d55
Kazue Igarashi <gatespace.k@gmail.com>
2013-12-27 16:29:49 +0900

    Post navigation bug fix.

--- inc/template-tags.php
+++ inc/template-tags.php
@@ -56,8 +56,8 @@ function _s_post_nav() {
 		<h1 class="screen-reader-text"><?php _e( 'Post navigation', '_s' ); ?></h1>
 		<div class="nav-links">
 
-			<?php previous_post_link( '%link', _x( '<span class="meta-nav">&larr;</span> %title', 'Previous post link', '_s' ) ); ?>
-			<?php next_post_link(     '%link', _x( '%title <span class="meta-nav">&rarr;</span>', 'Next post link',     '_s' ) ); ?>
+			<div class="nav-previous"><?php previous_post_link( '%link', _x( '<span class="meta-nav">&larr;</span> %title', 'Previous post link', '_s' ) ); ?></div>
+			<div class="nav-next"><?php next_post_link(     '%link', _x( '%title <span class="meta-nav">&rarr;</span>', 'Next post link',     '_s' ) ); ?></div>
 
 		</div><!-- .nav-links -->
 	</nav><!-- .navigation -->
