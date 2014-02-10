88cdc5222e0b88828c2067ce266cafba37e25d04
Ulrich Pogson <grapplerulrich@gmail.com>
2014-02-10 21:19:41 +0100

    Remove wpcom actions

--- header.php
+++ header.php
@@ -20,7 +20,7 @@
 
 <body <?php body_class(); ?>>
 <div id="page" class="hfeed site">
-	<?php do_action( 'before' ); ?>
+
 	<header id="masthead" class="site-header" role="banner">
 		<div class="site-branding">
 			<h1 class="site-title"><a href="<?php echo esc_url( home_url( '/' ) ); ?>" rel="home"><?php bloginfo( 'name' ); ?></a></h1>
