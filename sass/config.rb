5545b5dea884f7d81fb430147361723b73e4eaf8
Greg Rickaby <gregrickaby@gmail.com>
2014-02-20 16:13:34 -0600

    add (very basic) sass to _s

---
+++ sass/config.rb
@@ -0,0 +1,8 @@
+sass_dir        = "/"
+http_path       = "../"
+css_dir         = "../"
+images_dir      = "../"
+javascripts_dir = "../js"
+sass_options     = {:debug_info=>false}
+line_comments    = false
+preferred_syntax = :scss
