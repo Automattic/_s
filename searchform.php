4c84c0014342802ddf530094b5e01b907e1d3bc4
Piero Bellomo <piero.bellomo@viewlab.net>
2014-02-23 01:27:42 +0100

    Bootstrap 3 search form

--- searchform.php
+++ searchform.php
@@ -5,10 +5,10 @@
  * @package _s
  */
 ?>
-	<form method="get" id="searchform" action="<?php echo esc_url( home_url( '/' ) ); ?>" role="search" class="navbar-search form-search pull-right">
-		<label for="s" class="screen-reader-text"><?php _e( 'Search', '_s' ); ?></label>
-        <div class="input-append">
-		    <input type="text" class="search-query span2" name="s" value="<?php echo esc_attr( get_search_query() ); ?>" id="s" placeholder="<?php esc_attr_e( 'Search &hellip;', '_s' ); ?>" />
-            <button type="submit" class="btn"><i class="icon-search"></i></button>
+	<form method="get" id="searchform" action="<?php echo esc_url( home_url( '/' ) ); ?>" role="search" class="<?php echo bootstrap_searchform_class( debug_backtrace() ); ?>">
+        <div class="form-group">
+            <input type="text" class="form-control" name="s" value="<?php echo esc_attr( get_search_query() ); ?>" id="s" placeholder="<?php esc_attr_e( 'Search &hellip;', '_s' ); ?>" />
         </div>
+        <button type="submit" class="btn btn-default"><span class="glyphicon glyphicon-search"></span></button>
 	</form>
+
