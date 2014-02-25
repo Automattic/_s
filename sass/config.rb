cda790d5e2d4a5e2151c4f145fccb4b8875b31d1
Greg Rickaby <gregrickaby@gmail.com>
2014-02-25 14:19:01 -0600

    move partials into folder. config.rb tweak.

--- sass/config.rb
+++ sass/config.rb
@@ -1,8 +1,11 @@
-sass_dir        = "/"
-http_path       = "../"
-css_dir         = "../"
-images_dir      = "../"
-javascripts_dir = "../js"
+sass_dir         = "/"
+http_path        = "../"
+css_dir          = "../"
+images_dir       = "../"
+javascripts_dir  = "../js"
+relative_assets  = true
+output_style     = :expanded
+environment      = :development
 sass_options     = {:debug_info=>false}
 line_comments    = false
 preferred_syntax = :scss
