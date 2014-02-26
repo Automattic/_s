841e688166c71b4c7203f933028efd6189a65d39
Greg Rickaby <gregrickaby@gmail.com>
2014-02-26 16:53:46 -0600

    smarter config.rb

--- sass/config.rb
+++ sass/config.rb
@@ -4,8 +4,8 @@ css_dir          = "../"
 images_dir       = "../"
 javascripts_dir  = "../js"
 relative_assets  = true
-output_style     = :expanded
-environment      = :development
+environment      = :production
+output_style     = (environment == :production) ? :compact : :expanded
 sass_options     = {:debug_info=>false}
 line_comments    = false
 preferred_syntax = :scss
