5e62b8dc9edc13473a3d6cdcf423542d30346f1e
Ulrich Pogson <grapplerulrich@gmail.com>
2014-03-24 00:08:21 +0100

    Remove html from string

--- inc/template-tags.php
+++ inc/template-tags.php
@@ -82,7 +82,8 @@ function _s_posted_on() {
 		esc_html( get_the_modified_date() )
 	);
 
-	printf( __( '<span class="posted-on">Posted on %1$s</span><span class="byline"> by %2$s</span>', '_s' ),
+	printf(
+		'<span class="posted-on">' . __( 'Posted on %1$s', '_s' ) . '</span><span class="byline"> ' . __( 'by %2$s', '_s' ) . '</span>',
 		sprintf( '<a href="%1$s" rel="bookmark">%2$s</a>',
 			esc_url( get_permalink() ),
 			$time_string
