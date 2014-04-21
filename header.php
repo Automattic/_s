352e670b18008034d65b63fe2b821390ff590440
Aram Zucker-Scharff <azuckers@gmu.edu>
2014-04-21 18:51:34 -0400

    setting up a more full doctype

--- header.php
+++ header.php
@@ -6,8 +6,8 @@
  *
  * @package lcarzs
  */
-?><!DOCTYPE html>
-<html <?php language_attributes(); ?>>
+?><!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml" xmlns:og="http://ogp.me/ns#" xmlns:fb="https://www.facebook.com/2008/fbml" <?php language_attributes(); ?>>
 <head>
 <meta charset="<?php bloginfo( 'charset' ); ?>">
 <meta name="viewport" content="width=device-width, initial-scale=1">
