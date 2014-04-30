3ecf47b2f0ee7ea0207d406b60aa1e77171d5b70
Eric Lanehart <pushred@gmail.com>
2014-04-30 18:22:58 -0400

    Remove Feedback and Dashboard nav items, default to Posts index

--- functions.php
+++ functions.php
@@ -24,18 +24,25 @@ function api_url(){
     wp_redirect( 'https://www.storyteller.io/apis/wordpress-rest-api' );
 }
 
+function redirect_to_posts($url) {
+    return '/wp-admin/edit.php';
+}
+
 function customize_menu(){
     // http://justintadlock.com/archives/2011/06/13/removing-menu-pages-from-the-wordpress-admin
+    remove_menu_page('index.php');
     remove_menu_page('edit-comments.php');
     remove_menu_page('themes.php');
     remove_menu_page('tools.php');
     remove_menu_page('plugins.php');
+    remove_menu_page('edit.php?post_type=feedback');
 
     // http://codex.wordpress.org/Function_Reference/add_menu_page
     add_menu_page( 'API', 'API', 'activate_plugins', 'api', 'api_url', 'dashicons-admin-links', '20.1' );
 }
 
 add_action( 'after_setup_theme', 'setup' );
+add_filter( 'login_redirect', 'redirect_to_posts' );
 add_action( 'admin_menu', 'customize_menu', 999 );
 
 if( class_exists( WpeCommon ) ){
