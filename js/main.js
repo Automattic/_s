7c9513ea49beeaee051e17ec2fce358abce05219
Blaine Robison <brobison@elevatoragency.com>
2014-05-23 14:07:25 -0700

    added utility helper functions.

--- js/main.js
+++ js/main.js
@@ -3,8 +3,62 @@
 	$(document).ready( function() {
 		var Elevator = {
 			init : function() {
+                this.utils.init();
+			},
+            vals : {
+                $window : $(window)
+            },
+            utils : {
 
-			}
+                init : function() {
+
+                    this.mqState.appendEl();
+                    this.mqState.checkStateView();
+                    this.setDelay();
+
+                },
+                mqState : {
+
+                    appendEl : function() {
+
+                        $('body').append('<div class="mq-state"/>');
+
+                    },
+                    checkStateView : function() {
+
+                        Elevator.vals.state = $('.mq-state').css('content');
+                        Elevator.vals.view  = $('.mq-state').css('z-index');
+
+                    },
+
+                },
+                setDelay : function() {
+
+                    var scrolled = false,
+                        resized  = false;
+
+                    // delay checking of scroll
+                    Elevator.vals.$window.on( 'scroll touchmove', function() { scrolled = true; });
+                    // delay checking of window resize
+                    Elevator.vals.$window.on( 'resize', function() { resized = true; });
+
+                    setInterval( function() {
+
+                        if ( scrolled ) {
+
+                            scrolled = false;
+
+                        }
+
+                        if ( resized ) {
+
+                            resized = false;
+
+                        }
+
+                    }, 50);
+                }
+            },
 		};
 		Elevator.init();
 	});
