dde93a5f81278bcfadd7ba5dddf09912e0a59875
Blaine Robison <brobison@elevatoragency.com>
2014-05-23 14:23:05 -0700

    Compass configuration.

---
+++ config.rb
@@ -0,0 +1,14 @@
+require 'breakpoint'
+require 'susy'
+
+
+# http://compass-style.org/help/tutorials/configuration-reference/
+
+http_path       = '/'
+css_dir         = 'css'
+sass_dir        = 'css/sass'
+images_dir      = 'img'
+javascripts_dir = 'js'
+fonts_dir       = 'fonts'
+line_comments   = false
+output_style    = :expanded
