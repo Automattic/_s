09011e19f882f73058df2555d4bb5c856e366faf
Emil Uzelac <emil@uzelac.me>
2014-05-24 13:24:32 -0500

    Post Formats code formatting
    For consistency reasons and to match with the rest :) 
    No biggie even if not accepted, it just looks nicer that's all.

--- functions.php
+++ functions.php
@@ -46,7 +46,13 @@ function _s_setup() {
 	) );
 
 	// Enable support for Post Formats.
-	add_theme_support( 'post-formats', array( 'aside', 'image', 'video', 'quote', 'link' ) );
+	add_theme_support( 'post-formats', array( 
+		'aside', 
+		'image', 
+		'video', 
+		'quote', 
+		'link' 
+	) );
 
 	// Setup the WordPress core custom background feature.
 	add_theme_support( 'custom-background', apply_filters( '_s_custom_background_args', array(
