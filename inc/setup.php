e2e12f0a0f6c4c12a9528d6f9dc84f1159fe8e9f
Blaine Robison <brobison@elevatoragency.com>
2014-05-27 15:44:18 -0700

    Update vendor folder name for consistency.

--- inc/setup.php
+++ inc/setup.php
@@ -112,7 +112,7 @@ function _s_scripts() {
 
         // Scripts
         wp_enqueue_script( 'jquery' );
-        wp_enqueue_script( 'modernizr', get_template_directory_uri() . '/js/vendor/modernizr-2.6.2.min.js', array(), null );
+        wp_enqueue_script( 'modernizr', get_template_directory_uri() . '/js/plugins/modernizr-2.6.2.min.js', array(), null );
         wp_enqueue_script( 'plugins', get_template_directory_uri() . '/js/min/plugins.min.js', array( 'jquery' ), null, true );
         wp_enqueue_script( 'main', get_template_directory_uri() . '/js/min/main.min.js', array( 'jquery', 'plugins' ), null, true );
 
