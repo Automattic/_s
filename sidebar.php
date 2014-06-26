5106a9e3e4050623979be8e04bf21183a3511458
Konstantin Obenland <obenland@automattic.com>
2014-06-26 13:45:05 -0700

    _s: Use braces in all conditionals.
    Formatting updates for file comment, conditional braces, and indentation.

--- sidebar.php
+++ sidebar.php
@@ -1,13 +1,15 @@
 <?php
 /**
- * The sidebar containing the main widget areas.
+ * The sidebar containing the main widget area.
  *
  * @package _s
  */
 
-if ( ! is_active_sidebar( 'sidebar-1' ) )
+if ( ! is_active_sidebar( 'sidebar-1' ) ) {
 	return;
+}
 ?>
-	<div id="secondary" class="widget-area" role="complementary">
-		<?php dynamic_sidebar( 'sidebar-1' ); ?>
-	</div><!-- #secondary -->
+
+<div id="secondary" class="widget-area" role="complementary">
+	<?php dynamic_sidebar( 'sidebar-1' ); ?>
+</div><!-- #secondary -->
