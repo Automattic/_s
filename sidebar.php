d01a8d319b084dba0fe1acdb5ede275958881056
sixhours <calobee@gmail.com>
2014-06-26 11:06:33 -0400

    is_active_sidebar() check
    Check to ensure sidebar is active before displaying #secondary

--- sidebar.php
+++ sidebar.php
@@ -4,6 +4,9 @@
  *
  * @package _s
  */
+
+if ( ! is_active_sidebar( 'sidebar-1' ) ) :
+	return;
 ?>
 	<div id="secondary" class="widget-area" role="complementary">
 		<?php dynamic_sidebar( 'sidebar-1' ); ?>
