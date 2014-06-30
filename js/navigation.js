74f1149b20954656e5932311344c2cb4505581d3
Christopher Crouch <chrisdc@gmail.com>
2014-06-30 23:19:39 +0100

    Rebase for changes to _menus.scss

--- js/navigation.js
+++ js/navigation.js
@@ -1,10 +1,10 @@
 /**
  * navigation.js
  *
- * Handles toggling the navigation menu for small screens.
+ * Handles toggling the navigation menu for small screens and enabling tab support for dropdown menus.
  */
 ( function() {
-	var container, button, menu;
+	var container, button, menu, links;
 
 	container = document.getElementById( 'site-navigation' );
 	if ( ! container )
@@ -31,4 +31,39 @@
 		else
 			container.className += ' toggled';
 	};
+
+	/**
+	 * Make dropdown menus keyboard accessible.
+	 */
+
+	// Get all the link elements within the menu.
+	links = menu.getElementsByTagName( 'a' );
+
+	// Each time a menu link is focused or blurred call the function toggleFocus.
+	for ( var i = 0, len = links.length; i < len; i++ ) {
+		links[i].onfocus = toggleFocus;
+		links[i].onblur = toggleFocus;
+	}
+
+	function toggleFocus() {
+		var current = this,
+		    ancestors = [];
+
+		// Create an array of <li> ancestors of the current link. Stop upon
+		// reaching .nav-menu at the top of the current menu system.
+		while ( -1 === current.className.indexOf( 'nav-menu' ) ) {
+			if ( 'li' === current.tagName.toLowerCase() ) {
+				ancestors.unshift( current );
+			}
+			current = current.parentElement;
+		}
+
+		// For each element in ancestors[] toggle the class .focus.
+		for ( i = 0, len = ancestors.length; i < len; i++ ) {
+			if ( -1 !== ancestors[i].className.indexOf( 'focus' ) )
+				ancestors[i].className = ancestors[i].className.replace( ' focus', '' );
+			else
+				ancestors[i].className += ' focus';
+		}
+	}
 } )();
