8b9d8a5dee1be9747a184f0df3f6bea5e4198d98
Tammie Lister <karmatosed@gmail.com>
2014-08-13 12:05:06 +0100

    Adds enqueue style

--- functions.php
+++ functions.php
@@ -94,6 +94,8 @@ add_action( 'widgets_init', '_s_widgets_init' );
 function _s_scripts() {
 	wp_enqueue_style( '_s-style', get_stylesheet_uri() );
 
+	wp_enqueue_style( '_s-genericons', get_template_directory_uri() . '/genericons/genericons.css', array(), '20141308', true );
+
 	wp_enqueue_script( '_s-navigation', get_template_directory_uri() . '/js/navigation.js', array(), '20120206', true );
 
 	wp_enqueue_script( '_s-skip-link-focus-fix', get_template_directory_uri() . '/js/skip-link-focus-fix.js', array(), '20130115', true );
