33fbe4f0f4118e2ce40699c274f9703a3ccabce1
Nicola Mustone <mustone.nicola@gmail.com>
2014-08-18 18:46:25 +0200

    Using <aside>

--- sidebar.php
+++ sidebar.php
@@ -10,6 +10,6 @@ if ( ! is_active_sidebar( 'sidebar-1' ) ) {
 }
 ?>
 
-<div id="secondary" class="widget-area" role="complementary">
+<aside id="secondary" class="widget-area" role="complementary">
 	<?php dynamic_sidebar( 'sidebar-1' ); ?>
-</div><!-- #secondary -->
+</aside><!-- #secondary -->
