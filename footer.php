f3a8e5a82f97c75f406cb365bde3b18ebef3f9d7
William H <william@wpcreated.com>
2014-08-24 12:04:29 -0500

    Showing actual tag
    for clarity and consistency with header.php

--- footer.php
+++ footer.php
@@ -2,7 +2,7 @@
 /**
  * The template for displaying the footer.
  *
- * Contains the closing of the #content div and all content after
+ * Contains the closing of <div id="content"> and all content after
  *
  * @package _s
  */
