831d5eb06256bf5bdf37c7173fac1d4c23673cb9
Greg Rickaby <gregrickaby@gmail.com>
2014-10-06 10:05:31 -0500

    bring current with Automattic/_s

--- content-page.php
+++ content-page.php
@@ -24,5 +24,4 @@
 	<footer class="entry-footer">
 		<?php edit_post_link( __( 'Edit', '_s' ), '<span class="edit-link">', '</span>' ); ?>
 	</footer><!-- .entry-footer -->
-
 </article><!-- #post-## -->
--- inc/template-tags.php
+++ inc/template-tags.php
@@ -10,8 +10,6 @@
 if ( ! function_exists( '_s_paging_nav' ) ) :
 /**
  * Display navigation to next/previous set of posts when applicable.
- *
- * @return void
  */
 function _s_paging_nav() {
 	// Don't print empty markup if there's only one page.
@@ -40,8 +38,6 @@ endif;
 if ( ! function_exists( '_s_post_nav' ) ) :
 /**
  * Display navigation to next/previous post when applicable.
- *
- * @return void
  */
 function _s_post_nav() {
 	// Don't print empty markup if there's nowhere to navigate.
@@ -70,9 +66,9 @@ if ( ! function_exists( '_s_posted_on' ) ) :
  * Prints HTML with meta information for the current post-date/time and author.
  */
 function _s_posted_on() {
-	$time_string = '<time class="entry-date published" datetime="%1$s">%2$s</time>';
+	$time_string = '<time class="entry-date published updated" datetime="%1$s">%2$s</time>';
 	if ( get_the_time( 'U' ) !== get_the_modified_time( 'U' ) ) {
-		$time_string .= '<time class="updated" datetime="%3$s">%4$s</time>';
+		$time_string = '<time class="entry-date published" datetime="%1$s">%2$s</time><time class="updated" datetime="%3$s">%4$s</time>';
 	}
 
 	$time_string = sprintf( $time_string,
@@ -97,23 +93,59 @@ function _s_posted_on() {
 }
 endif;
 
+if ( ! function_exists( '_s_entry_footer' ) ) :
+/**
+ * Prints HTML with meta information for the categories, tags and comments.
+ */
+function _s_entry_footer() {
+	// Hide category and tag text for pages.
+	if ( 'post' == get_post_type() ) {
+		/* translators: used between list items, there is a space after the comma */
+		$categories_list = get_the_category_list( __( ', ', '_s' ) );
+		if ( $categories_list && _s_categorized_blog() ) {
+			printf( '<span class="cat-links">' . __( 'Posted in %1$s', '_s' ) . '</span>', $categories_list );
+		}
+
+		/* translators: used between list items, there is a space after the comma */
+		$tags_list = get_the_tag_list( '', __( ', ', '_s' ) );
+		if ( $tags_list ) {
+			printf( '<span class="tags-links">' . __( 'Tagged %1$s', '_s' ) . '</span>', $tags_list );
+		}
+	}
+
+	if ( ! is_single() && ! post_password_required() && ( comments_open() || get_comments_number() ) ) {
+		echo '<span class="comments-link">';
+		comments_popup_link( __( 'Leave a comment', '_s' ), __( '1 Comment', '_s' ), __( '% Comments', '_s' ) );
+		echo '</span>';
+	}
+
+	edit_post_link( __( 'Edit', '_s' ), '<span class="edit-link">', '</span>' );
+}
+endif;
+
 /**
  * Returns true if a blog has more than 1 category.
+ *
+ * @return bool
  */
 function _s_categorized_blog() {
-	if ( false === ( $all_the_cool_cats = get_transient( 'all_the_cool_cats' ) ) ) {
+	if ( false === ( $all_the_cool_cats = get_transient( '_s_categories' ) ) ) {
 		// Create an array of all the categories that are attached to posts.
 		$all_the_cool_cats = get_categories( array(
+			'fields'     => 'ids',
 			'hide_empty' => 1,
+
+			// We only need to know if there is more than one category.
+			'number'     => 2,
 		) );
 
 		// Count the number of categories that are attached to the posts.
 		$all_the_cool_cats = count( $all_the_cool_cats );
 
-		set_transient( 'all_the_cool_cats', $all_the_cool_cats );
+		set_transient( '_s_categories', $all_the_cool_cats );
 	}
 
-	if ( '1' != $all_the_cool_cats ) {
+	if ( $all_the_cool_cats > 1 ) {
 		// This blog has more than 1 category so _s_categorized_blog should return true.
 		return true;
 	} else {
@@ -127,7 +159,7 @@ function _s_categorized_blog() {
  */
 function _s_category_transient_flusher() {
 	// Like, beat it. Dig?
-	delete_transient( 'all_the_cool_cats' );
+	delete_transient( '_s_categories' );
 }
 add_action( 'edit_category', '_s_category_transient_flusher' );
 add_action( 'save_post',     '_s_category_transient_flusher' );
