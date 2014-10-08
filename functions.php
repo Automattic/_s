348886e168f894cfb9e15b0e02b0c67876b6a667
Kirk Wight <kwight@kwight.ca>
2014-10-07 18:58:25 -0700

    Comment correction: "setup" is a noun, "set up" is the action.

--- functions.php
+++ functions.php
@@ -61,7 +61,7 @@ function _s_setup() {
 		'aside', 'image', 'video', 'quote', 'link',
 	) );
 
-	// Setup the WordPress core custom background feature.
+	// Set up the WordPress core custom background feature.
 	add_theme_support( 'custom-background', apply_filters( '_s_custom_background_args', array(
 		'default-color' => 'ffffff',
 		'default-image' => '',
