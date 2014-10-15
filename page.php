b9ebebb28bd808704b385b90e56b1da7a4875145
Sean Davis <sean@sdavismedia.com>
2014-10-14 19:57:29 -0500

    remove unnecessary comparison

--- page.php
+++ page.php
@@ -21,7 +21,7 @@ get_header(); ?>
 
 				<?php
 					// If comments are open or we have at least one comment, load up the comment template
-					if ( comments_open() || '0' != get_comments_number() ) :
+					if ( comments_open() || get_comments_number() ) :
 						comments_template();
 					endif;
 				?>
