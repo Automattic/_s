53d59744bff4449831505b9d42c7793931ce0a98
leopuleo <leonardo.giacone@gmail.com>
2014-10-31 18:24:39 +0100

    New file organization as discussed in #619 + Jetpack Infinite scroll Jetpack suggested by @grappler

--- inc/jetpack.php
+++ inc/jetpack.php
@@ -13,7 +13,15 @@
 function _s_jetpack_setup() {
 	add_theme_support( 'infinite-scroll', array(
 		'container' => 'main',
-		'footer'    => 'page',
+		'render'     => '_s_infinite_scroll_render',
+		'footer'      => 'page',
 	) );
 }
+
+/**
+ * Add Infinite Scroll compatibility after content.php has been moved in template-parts folder.
+ */
 add_action( 'after_setup_theme', '_s_jetpack_setup' );
+function _s_infinite_scroll_render() {
+	get_template_part( 'template-parts/content', get_post_format() );
+}
