c97ebed03b4e424b66185e438ae853375aaedcc1
Nick Daugherty <ndaugherty987@gmail.com>
2014-11-13 17:37:27 -0700

    Suffix variables containing html for better readability
    It helps anyone looking at the code to understand that the variable
    contains html and has been pre-escaped

--- inc/template-tags.php
+++ inc/template-tags.php
@@ -71,24 +71,24 @@ function _s_posted_on() {
 		$time_string = '<time class="entry-date published" datetime="%1$s">%2$s</time><time class="updated" datetime="%3$s">%4$s</time>';
 	}
 
-	$time_string = sprintf( $time_string,
+	$time_string_html = sprintf( $time_string,
 		esc_attr( get_the_date( 'c' ) ),
 		esc_html( get_the_date() ),
 		esc_attr( get_the_modified_date( 'c' ) ),
 		esc_html( get_the_modified_date() )
 	);
 
-	$posted_on = sprintf(
+	$posted_on_html = sprintf(
 		_x( 'Posted on %s', 'post date', '_s' ),
-		'<a href="' . esc_url( get_permalink() ) . '" rel="bookmark">' . $time_string . '</a>'
+		'<a href="' . esc_url( get_permalink() ) . '" rel="bookmark">' . $time_string_html . '</a>'
 	);
 
-	$byline = sprintf(
+	$byline_html = sprintf(
 		_x( 'by %s', 'post author', '_s' ),
 		'<span class="author vcard"><a class="url fn n" href="' . esc_url( get_author_posts_url( get_the_author_meta( 'ID' ) ) ) . '">' . esc_html( get_the_author() ) . '</a></span>'
 	);
 
-	echo '<span class="posted-on">' . $posted_on . '</span><span class="byline"> ' . $byline . '</span>';
+	echo '<span class="posted-on">' . $posted_on_html . '</span><span class="byline"> ' . $byline_html . '</span>';
 
 }
 endif;
