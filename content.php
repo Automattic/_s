1e8d4a9f391cd21c347520b9e8a893a108c3962a
Morten Rand-Hendriksen <morten@pinkandyellow.com>
2014-11-24 19:10:11 -0800

    Aims to create a more hierarchical heading structure. Somewhat based on structure in Twenty Fifteen and inspired by http://blog.rrwd.nl/2014/11/21/html5-headings-in-wordpress-lets-fight/ and https://www.joedolson.com/2014/11/headings-hierarchy-problem/

--- content.php
+++ content.php
@@ -6,7 +6,7 @@
 
 <article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
 	<header class="entry-header">
-		<?php the_title( sprintf( '<h1 class="entry-title"><a href="%s" rel="bookmark">', esc_url( get_permalink() ) ), '</a></h1>' ); ?>
+		<?php the_title( sprintf( '<h2 class="entry-title"><a href="%s" rel="bookmark">', esc_url( get_permalink() ) ), '</a></h2>' ); ?>
 
 		<?php if ( 'post' == get_post_type() ) : ?>
 		<div class="entry-meta">
--- header.php
+++ header.php
@@ -24,8 +24,18 @@
 
 	<header id="masthead" class="site-header" role="banner">
 		<div class="site-branding">
-			<h1 class="site-title"><a href="<?php echo esc_url( home_url( '/' ) ); ?>" rel="home"><?php bloginfo( 'name' ); ?></a></h1>
-			<h2 class="site-description"><?php bloginfo( 'description' ); ?></h2>
+			<?php
+                            if ( is_front_page() && is_home() ) : ?>
+				<h1 class="site-title"><a href="<?php echo esc_url( home_url( '/' ) ); ?>" rel="home"><?php bloginfo( 'name' ); ?></a></h1>
+                            <?php else : ?>
+                                <div class="site-title"><a href="<?php echo esc_url( home_url( '/' ) ); ?>" rel="home"><?php bloginfo( 'name' ); ?></a></div>
+                            <?php endif; 
+                            
+                            $description = get_bloginfo( 'description' );
+                            if ( ! empty( $description ) ) : ?>
+                                <div class="site-description"><?php echo esc_html( $description ); ?></div>
+                            <?php endif;
+                        ?>
 		</div><!-- .site-branding -->
 
 		<nav id="site-navigation" class="main-navigation" role="navigation">
