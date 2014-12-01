93b47c1ab39b322a899b5d625c8d16ec3c20e7ab
Greg Rickaby <gregrickaby@gmail.com>
2014-12-01 11:40:52 -0600

    remove support for IE8

--- inc/scripts.php
+++ inc/scripts.php
@@ -51,34 +51,10 @@ function _s_scripts() {
 	wp_enqueue_style( '_s-google-font', _s_font_url(), array(), null );
 	wp_enqueue_style( '_s-style', get_stylesheet_directory_uri() . '/style' . $minnified . '.css' );
 
-	wp_enqueue_script( '_s-project', get_template_directory_uri() . '/js/project' . $minnified . '.js', array('jquery'), $version, true );
+	wp_enqueue_script( '_s-project', get_template_directory_uri() . '/js/project' . $minnified . '.js', array( 'jquery' ), $version, true );
 
 	if ( is_singular() && comments_open() && get_option( 'thread_comments' ) ) {
 		wp_enqueue_script( 'comment-reply' );
 	}
-
-	/**
-	 * WebDevStudios still supports IE8. Enqueue files to help make our job easier.
-	 */
-	if ( $is_IE ) {
-
-		$GLOBALS['wp_scripts']->add_data( 'html5shiv', 'conditional', 'lte IE 9' );
-		wp_enqueue_script( 'html5shiv', '//cdnjs.cloudflare.com/ajax/libs/html5shiv/3.7/html5shiv.min.js', null, $version, false );
-
-		$GLOBALS['wp_scripts']->add_data( 'respond', 'conditional', 'lte IE 9' );
-		wp_enqueue_script( 'respond', '//cdnjs.cloudflare.com/ajax/libs/respond.js/1.4.2/respond.js', null, $version, false );
-
-		$GLOBALS['wp_scripts']->add_data( 'nwmatcher', 'conditional', 'lte IE 9' );
-		wp_enqueue_script( 'nwmatcher', '//cdnjs.cloudflare.com/ajax/libs/nwmatcher/1.2.5/nwmatcher.min.js', null, $version, false );
-
-		$GLOBALS['wp_scripts']->add_data( 'selectivizr', 'conditional', 'lte IE 9' );
-		wp_enqueue_script( 'selectivizr','//cdnjs.cloudflare.com/ajax/libs/selectivizr/1.0.2/selectivizr-min.js', null, $version, false );
-
-		$GLOBALS['wp_scripts']->add_data( 'placeholder', 'conditional', 'lte IE 9' );
-		wp_enqueue_script( 'placeholder', '//cdnjs.cloudflare.com/ajax/libs/jquery-placeholder/2.0.7/jquery.placeholder.min.js', null, $version, false );
-
-	}
-
-
 }
 add_action( 'wp_enqueue_scripts', '_s_scripts' );
