008a89971ad8b85e5c10401e0c0e038f0156d13b
miya0001 <miya@wpist.me>
2014-12-12 03:03:38 +0900

    fix error with travis

--- inc/template-tags.php
+++ inc/template-tags.php
@@ -53,7 +53,7 @@ function _s_post_nav() {
 		<div class="nav-links">
 			<?php
 				previous_post_link( '<div class="nav-previous">%link</div>', _x( '<span class="meta-nav">&larr;</span>&nbsp;%title', 'Previous post link', '_s' ) );
-				next_post_link(     '<div class="nav-next">%link</div>',     _x( '%title&nbsp;<span class="meta-nav">&rarr;</span>', 'Next post link',     '_s' ) );
+				next_post_link( '<div class="nav-next">%link</div>', _x( '%title&nbsp;<span class="meta-nav">&rarr;</span>', 'Next post link', '_s' ) );
 			?>
 		</div><!-- .nav-links -->
 	</nav><!-- .navigation -->
