204654ac1ffd066e765ef0cc7802d5e3c8eb5b74
sixhours <calobee@gmail.com>
2014-12-17 15:09:44 -0500

    Revert previous (unintentional) commit

--- js/navigation.js
+++ js/navigation.js
@@ -41,35 +41,4 @@
 			menu.setAttribute( 'aria-expanded', 'true' );
 		}
 	};
-	
-	// Fix child menus for touch devices.
-	function fixMenuTouchTaps( container ) {
-		var touchStartFn,
-		    parentLink = container.querySelectorAll( '.menu-item-has-children > a, .page_item_has_children > a' );
-
-		if ( 'ontouchstart' in window ) {
-			touchStartFn = function( e ) {
-				var menuItem = this.parentNode;
-
-				if ( ! menuItem.classList.contains( 'focus' ) ) {
-					e.preventDefault();
-					for( var i = 0; i < menuItem.parentNode.children.length; ++i ) {
-						if ( menuItem === menuItem.parentNode.children[i] ) {
-							continue;
-						}
-						menuItem.parentNode.children[i].classList.remove( 'focus' );
-					}
-					menuItem.classList.add( 'focus' );
-				} else {
-					menuItem.classList.remove( 'focus' );
-				}
-			};
-
-			for ( var i = 0; i < parentLink.length; ++i ) {
-				parentLink[i].addEventListener( 'touchstart', touchStartFn, false )
-			}
-		}
-	}
-
-	fixMenuTouchTaps( container );
 } )();
