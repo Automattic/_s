c4f6755f61db38949d55ef7706adbc108f6acc3d
Fränk Klein <contact@fklein.info>
2014-12-30 14:53:42 +0100

    Remove home link from wp_page_menu() args.

--- inc/extras.php
+++ inc/extras.php
@@ -8,18 +8,6 @@
  */
 
 /**
- * Get our wp_nav_menu() fallback, wp_page_menu(), to show a home link.
- *
- * @param array $args Configuration arguments.
- * @return array
- */
-function _s_page_menu_args( $args ) {
-	$args['show_home'] = true;
-	return $args;
-}
-add_filter( 'wp_page_menu_args', '_s_page_menu_args' );
-
-/**
  * Adds custom classes to the array of body classes.
  *
  * @param array $classes Classes for the body element.
