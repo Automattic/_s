785d4190cf0bb045e2b2997b2130b50b087b3b3f
Blaine Robison <brobison@elevatoragency.com>
2015-01-09 12:23:16 -0800

    Better parity with "_s" setup function.

--- inc/setup.php
+++ inc/setup.php
@@ -24,15 +24,32 @@ if ( ! function_exists( '_s_setup' ) ) :
  */
 function _s_setup() {
 
-    /**
-     * Add default posts and comments RSS feed links to head
+    /*
+     * Make theme available for translation.
+     * Translations can be filed in the /languages/ directory.
+     * If you're building a theme based on _s, use a find and replace
+     * to change '_s' to the name of your theme in all the template files
      */
+    load_theme_textdomain( '_s', get_template_directory() . '/languages' );
+
+    // Add default posts and comments RSS feed links to head.
     add_theme_support( 'automatic-feed-links' );
 
-    /**
-     * Add html5 elements
+    /*
+     * Let WordPress manage the document title.
+     * By adding theme support, we declare that this theme does not use a
+     * hard-coded <title> tag in the document head, and expect WordPress to
+     * provide it for us.
      */
-    add_theme_support( 'html5', array( 'comment-list', 'search-form', 'comment-form', ) );
+    add_theme_support( 'title-tag' );
+
+    /*
+     * Switch default core markup for search form, comment form, and comments
+     * to output valid HTML5.
+     */
+    add_theme_support( 'html5', array(
+        'search-form', 'comment-form', 'comment-list', 'gallery', 'caption',
+    ) );
 
     /**
      * Enable support for Post Thumbnails on posts and pages
@@ -75,7 +92,9 @@ add_action( 'after_setup_theme', '_s_setup' );
 
 
 /**
- * Register widgetized area and update sidebar with default widgets
+ * Register widget area.
+ *
+ * @link http://codex.wordpress.org/Function_Reference/register_sidebar
  */
 function _s_widgets_init() {
     register_sidebar( array(
