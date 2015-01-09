c48207293202a2f70934a2249d75d45515400645
Mignon Style <mignonxstyle@gmail.com>
2015-01-09 17:09:22 +0900

    Reflect the change of color in the customizer.
    Fixed ".site-title" to ".site-title a".

--- js/customizer.js
+++ js/customizer.js
@@ -20,12 +20,12 @@
 	wp.customize( 'header_textcolor', function( value ) {
 		value.bind( function( to ) {
 			if ( 'blank' === to ) {
-				$( '.site-title, .site-description' ).css( {
+				$( '.site-title a, .site-description' ).css( {
 					'clip': 'rect(1px, 1px, 1px, 1px)',
 					'position': 'absolute'
 				} );
 			} else {
-				$( '.site-title, .site-description' ).css( {
+				$( '.site-title a, .site-description' ).css( {
 					'clip': 'auto',
 					'color': to,
 					'position': 'relative'
