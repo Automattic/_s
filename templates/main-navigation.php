d24eece2ff2d085a90c62770aa920112619f35e8
Steven Slack <steven@s2webpress.com>
2015-01-15 12:02:13 -0500

    button styles sassified

--- templates/main-navigation.php
+++ templates/main-navigation.php
@@ -1,13 +1,12 @@
 		<button class="nav-toggle" data-nav-toggle="#nav-primary" href="#">Menu &#9660;</button>
 
-        <nav id="menu" class="nav" role="navigation" itemscope="itemscope" itemtype="http://schema.org/SiteNavigationElement">
-            <?php wp_nav_menu( 
+		<nav id="menu" class="nav" role="navigation" itemscope="itemscope" itemtype="http://schema.org/SiteNavigationElement">
+			<?php wp_nav_menu( 
 				array( 
 					'theme_location'	=> 'primary',
 					'container'			=> false,
-					'menu_class'		=> 'nav-collapse',
-					'menu_id'         	=> 'nav-primary',
-					"walker"            => new Drop_Menu_Walker(),
+					'menu_id'			=> 'nav-primary',
+					"walker"			=> new Drop_Menu_Walker(),
 				)
 			); ?>
         </nav>
