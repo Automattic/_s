d35c65d0a4686daad4f242ff1a6c7e1c2822aec8
Blaine Robison <brobison@elevatoragency.com>
2015-01-19 13:20:19 -0800

    Remove recent comment inline styling.

--- inc/setup.php
+++ inc/setup.php
@@ -214,3 +214,19 @@ function _s_modify_main_query( $query ) {
     // }
 }
 add_action( 'pre_get_posts', '_s_modify_main_query', 1 );
+
+
+
+
+
+/**
+ * Remove inline styling for recent comments widget
+ */
+function _s_remove_recent_comments_style() {
+
+        global $wp_widget_factory;
+
+        remove_action( 'wp_head', array( $wp_widget_factory->widgets['WP_Widget_Recent_Comments'], 'recent_comments_style' ) );
+
+    }
+add_action( 'widgets_init', '_s_remove_recent_comments_style' );
