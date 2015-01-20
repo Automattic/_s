6a9e9620eab16927c061d825d218fe3db81ec9c9
Blaine Robison <brobison@elevatoragency.com>
2015-01-20 15:05:24 -0800

    BEM class naming for WordPress widgets.

--- inc/setup.php
+++ inc/setup.php
@@ -97,14 +97,16 @@ add_action( 'after_setup_theme', '_s_setup' );
  * @link http://codex.wordpress.org/Function_Reference/register_sidebar
  */
 function _s_widgets_init() {
+
     register_sidebar( array(
         'name'          => __( 'Sidebar', '_s' ),
         'id'            => 'sidebar-1',
         'before_widget' => '<aside id="%1$s" class="widget %2$s">',
         'after_widget'  => '</aside>',
-        'before_title'  => '<h1 class="widget-title">',
-        'after_title'   => '</h1>',
+        'before_title'  => '<h3 class="widget__heading">',
+        'after_title'   => '</h3>',
     ) );
+
 }
 add_action( 'widgets_init', '_s_widgets_init' );
 
