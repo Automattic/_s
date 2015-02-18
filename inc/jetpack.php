4d9c2f6e8d8e6c40012cfc9069bf01c0c838d9c1
chris barnes <mail@cjbarnes.co.uk>
2015-02-18 14:46:47 +0000

    Removed Infinite Scroll support

--- inc/jetpack.php
+++ inc/jetpack.php
@@ -5,15 +5,3 @@
  *
  * @package YuMag
  */
-
-/**
- * Add theme support for Infinite Scroll.
- * See: http://jetpack.me/support/infinite-scroll/
- */
-function yumag_jetpack_setup() {
-	add_theme_support( 'infinite-scroll', array(
-		'container' => 'main',
-		'footer'    => 'page',
-	) );
-}
-add_action( 'after_setup_theme', 'yumag_jetpack_setup' );
