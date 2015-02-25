33fd6cd61a00827d73b9b275e38e2ec1b4103d3d
chris barnes <mail@cjbarnes.co.uk>
2015-02-25 16:18:12 +0000

    Some PHP code style changes for consistency

--- inc/extras.php
+++ inc/extras.php
@@ -24,48 +24,48 @@ function yumag_body_classes( $classes ) {
 add_filter( 'body_class', 'yumag_body_classes' );
 
 if ( version_compare( $GLOBALS['wp_version'], '4.1', '<' ) ) :
-	/**
-	 * Filters wp_title to print a neat <title> tag based on what is being viewed.
-	 *
-	 * @param string $title Default title text for current view.
-	 * @param string $sep Optional separator.
-	 * @return string The filtered title.
-	 */
-	function yumag_wp_title( $title, $sep ) {
-		if ( is_feed() ) {
-			return $title;
-		}
-
-		global $page, $paged;
-
-		// Add the blog name
-		$title .= get_bloginfo( 'name', 'display' );
-
-		// Add the blog description for the home/front page.
-		$site_description = get_bloginfo( 'description', 'display' );
-		if ( $site_description && ( is_home() || is_front_page() ) ) {
-			$title .= " $sep $site_description";
-		}
-
-		// Add a page number if necessary:
-		if ( ( $paged >= 2 || $page >= 2 ) && ! is_404() ) {
-			$title .= " $sep " . sprintf( __( 'Page %s', 'yumag' ), max( $paged, $page ) );
-		}
-
+/**
+ * Filters wp_title to print a neat <title> tag based on what is being viewed.
+ *
+ * @param string $title Default title text for current view.
+ * @param string $sep Optional separator.
+ * @return string The filtered title.
+ */
+function yumag_wp_title( $title, $sep ) {
+	if ( is_feed() ) {
 		return $title;
 	}
-	add_filter( 'wp_title', 'yumag_wp_title', 10, 2 );
 
-	/**
-	 * Title shim for sites older than WordPress 4.1.
-	 *
-	 * @link https://make.wordpress.org/core/2014/10/29/title-tags-in-4-1/
-	 * @todo Remove this function when WordPress 4.3 is released.
-	 */
-	function yumag_render_title() {
-		?>
-		<title><?php wp_title( '|', true, 'right' ); ?></title>
-		<?php
+	global $page, $paged;
+
+	// Add the blog name
+	$title .= get_bloginfo( 'name', 'display' );
+
+	// Add the blog description for the home/front page.
+	$site_description = get_bloginfo( 'description', 'display' );
+	if ( $site_description && ( is_home() || is_front_page() ) ) {
+		$title .= " $sep $site_description";
 	}
-	add_action( 'wp_head', 'yumag_render_title' );
+
+	// Add a page number if necessary:
+	if ( ( $paged >= 2 || $page >= 2 ) && ! is_404() ) {
+		$title .= " $sep " . sprintf( __( 'Page %s', 'yumag' ), max( $paged, $page ) );
+	}
+
+	return $title;
+}
+add_filter( 'wp_title', 'yumag_wp_title', 10, 2 );
+
+/**
+ * Title shim for sites older than WordPress 4.1.
+ *
+ * @link https://make.wordpress.org/core/2014/10/29/title-tags-in-4-1/
+ * @todo Remove this function when WordPress 4.3 is released.
+ */
+function yumag_render_title() {
+	?>
+	<title><?php wp_title( '|', true, 'right' ); ?></title>
+	<?php
+}
+add_action( 'wp_head', 'yumag_render_title' );
 endif;
--- inc/template-tags.php
+++ inc/template-tags.php
@@ -134,5 +134,5 @@ function yumag_typekit_script() {
 	<script>(function(d) {var tkTimeout=<?php echo $script_timeout; ?>;if(window.sessionStorage){if(sessionStorage.getItem('useTypekit')==='false'){tkTimeout=0;}}var config = {kitId: '<?php echo $kit_id; ?>',scriptTimeout: tkTimeout},h=d.documentElement,t=setTimeout(function(){h.className=h.className.replace(/\bwf-loading\b/g,"")+"wf-inactive";if(window.sessionStorage){sessionStorage.setItem("useTypekit","false")}},config.scriptTimeout),tk=d.createElement("script"),f=false,s=d.getElementsByTagName("script")[0],a;h.className+="wf-loading";tk.src='//use.typekit.net/'+config.kitId+'.js';tk.async=true;tk.onload=tk.onreadystatechange=function(){a=this.readyState;if(f||a&&a!="complete"&&a!="loaded")return;f=true;clearTimeout(t);try{Typekit.load(config)}catch(e){}};s.parentNode.insertBefore(tk,s)})(document);</script>
 	<?
 }
-endif;
 add_action( 'wp_head', 'yumag_typekit_script' );
+endif;
