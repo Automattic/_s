0fd0757fe506fbb0acc34a0078f5afb9bde909d0
chris barnes <mail@cjbarnes.co.uk>
2015-02-25 13:08:53 +0000

    Use H2s for widgets, not H1s

--- functions.php
+++ functions.php
@@ -90,8 +90,8 @@ function yumag_widgets_init() {
 		'description'   => '',
 		'before_widget' => '<aside id="%1$s" class="widget %2$s">',
 		'after_widget'  => '</aside>',
-		'before_title'  => '<h1 class="widget-title">',
-		'after_title'   => '</h1>',
+		'before_title'  => '<h2 class="widget-title">',
+		'after_title'   => '</h2>',
 	) );
 }
 add_action( 'widgets_init', 'yumag_widgets_init' );
