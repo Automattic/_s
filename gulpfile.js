4c24bb3698c5847ca929862e1a74465b6d775575
chris barnes <mail@cjbarnes.co.uk>
2015-03-03 15:26:46 +0000

    Better code style for build script

--- gulpfile.js
+++ gulpfile.js
@@ -1,29 +1,29 @@
 /* jshint node: true */
 'use strict';
 
-var gulp = require('gulp');
-var autoprefixer = require('gulp-autoprefixer');
-var csscomb      = require('gulp-csscomb');
-var notify       = require('gulp-notify');
-var plumber      = require('gulp-plumber');
-var sass         = require('gulp-sass');
-var sourcemaps   = require('gulp-sourcemaps');
+var gulp         = require( 'gulp' );
+var autoprefixer = require( 'gulp-autoprefixer' );
+var csscomb      = require( 'gulp-csscomb' );
+var notify       = require( 'gulp-notify' );
+var plumber      = require( 'gulp-plumber' );
+var sass         = require( 'gulp-sass' );
+var sourcemaps   = require( 'gulp-sourcemaps' );
 
 // Compile Sass.
-gulp.task('styles', function() {
+gulp.task( 'styles', function() {
 
-	return gulp.src('sass/*.scss')
+	return gulp.src( 'sass/*.scss' )
 		// Error handling: notify on error.
-		.pipe(plumber(notify.onError({
+		.pipe( plumber( notify.onError( {
 			title: 'Error compiling yumag styles',
 			message: 'Styles Error: <%= error.message %> in <%=error.fileName %> (line <%=error.lineNumber %>)',
 			sound: 'Pop'
-		})))
-		.pipe(sourcemaps.init())
+		} ) ) )
+		.pipe( sourcemaps.init() )
 		// Compile Sass.
-		.pipe(sass())
+		.pipe( sass() )
 		// Automated support for old browsers.
-		.pipe(autoprefixer({
+		.pipe( autoprefixer( {
 			browsers: [
 				// Matches core's Autoprefixer settings.
 				'Android >= 2.1',
@@ -33,21 +33,21 @@ gulp.task('styles', function() {
 				'Opera >= 12.1',
 				'Safari >= 6.0'
 			]
-		}))
+		} ) )
 		// Enforce the preferred CSS property order and format.
-		.pipe(csscomb())
+		.pipe( csscomb() )
 		// Output with sourcemap.
-		.pipe(sourcemaps.write('.'))
-		.pipe(gulp.dest('./'))
-		.pipe(notify({
+		.pipe( sourcemaps.write( '.' ) )
+		.pipe( gulp.dest( './' ) )
+		.pipe( notify( {
 			title: 'Compiled yumag styles successfully',
 			sound: false
-		}));
-});
+		} ) );
+} );
 
-gulp.task('default', ['styles'], function() {
+gulp.task( 'default', ['styles'], function() {
 
 	// Watch Sass files.
-	gulp.watch('sass/**/*', ['styles']);
+	gulp.watch( 'sass/**/*', ['styles'] );
 
-});
+} );
