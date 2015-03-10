701bb139906068e43742eb07ce36bbe1c2d56407
Piero Bellomo <piero.bellomo@viewlab.net>
2015-03-10 12:31:49 +0100

    Improved lt IE 9 support (thanks @anagio)

--- header.php
+++ header.php
@@ -13,6 +13,10 @@
 <meta name="viewport" content="width=device-width, initial-scale=1">
 <link rel="profile" href="http://gmpg.org/xfn/11">
 <link rel="pingback" href="<?php bloginfo( 'pingback_url' ); ?>">
+<!--[if lt IE 9]>
+<script src="<?php echo get_template_directory_uri(); ?>/js/html5.js" type="text/javascript"></script>
+<script src="//cdn.jsdelivr.net/respond/1.4.2/respond.min.js"></script>
+<![endif]-->
 
 <?php wp_head(); ?>
 </head>
