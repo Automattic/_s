264048cbcdaae45dbb01acb32a67de01f4041fb0
chris barnes <mail@cjbarnes.co.uk>
2015-03-10 16:38:30 +0000

    Comments area header

--- comments.php
+++ comments.php
@@ -23,12 +23,14 @@ if ( post_password_required() ) {
 	<?php // You can start editing here -- including this comment! ?>
 
 	<?php if ( have_comments() ) : ?>
-		<h2 class="comments-title">
+		<h3 class="comments-title">
 			<?php
-				printf( _nx( 'One thought on &ldquo;%2$s&rdquo;', '%1$s thoughts on &ldquo;%2$s&rdquo;', get_comments_number(), 'comments title', 'yumag' ),
-					number_format_i18n( get_comments_number() ), '<span>' . get_the_title() . '</span>' );
+			printf(
+				_nx( 'One comment', '%1$s comments', get_comments_number(), 'comments title', 'yumag' ),
+				number_format_i18n( get_comments_number() )
+			);
 			?>
-		</h2>
+		</h3>
 
 		<?php if ( get_comment_pages_count() > 1 && get_option( 'page_comments' ) ) : // are there comments to navigate through ?>
 		<nav id="comment-nav-above" class="comment-navigation" role="navigation">
