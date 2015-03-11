8652ede022e593d58a76d6f775460fe251a9f980
chris barnes <mail@cjbarnes.co.uk>
2015-03-11 16:12:11 +0000

    Removed colophon so entry footer is the last section of the page (except comments, if applicable)

--- footer.php
+++ footer.php
@@ -9,12 +9,6 @@
 ?>
 
 	</div><!-- #content -->
-
-	<footer id="colophon" class="site-footer" role="contentinfo">
-		<div class="site-info">
-			<?php printf( __( 'web design by %1$s', 'yumag' ), '<a href="http://www.cjbarnes.co.uk/" rel="designer">cJ barnes</a>' ); ?>
-		</div><!-- .site-info -->
-	</footer><!-- #colophon -->
 </div><!-- #page -->
 
 <?php wp_footer(); ?>
--- single.php
+++ single.php
@@ -19,8 +19,6 @@ get_header(); ?>
 
 			<?php get_template_part( 'content', 'single' ); ?>
 
-			<?php the_post_navigation(); ?>
-
 			<?php
 				// If comments are open or we have at least one comment, load up the comment template
 				if ( comments_open() || get_comments_number() ) :
