6df896eecf18fc913ec1d4093dd26e234a6ec1b9
chris barnes <mail@cjbarnes.co.uk>
2015-03-18 19:48:58 +0000

    Added separate template for sections (i.e. categories)

---
+++ category.php
@@ -0,0 +1,47 @@
+<?php
+/**
+ * The template for displaying sections.
+ *
+ * Learn more: http://codex.wordpress.org/Template_Hierarchy
+ *
+ * @package YuMag
+ */
+
+get_header(); ?>
+
+	<div id="primary" class="content-area">
+		<main id="main" class="site-main" role="main">
+
+		<?php if ( have_posts() ) : ?>
+
+			<header class="category-header">
+				<h1 class="category-title"><?php single_term_title(); ?></h1>
+				<?php the_archive_description( '<div class="category-description">', '</div>' ); ?>
+			</header><!-- .page-header -->
+
+			<?php /* Start the Loop */ ?>
+			<?php while ( have_posts() ) : the_post(); ?>
+
+				<?php
+					/* Include the Post-Format-specific template for the content.
+					 * If you want to override this in a child theme, then include a file
+					 * called content-___.php (where ___ is the Post Format name) and that will be used instead.
+					 */
+					get_template_part( 'content', get_post_format() );
+				?>
+
+			<?php endwhile; ?>
+
+			<?php the_posts_navigation(); ?>
+
+		<?php else : ?>
+
+			<?php get_template_part( 'content', 'none' ); ?>
+
+		<?php endif; ?>
+
+		</main><!-- #main -->
+	</div><!-- #primary -->
+
+<?php get_sidebar(); ?>
+<?php get_footer(); ?>
