08ae4e9d46f615ca69e44b979f9918e5825a8936
chris barnes <mail@cjbarnes.co.uk>
2015-03-23 13:21:13 +0000

    Search results header layout

--- search.php
+++ search.php
@@ -7,16 +7,21 @@
 
 get_header(); ?>
 
+	<div class="search-results-search">
+		<?php get_search_form(); ?>
+	</div>
+
 	<section id="primary" class="content-area">
+
+		<header class="archive-header search-header">
+			<h1 class="archive-title search-title"><?php printf( __( 'Search results for &ldquo;%s&rdquo;', 'yumag' ), '<span class="search-term">' . get_search_query() . '</span>' ); ?></h1>
+		</header><!-- .archive-header -->
+
 		<main id="main" class="site-main" role="main">
 
 		<?php if ( have_posts() ) : ?>
 
 			<section class="search-section">
-				<header class="page-header search-header">
-					<h1 class="page-title"><?php printf( __( 'Search for &ldquo;%s&rdquo;', 'yumag' ), '<span>' . get_search_query() . '</span>' ); ?></h1>
-				</header><!-- .page-header -->
-
 				<div class="index-content search-contents">
 					<div class="index-posts search-posts">
 						<div>
