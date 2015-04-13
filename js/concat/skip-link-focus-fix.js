6f7d85d65fdf78b465392b52e48a0a7958898248
Stacy Kvernmo <stacy@webdevstudios.com>
2015-04-13 10:29:56 -0500

    bring header and navigation (styles and js)  up to date with _s

--- js/concat/skip-link-focus-fix.js
+++ js/concat/skip-link-focus-fix.js
@@ -8,14 +8,14 @@
 			var id = location.hash.substring( 1 ),
 				element;
 
-			if ( ! /^[A-z0-9_-]+$/.test( id ) ) {
+			if ( ! ( /^[A-z0-9_-]+$/.test( id ) ) ) {
 				return;
 			}
 
 			element = document.getElementById( id );
 
 			if ( element ) {
-				if ( ! /^(?:a|select|input|button|textarea)$/i.test( element.tagName ) ) {
+				if ( ! ( /^(?:a|select|input|button|textarea)$/i.test( element.tagName ) ) ) {
 					element.tabIndex = -1;
 				}
 
