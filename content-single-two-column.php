e8776f6756b4bbc1d6f0343624041c389fd015b6
chris barnes <mail@cjbarnes.co.uk>
2015-04-14 16:41:52 +0100

    Fixed full-width footer on 2-column templates

--- content-single-two-column.php
+++ content-single-two-column.php
@@ -5,25 +5,28 @@
 ?>
 
 <article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
-	<?php yumag_entry_image( "yumag-featured-photo-portrait", false ) ?>
-	<header class="entry-header">
-		<?php yumag_entry_title() ?>
-		<?php yumag_byline(); ?>
-	</header><!-- .entry-header -->
+	<div class="entry-above-footer">
+		<?php yumag_entry_image( "yumag-featured-photo-portrait", false ) ?>
+		<header class="entry-header">
+			<?php yumag_entry_title() ?>
+			<?php yumag_byline(); ?>
+		</header><!-- .entry-header -->
 
-	<div class="entry-content">
-		<?php the_content(); ?>
-		<?php
-			wp_link_pages( array(
-				'before' => '<div class="page-links">' . __( 'Pages:', 'yumag' ),
-				'after'  => '</div>',
-			) );
-		?>
-	</div><!-- .entry-content -->
-
-	<footer class="footer entry-footer">
-		<?php yumag_author_box(); ?>
-		<?php yumag_entry_footer(); ?>
-		<?php yumag_entry_tags(); ?>
-	</footer><!-- .entry-footer -->
+		<div class="entry-content">
+			<?php the_content(); ?>
+			<?php
+				wp_link_pages( array(
+					'before' => '<div class="page-links">' . __( 'Pages:', 'yumag' ),
+					'after'  => '</div>',
+				) );
+			?>
+		</div><!-- .entry-content -->
+	</div><!-- .entry-above-footer -->
+	<div class="footer entry-footer-wrap">
+		<footer class="entry-footer">
+			<?php yumag_author_box(); ?>
+			<?php yumag_entry_footer(); ?>
+			<?php yumag_entry_tags(); ?>
+		</footer><!-- .entry-footer -->
+	</div><!-- .entry-footer-wrap -->
 </article><!-- #post-## -->
