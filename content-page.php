f01b651ea344dfbb5e0c4d906780fa5df2ce9f83
chris barnes <mail@cjbarnes.co.uk>
2015-04-14 16:10:24 +0100

    Fixed full-width content on superwide screens

--- content-page.php
+++ content-page.php
@@ -7,20 +7,19 @@
 ?>
 
 <article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
-	<header class="entry-header">
-		<?php yumag_entry_title() ?>
-	</header><!-- .entry-header -->
+	<div class="entry-above-footer">
+		<header class="entry-header">
+			<?php yumag_entry_title() ?>
+		</header><!-- .entry-header -->
 
-	<div class="entry-content">
-		<?php the_content(); ?>
-		<?php
-			wp_link_pages( array(
-				'before' => '<div class="page-links">' . __( 'Pages:', 'yumag' ),
-				'after'  => '</div>',
-			) );
-		?>
-	</div><!-- .entry-content -->
-
-	<footer class="entry-footer">
-	</footer><!-- .entry-footer -->
+		<div class="entry-content">
+			<?php the_content(); ?>
+			<?php
+				wp_link_pages( array(
+					'before' => '<div class="page-links">' . __( 'Pages:', 'yumag' ),
+					'after'  => '</div>',
+				) );
+			?>
+		</div><!-- .entry-content -->
+	</div><!-- .entry-above-footer -->
 </article><!-- #post-## -->
