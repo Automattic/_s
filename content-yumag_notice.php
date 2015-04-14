3223b53b786e4757e80d479bf928d02f25c7c115
chris barnes <mail@cjbarnes.co.uk>
2015-04-14 15:25:09 +0100

    Added obits styling and layout

--- content-yumag_notice.php
+++ content-yumag_notice.php
@@ -46,21 +46,26 @@ if ( empty( $notice_types ) ) {
 			<?php if ( $notice_title ) : ?>
 				<p class="notice-name"><em><?php echo $notice_title; ?></em></p>
 			<?php endif; ?>
+		<?php elseif ( in_array( 'Obituary', $notice_types ) ) : ?>
+			<p class="obit-label">Sadly passed away.</p>
+			<?php if ( $notice_title ) : ?>
+				<p class="notice-name"><?php echo $notice_title; ?></p>
+			<?php endif; ?>
 		<?php else : ?>
 			<p class="notice-label">Update:</p>
 			<?php if ( $notice_title ) : ?>
 				<p class="notice-name"><?php echo $notice_title; ?></p>
 			<?php endif; ?>
 		<?php endif; ?>
-		<?php
-		// if ( has_post_thumbnail() ) {
-		// 	yumag_entry_image( 'thumbnail', false );
-		// }
-		echo types_render_field( 'submission_content', array() );
-		?>
+		<?php echo types_render_field( 'submission_content', array() ); ?>
 		<?php if ( ! empty( $notice_url ) ) : ?>
 			<p class="notice-url"><a href="<?php echo $notice_url; ?>" title="Link for notice <?php the_ID() ?>">Link</a></p>
 		<?php endif; ?>
+		<?php
+		if ( has_post_thumbnail() ) {
+		 	yumag_entry_image( array( 666, 444 ) );
+		}
+		?>
 	</div><!-- .entry-content -->
 
 	<footer class="entry-footer notice-footer">
