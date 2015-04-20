fcbf8176134518efca4c078defb186c7c00ceca2
chris barnes <mail@cjbarnes.co.uk>
2015-04-20 16:37:32 +0100

    Added web design credit

--- footer.php
+++ footer.php
@@ -19,7 +19,8 @@ $src = get_template_directory_uri() . '/assets/';
 						</a>
 					</div>
 					<p class="site-description"><?php bloginfo( 'description' ); ?></p>
-					<p class="site-credits"><small class="site-editor">Online Editor: <a href="#">Alfie Packham</a></small><br>
+					<p class="site-credits"><small class="site-editor">Online Editor: <a href="<?php echo esc_url( home_url( '/author/alfiepackham/' ) ); ?>">Alfie Packham</a></small><br>
+					<small class="site-editor">Web design by <a href="http://www.cjbarnes.co.uk/">Chris Barnes</a></small><br>
 					<small class="colophon">© 2015<?php if ( 2015 < (int) date( 'Y' ) ) echo '&ndash;' . date( 'Y' ); ?> University of York</small></p>
 				</div>
 			</div><!-- .site-footer-row -->
