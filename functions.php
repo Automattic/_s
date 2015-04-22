8e4cdfbff03b6e294afa4738d196795ecdd3b31f
Josiah Sprague <josiah.sprague@gmail.com>
2015-04-22 11:02:57 -0400

    get fields from Types 'public' group
    rather than having a hard coded list, get items from they Types 'public' group of custom fields

--- functions.php
+++ functions.php
@@ -46,21 +46,25 @@ function customize_menu(){
     add_menu_page( 'API', 'API', 'activate_plugins', 'api', 'api_url', 'dashicons-admin-links', '20.1' );
 }
 
-// Add jungle data to public api response
-function add_custom_meta($data, $post, $context) {
-    if ( ($post['post_type'] == 'release') || ($post['post_type'] == 'lyrics') || ($post['post_type'] == 'post') ) {
+// Add custom fields created by Types plugin to public types_custom_meta key
+function add_types_custom_meta($data, $post, $context) {
+    if (function_exists(types_get_fields_by_group)) {
+        $post_custom_data = get_post_custom( $post['ID'] );
 
-        $all_custom_data = get_post_custom( $post['ID'] );
+        // Get a list of Types custom fields in the "public" group
+        $public_types_fields = types_get_fields_by_group('public');
 
-        // All of the custom fields that we want to copy into the public key
-        $jungle_fields = array("wpcf-format", "wpcf-byline", "wpcf-tracks", "wpcf-artist", "wpcf-release_date", "wpcf-retailers", "wpcf-director", "wpcf-retailer");
-
-        foreach ( $all_custom_data as $key => $value ) {
-            if ( in_array($key, $jungle_fields) ) {
-                $data['jungle_data'][$key] = $value;
+        foreach ( $post_custom_data as $key => $value ) {
+            if ( in_array($key, array_keys($public_types_fields)) ) {
+                $types_custom_meta[$key] = $value;
             }
         }
+
+        if ( !empty($types_custom_meta) ) {
+            $data['types_custom_meta'] = $types_custom_meta;
+        }
     }
+
     return $data;
 }
 
@@ -68,7 +72,7 @@ add_action( 'init', 'register_media_taxonomies' );
 add_action( 'after_setup_theme', 'setup' );
 add_filter( 'login_redirect', 'redirect_to_posts' );
 add_action( 'admin_menu', 'customize_menu', 999 );
-add_filter( 'json_prepare_post', 'add_custom_meta', 10, 3 );
+add_filter( 'json_prepare_post', 'add_types_custom_meta', 10, 3 );
 
 if( class_exists( WpeCommon ) ){
     require 'wpengine.php';
