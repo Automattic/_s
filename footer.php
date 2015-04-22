57fd053289bc4e4e3929a8e03f2ce08367e458bc
chris barnes <mail@cjbarnes.co.uk>
2015-04-22 14:45:43 +0100

    Added University of York logo to footer

--- footer.php
+++ footer.php
@@ -19,9 +19,15 @@ $src = get_template_directory_uri() . '/assets/';
 						</a>
 					</div>
 					<p class="site-description"><?php bloginfo( 'description' ); ?></p>
-					<p class="site-credits"><small class="site-editor">Online Editor: <a href="<?php echo esc_url( home_url( '/author/alfiepackham/' ) ); ?>">Alfie Packham</a></small><br>
+					<p class="site-credits">
+					<small class="site-editor">Online Editor: <a href="<?php echo esc_url( home_url( '/author/alfiepackham/' ) ); ?>">Alfie Packham</a></small><br>
 					<small class="site-editor">Web design by <a href="http://www.cjbarnes.co.uk/">Chris Barnes</a></small><br>
 					<small class="colophon">© 2015<?php if ( 2015 < (int) date( 'Y' ) ) echo '&ndash;' . date( 'Y' ); ?> University of York</small></p>
+					<div class="footer-uoy-logo">
+						<a class="site-logo-link" href="//www.york.ac.uk">
+							<img src="<?php echo $src; ?>uoy.png" srcset="<?php echo $src; ?>uoy@2x.png 2x, <?php echo $src; ?>uoy.png 1x" alt="University of York" width="250" height="38">
+						</a>
+					</div>
 				</div>
 			</div><!-- .site-footer-row -->
 		</div><!-- .site-footer -->
