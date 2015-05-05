ef1a884b6ca70cebe5b512bc49897b322f14dab8
Fränk Klein <contact@fklein.info>
2015-05-05 11:59:02 +0200

    Escape translation in title shim.
    See #737.

--- inc/extras.php
+++ inc/extras.php
@@ -49,7 +49,7 @@ if ( version_compare( $GLOBALS['wp_version'], '4.1', '<' ) ) :
 
 		// Add a page number if necessary:
 		if ( ( $paged >= 2 || $page >= 2 ) && ! is_404() ) {
-			$title .= " $sep " . sprintf( __( 'Page %s', '_s' ), max( $paged, $page ) );
+			$title .= " $sep " . sprintf( esc_html__( 'Page %s', '_s' ), max( $paged, $page ) );
 		}
 
 		return $title;
