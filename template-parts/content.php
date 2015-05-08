6094916a24460e1fb0c950f13f95e4e11875aea5
Andrew Saint <andrewsaint@Andrews-iMac.default>
2015-05-08 10:46:27 +0100

    Allow the class attribute to pass through kses

--- template-parts/content.php
+++ template-parts/content.php
@@ -19,7 +19,7 @@
 		<?php
 			/* translators: %s: Name of current post */
 			the_content( sprintf(
-				wp_kses( __( 'Continue reading %s <span class="meta-nav">&rarr;</span>', '_s' ), array( 'span' => array() ) ),
+				wp_kses( __( 'Continue reading %s <span class="meta-nav">&rarr;</span>', '_s' ), array( 'span' => array( 'class' => array() ) ) ),
 				the_title( '<span class="screen-reader-text">"', '"</span>', false )
 			) );
 		?>
