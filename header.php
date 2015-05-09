c4ed66e75569772cf099a68b615e75db9641b35a
David Shanske <david@shanske.com>
2015-05-09 10:02:48 -0400

    Apply Hfeed to Singular Posts Only
    Tabs Fixed to Coding Standards
    
    Fix Formatting of Conditional

--- header.php
+++ header.php
@@ -18,7 +18,7 @@
 </head>
 
 <body <?php body_class(); ?>>
-<div id="page" class="hfeed site">
+<div id="page" class="site">
 	<a class="skip-link screen-reader-text" href="#content"><?php esc_html_e( 'Skip to content', '_s' ); ?></a>
 
 	<header id="masthead" class="site-header" role="banner">
