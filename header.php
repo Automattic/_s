722c23717bf3b93ef54318382b8a8a42e5f379a4
Steven Slack <steven@s2webpress.com>
2015-05-15 13:02:52 -0400

    removed svg4everybody

--- header.php
+++ header.php
@@ -12,10 +12,12 @@
 <head>
 <meta charset="<?php bloginfo( 'charset' ); ?>">
 <meta name="viewport" content="width=device-width, initial-scale=1">
+<meta http-equiv="X-UA-Compatible" content="IE=Edge">
 <link rel="pingback" href="<?php bloginfo( 'pingback_url' ); ?>">
 
 <!--[if lt IE 9]>
 <script src="<?php echo get_template_directory_uri(); ?>/js/html5.js" type="text/javascript"></script>
+<script src="<?php echo get_template_directory_uri(); ?>/js/svg4everybody.ie8.min.js"></script>
 <![endif]-->
 <?php wp_head(); ?>
 </head>
