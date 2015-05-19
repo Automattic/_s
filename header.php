aab7ed6e12430b3a937fb4ce4373180c7e689c9b
Steven Slack <steven@s2webpress.com>
2015-05-18 23:58:05 -0400

    Correct path for html5shiv

--- header.php
+++ header.php
@@ -16,8 +16,7 @@
 <link rel="pingback" href="<?php bloginfo( 'pingback_url' ); ?>">
 
 <!--[if lt IE 9]>
-<script src="<?php echo get_template_directory_uri(); ?>/js/html5.js" type="text/javascript"></script>
-<script src="<?php echo get_template_directory_uri(); ?>/js/svg4everybody.ie8.min.js"></script>
+<script src="<?php echo get_template_directory_uri(); ?>/assets/js/html5.js" type="text/javascript"></script>
 <![endif]-->
 <?php wp_head(); ?>
 </head>
