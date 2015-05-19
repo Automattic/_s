a277e506824ad8615fe1142cd05a0dccf5dc0d22
Steven Slack <steven@s2webpress.com>
2015-05-19 01:21:04 -0400

    removed navigation.js

--- templates/main-navigation.php
+++ templates/main-navigation.php
@@ -1,11 +1,12 @@
-		<button class="nav-toggle" data-nav-toggle="#nav-primary" href="#">Menu &#9660;</button>
+		<button class="nav-toggle" data-nav-toggle="#nav-primary" href="#"><?php _e( 'Menu', '_s2' ); ?> <span class="genericon genericon-menu"></span></button>
 
-		<nav id="menu" class="nav" role="navigation" itemscope="itemscope" itemtype="http://schema.org/SiteNavigationElement">
+		<nav id="site-navigation" class="nav" role="navigation" itemscope="itemscope" itemtype="http://schema.org/SiteNavigationElement">
 			<?php wp_nav_menu( 
 				array( 
 					'theme_location'	=> 'primary',
 					'container'			=> false,
 					'menu_id'			=> 'nav-primary',
+					'menu_class'        => 'nav-menu',
 					'fallback_cb'		=> '',
 					"walker"			=> new Drop_Menu_Walker(),
 				)
