ec782656c0166502befd7f6e1308e4faece3d1d3
Chris Wilcoxson <chris@slushman.com>
2015-05-18 22:19:54 -0500

    Changed classes to page-header and page-content in content-page.php file

--- template-parts/content-page.php
+++ template-parts/content-page.php
@@ -7,11 +7,11 @@
 ?>
 
 <article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
-	<header class="entry-header">
+	<header class="page-header">
 		<?php the_title( '<h1 class="entry-title">', '</h1>' ); ?>
 	</header><!-- .entry-header -->
 
-	<div class="entry-content">
+	<div class="page-content">
 		<?php the_content(); ?>
 		<?php
 			wp_link_pages( array(
