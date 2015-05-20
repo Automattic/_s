cf34487905eaa8b0080bbdd0ff97d4db6748172e
Greg Rickaby <gregrickaby@gmail.com>
2015-05-20 17:17:23 -0500

    add .wrap to header.php and footer.php

--- footer.php
+++ footer.php
@@ -11,25 +11,29 @@
 	</div><!-- #content -->
 
 	<footer id="colophon" class="site-footer" role="contentinfo">
-		<div class="site-info">
-			<a href="<?php echo esc_url( __( 'http://wordpress.org/', '_s' ) ); ?>"><?php printf( __( 'Proudly powered by %s', '_s' ), 'WordPress' ); ?></a>
-			<span class="sep"> | </span>
-			<?php printf( esc_html__( 'Theme: %1$s by %2$s.', '_s' ), '_s', '<a href="http://automattic.com/" rel="designer">Automattic</a>' ); ?>
-		</div><!-- .site-info -->
+		<div class="wrap">
 
-		<?php
-			if ( has_nav_menu( 'social' ) ) :
-				// Social menu
-				wp_nav_menu( array(
-					'theme_location' => 'social',
-					'container'      => false,
-					'menu_class'     => 'menu-social menu menu-horizontal',
-					'link_before'    => '<span class="screen-reader-text">',
-					'link_after'     => '</span>'
-					)
-				);
-			endif;
-		?>
+			<div class="site-info">
+				<a href="<?php echo esc_url( __( 'http://wordpress.org/', '_s' ) ); ?>"><?php printf( __( 'Proudly powered by %s', '_s' ), 'WordPress' ); ?></a>
+				<span class="sep"> | </span>
+				<?php printf( esc_html__( 'Theme: %1$s by %2$s.', '_s' ), '_s', '<a href="http://automattic.com/" rel="designer">Automattic</a>' ); ?>
+			</div><!-- .site-info -->
+
+			<?php
+				if ( has_nav_menu( 'social' ) ) :
+					// Social menu
+					wp_nav_menu( array(
+						'theme_location' => 'social',
+						'container'      => false,
+						'menu_class'     => 'menu-social menu menu-horizontal',
+						'link_before'    => '<span class="screen-reader-text">',
+						'link_after'     => '</span>'
+						)
+					);
+				endif;
+			?>
+
+		</div><!-- .wrap -->
 	</footer><!-- #colophon -->
 </div><!-- #page -->
 
--- header.php
+++ header.php
@@ -25,21 +25,25 @@
 	<a class="skip-link screen-reader-text" href="#content"><?php _e( 'Skip to content', '_s' ); ?></a>
 
 	<header id="masthead" class="site-header" role="banner">
-		<div class="site-branding">
-			<h1 class="site-title"><a href="<?php echo esc_url( home_url( '/' ) ); ?>" rel="home"><?php bloginfo( 'name' ); ?></a></h1>
-			<h2 class="site-description"><?php bloginfo( 'description' ); ?></h2>
-		</div><!-- .site-branding -->
+		<div class="wrap">
 
-		<nav id="site-navigation" class="main-navigation" role="navigation">
-			<button class="menu-toggle" aria-controls="primary-menu" aria-expanded="false"><?php _e( 'Primary Menu', '_s' ); ?></button>
-			<?php
-				wp_nav_menu( array( 
-					'theme_location' => 'primary', 
-					'menu_id'        => 'primary-menu', 
-					'menu_class'     => 'menu dropdown' 
-				) ); 
-			?>
-		</nav><!-- #site-navigation -->
+			<div class="site-branding">
+				<h1 class="site-title"><a href="<?php echo esc_url( home_url( '/' ) ); ?>" rel="home"><?php bloginfo( 'name' ); ?></a></h1>
+				<h2 class="site-description"><?php bloginfo( 'description' ); ?></h2>
+			</div><!-- .site-branding -->
+
+			<nav id="site-navigation" class="main-navigation" role="navigation">
+				<button class="menu-toggle" aria-controls="primary-menu" aria-expanded="false"><?php _e( 'Primary Menu', '_s' ); ?></button>
+				<?php
+					wp_nav_menu( array(
+						'theme_location' => 'primary',
+						'menu_id'        => 'primary-menu',
+						'menu_class'     => 'menu dropdown'
+					) );
+				?>
+			</nav><!-- #site-navigation -->
+
+		</div><!-- .wrap -->
 	</header><!-- #masthead -->
 
 	<div id="content" class="site-content">
