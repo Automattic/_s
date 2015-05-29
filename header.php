fd76b25786af7db86605f5e0d95f26bafcac1f96
Sagar Jadhav <sagar.jadhav@rtcamp.com>
2015-05-29 19:55:58 +0530

    Updated Tagline markup as per WC3 guidelines.

--- header.php
+++ header.php
@@ -25,7 +25,7 @@
 	<header id="masthead" class="site-header" role="banner">
 		<div class="site-branding">
 			<h1 class="site-title"><a href="<?php echo esc_url( home_url( '/' ) ); ?>" rel="home"><?php bloginfo( 'name' ); ?></a></h1>
-			<h2 class="site-description"><?php bloginfo( 'description' ); ?></h2>
+			<p class="site-description"><?php bloginfo( 'description' ); ?></p>
 		</div><!-- .site-branding -->
 
 		<nav id="site-navigation" class="main-navigation" role="navigation">
