b5777c0d7e3faa83f5eddb8823b8076b8dc3631a
Konstantin Obenland <obenland@automattic.com>
2015-07-02 14:21:17 -0700

    Move translator comment closer to string.
    Translator comments should be placed directly in the line above the string to be translated, in order for parser to pick it up.

--- template-parts/content.php
+++ template-parts/content.php
@@ -20,8 +20,8 @@
 
 	<div class="entry-content">
 		<?php
-			/* translators: %s: Name of current post */
 			the_content( sprintf(
+				/* translators: %s: Name of current post. */
 				wp_kses( __( 'Continue reading %s <span class="meta-nav">&rarr;</span>', '_s' ), array( 'span' => array( 'class' => array() ) ) ),
 				the_title( '<span class="screen-reader-text">"', '"</span>', false )
 			) );
