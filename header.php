069d708834341929b8a0e5ed82fdb43787b512ad
Morten Rand-Hendriksen <morten@pinkandyellow.com>
2015-07-03 14:04:38 -0700

    Amended patch to display site-title as paragraph on single posts and pages as per discussion in PR.

--- header.php
+++ header.php
@@ -27,7 +27,7 @@
 			<?php if ( is_front_page() && is_home() ) : ?>
 				<h1 class="site-title"><a href="<?php echo esc_url( home_url( '/' ) ); ?>" rel="home"><?php bloginfo( 'name' ); ?></a></h1>
 			<?php else : ?>
-				<div class="site-title"><a href="<?php echo esc_url( home_url( '/' ) ); ?>" rel="home"><?php bloginfo( 'name' ); ?></a></div>
+				<p class="site-title"><a href="<?php echo esc_url( home_url( '/' ) ); ?>" rel="home"><?php bloginfo( 'name' ); ?></a></p>
 			<?php endif; ?>
 			<p class="site-description"><?php bloginfo( 'description' ); ?></p>
 		</div><!-- .site-branding -->
