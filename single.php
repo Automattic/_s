646b7a40425b69a24d3cc82099ae5b94f1b5290b
Cameron Campbell <cdcampbell26@gmail.com>
2015-07-15 11:11:40 -0400

    updated function call

--- single.php
+++ single.php
@@ -16,7 +16,7 @@ get_header(); ?>
 	
 				<?php get_template_part( 'content', 'single' ); ?>
 	
-				<?php post_navigation(); ?>
+				<?php the_post_navigation(); ?>
 	
 				<?php
 					// If comments are open or we have at least one comment, load up the comment template
