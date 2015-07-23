2901b6dba41ac5794752a392213c031f9c0c823f
Ryan Olson <ryan@thatryan.com>
2015-07-23 00:56:04 -0700

    Fix search icon
    icon- prefix on search causing svg output to be icon-icon-search.

--- searchform.php
+++ searchform.php
@@ -4,6 +4,6 @@
 		<input type="search" class="search-field" placeholder="<?php echo esc_attr_x( 'Search', 'hippie' ) ?>" value="<?php echo get_search_query() ?>" name="s" title="<?php echo esc_attr_x( 'Search...', 'hippie' ) ?>" />
 	</label>
 	<button aria-label="search">
-		<?php _s_do_svg( 'icon-search' ); ?>
+		<?php _s_do_svg( 'search' ); ?>
 	</button>
 </form>
