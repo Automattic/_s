404a42db37c324fc14cdaf4c0ed4f12d207d4c08
Guillaume Kanoufi <g.kanoufi@gmail.com>
2015-08-16 11:14:35 +0100

    Added mainContentOfPage to single.php

--- single.php
+++ single.php
@@ -8,7 +8,7 @@
 get_header(); ?>
 
 	<div id="primary" class="content-area">
-		<main id="main" class="site-main" role="main">
+		<main id="main" class="site-main" role="main" itemprop="mainContentOfPage" itemscope="itemscope" itemtype="http://schema.org/Blog">
 
 		<?php while ( have_posts() ) : the_post(); ?>
 
