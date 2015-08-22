4b519c124bb49fe64cad68be44388b93934008dc
Emil Uzelac <emil@uzelac.me>
2015-08-21 22:24:25 -0500

    Update functions.php

--- functions.php
+++ functions.php
@@ -71,7 +71,7 @@ function _s_setup() {
 		'link',
 	) );
 
-	// Set up the WordPress core custom background feature.
+	// Setup the WordPress core custom background feature.
 	add_theme_support( 'custom-background', apply_filters( '_s_custom_background_args', array(
 		'default-color' => 'ffffff',
 		'default-image' => '',
