b4cbff3dd0ead78e8db12e9cb94ef2ecce5ad15d
Mike King <mike@85pixels.com>
2015-08-23 15:05:54 -1000

    Hide admin bar for all users by default

--- functions.php
+++ functions.php
@@ -72,8 +72,13 @@ function _s_setup() {
 	) );
 
 	/**
+	 * Hide admin bar for all users
+	 */
+	add_filter('show_admin_bar', '__return_false');
+
+	/**
 	 * Clean up wp_head
-	*/
+	 */
 	remove_action( 'wp_head', 'feed_links_extra', 3 );
 	remove_action( 'wp_head', 'feed_links', 2 );
 	remove_action( 'wp_head', 'rsd_link');
