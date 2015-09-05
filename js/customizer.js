b80f923a3f5513624df39ccc3f0723abe3eec225
Carl Alberto <cralberto11@gmail.com>
2015-09-05 16:33:30 +0800

    Updated the code to adjust the css color of .site-title a

--- js/customizer.js
+++ js/customizer.js
@@ -20,15 +20,17 @@
 	wp.customize( 'header_textcolor', function( value ) {
 		value.bind( function( to ) {
 			if ( 'blank' === to ) {
-				$( '.site-title, .site-title a, .site-description' ).css( {
+				$( '.site-title, .site-description' ).css( {
 					'clip': 'rect(1px, 1px, 1px, 1px)',
 					'position': 'absolute'
 				} );
 			} else {
-				$( '.site-title, .site-title a, .site-description' ).css( {
+				$( '.site-title, .site-description' ).css( {
 					'clip': 'auto',
-					'color': to,
 					'position': 'relative'
+				} );
+				$( '.site-title a, .site-description' ).css( {
+					'color': to
 				} );
 			}
 		} );
