bf9838d338134d23f9083a9d5628247261ce3cbf
Brad Parbs <brad@bradparbs.com>
2015-09-19 23:12:06 -0500

    Merge inc/jetpack.php from upstream _s

--- inc/jetpack.php
+++ inc/jetpack.php
@@ -1,25 +1,37 @@
 <?php
 /**
- * Jetpack Compatibility File
- * See: http://jetpack.me/
+ * Jetpack Compatibility File.
+ *
+ * @link https://jetpack.me/
  *
  * @package _s
  */
 
 /**
  * Add theme support for Infinite Scroll.
- * See: http://jetpack.me/support/infinite-scroll/
+ * See: https://jetpack.me/support/infinite-scroll/
  */
 function _s_jetpack_setup() {
 	add_theme_support( 'infinite-scroll', array(
 		'container' => 'main',
+		'render'    => '_s_infinite_scroll_render',
 		'footer'    => 'page',
 	) );
-}
+} // end function _s_jetpack_setup
 add_action( 'after_setup_theme', '_s_jetpack_setup' );
-
 
 /**
  * Prevent Jetpack from enabling modules by default.
  */
 add_filter( 'jetpack_get_default_modules', '__return_empty_array', 99 );
+
+
+/**
+ * Custom render function for Infinite Scroll.
+ */
+function _s_infinite_scroll_render() {
+	while ( have_posts() ) {
+		the_post();
+		get_template_part( 'template-parts/content', get_post_format() );
+	}
+} // end function _s_infinite_scroll_render
