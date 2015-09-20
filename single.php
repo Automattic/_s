fbb9b797b23e9de47ac80b1c4cc804d0e91ee819
Brad Parbs <brad@bradparbs.com>
2015-09-19 23:27:46 -0500

    Merge single.php upstream from _s

--- single.php
+++ single.php
@@ -2,6 +2,8 @@
 /**
  * The template for displaying all single posts.
  *
+ * @link https://developer.wordpress.org/themes/basics/template-hierarchy/#single-post
+ *
  * @package _s
  */
 
@@ -19,13 +21,13 @@ get_header(); ?>
 				<?php the_post_navigation(); ?>
 
 				<?php
-					// If comments are open or we have at least one comment, load up the comment template
+					// If comments are open or we have at least one comment, load up the comment template.
 					if ( comments_open() || get_comments_number() ) :
 						comments_template();
 					endif;
 				?>
 
-			<?php endwhile; // end of the loop. ?>
+			<?php endwhile; // End of the loop. ?>
 
 			</main><!-- #main -->
 		</div><!-- .primary -->
