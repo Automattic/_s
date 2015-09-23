424f1a295337335f71fcdaa8050f833e78b17cdd
Greg Rickaby <greg@gregrickaby.com>
2015-09-23 09:25:02 -0500

    Remove copy/pasta'd textdomain

--- searchform.php
+++ searchform.php
@@ -1,7 +1,7 @@
 <form role="search" method="get" class="search-form" action="<?php echo esc_url( home_url( '/' ) ); ?>">
 	<label>
-		<span class="screen-reader-text"><?php echo _x( 'Search for:', 'hippie' ) ?></span>
-		<input type="search" class="search-field" placeholder="<?php echo esc_attr_x( 'Search', 'hippie' ) ?>" value="<?php echo get_search_query() ?>" name="s" title="<?php echo esc_attr_x( 'Search...', 'hippie' ) ?>" />
+		<span class="screen-reader-text"><?php echo _x( 'Search for:', '_s' ) ?></span>
+		<input type="search" class="search-field" placeholder="<?php echo esc_attr_x( 'Search', '_s' ) ?>" value="<?php echo get_search_query() ?>" name="s" title="<?php echo esc_attr_x( 'Search...', '_s' ) ?>" />
 	</label>
 	<button>
 		<?php _e( 'Submit', '_s' ); ?>
