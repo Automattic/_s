d78f2d29a3cd442d26c61e192b114e3918ccf7b0
Greg Rickaby <gregrickaby@gmail.com>
2015-09-28 09:30:05 -0500

    screen reader and ARIA friendly search form

--- searchform.php
+++ searchform.php
@@ -1,9 +1,5 @@
-<form role="search" method="get" class="search-form" action="<?php echo esc_url( home_url( '/' ) ); ?>">
-	<label>
-		<span class="screen-reader-text"><?php echo _x( 'Search for:', '_s' ) ?></span>
-		<input type="search" class="search-field" placeholder="<?php echo esc_attr_x( 'Search', '_s' ) ?>" value="<?php echo get_search_query() ?>" name="s" title="<?php echo esc_attr_x( 'Search...', '_s' ) ?>" />
-	</label>
-	<button>
-		<?php _e( 'Submit', '_s' ); ?>
-	</button>
+<form method="get" class="search-form" action="<?php echo esc_url( home_url( '/' ) ); ?>" role="search">
+	<label for="search-field"><span class="screen-reader-text"><?php esc_html_e( 'To search this site, enter a search term', '_s' ) ?></span></label>
+	<input class="search-field" id="search-field" type="text" name="s" value="<?php echo get_search_query() ?>" aria-required="false" autocomplete="off" placeholder="<?php echo esc_attr_x( 'Search', '_s' ) ?>" />
+	<button><?php esc_html_e( 'Submit', '_s' ); ?></button>
 </form>
