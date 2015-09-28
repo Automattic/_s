8f13f3924fbb2a040b519bcece38f478a56fd238
Greg Rickaby <gregrickaby@gmail.com>
2015-09-28 09:28:21 -0500

    search results headlines should be <h2>

--- search.php
+++ search.php
@@ -18,7 +18,7 @@ get_header(); ?>
 			<?php if ( have_posts() ) : ?>
 
 				<header class="page-header">
-					<h1 class="page-title"><?php printf( esc_html__( 'Search Results for: %s', '_s' ), '<span>' . get_search_query() . '</span>' ); ?></h1>
+					<h1 class="page-title"><?php printf( esc_html__( 'Search results for: %s', '_s' ), '<span>' . get_search_query() . '</span>' ); ?></h1>
 				</header><!-- .page-header -->
 
 				<?php /* Start the Loop */ ?>
