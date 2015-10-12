ae69a19358e48d11df063e2c3c5635aaa3674476
Kellee <kellee@voltagead.com>
2015-10-12 14:41:54 -0600

    added gtm tag

--- wp-content/themes/vtg-theme/footer.php
+++ wp-content/themes/vtg-theme/footer.php
@@ -23,13 +23,13 @@
 <?php wp_footer(); ?>
 
 <!-- Google Tag Manager -->
-<noscript><iframe src="//www.googletagmanager.com/ns.html?id=GTM-<!-- INSERT CODE SPECIFIC TO GTM ACCOUNT HERE EX. PLB42T -->"
+<noscript><iframe src="//www.googletagmanager.com/ns.html?id=GTM-<!-- INSERT CODE SPECIFIC TO GTM ACCOUNT HERE EX.PLB42T -->"
 height="0" width="0" style="display:none;visibility:hidden"></iframe></noscript>
 <script>(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
 new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
 j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
 '//www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
-})(window,document,'script','dataLayer','GTM-<!-- INSERT CODE SPECIFIC TO GTM ACCOUNT HERE EX. PLB42T -->');</script>
+})(window,document,'script','dataLayer','GTM-<!-- INSERT CODE SPECIFIC TO GTM ACCOUNT HERE EX.PLB42T -->');</script>
 <!-- End Google Tag Manager -->
 
 </body>
