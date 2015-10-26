6f39c5ecf8d58efe5b5573bc423c478634d429ee
Thomas Guillot <thomas@automattic.com>
2015-10-26 16:33:55 +0100

    Update PHP Documentation

--- inc/jetpack.php
+++ inc/jetpack.php
@@ -7,21 +7,21 @@
  * @package _s
  */
 
+/**
+ * Jetpack setup function.
+ *
+ * See: https://jetpack.me/support/infinite-scroll/
+ * See: https://jetpack.me/support/responsive-videos/
+ */
 function _s_jetpack_setup() {
-	/**
-	 * Add theme support for Infinite Scroll.
-	 * See: https://jetpack.me/support/infinite-scroll/
-	 */
+	// Add theme support for Infinite Scroll.
 	add_theme_support( 'infinite-scroll', array(
 		'container' => 'main',
 		'render'    => '_s_infinite_scroll_render',
 		'footer'    => 'page',
 	) );
 
-	/**
-	 * Add theme support for Responsive Videos.
-	 * See: https://jetpack.me/support/responsive-videos/
-	 */
+	// Add theme support for Responsive Videos.
 	add_theme_support( 'jetpack-responsive-videos' );
 } // end function _s_jetpack_setup
 add_action( 'after_setup_theme', '_s_jetpack_setup' );
