ea56c92bb8079ef7df51f39c6113629466184fa4
Greg Rickaby <gregrickaby@gmail.com>
2015-11-12 13:19:55 -0600

    Improved _s_categorized_blog() function. Fixes #862

--- inc/template-tags.php
+++ inc/template-tags.php
@@ -83,28 +83,30 @@ endif;
  * @return bool
  */
 function _s_categorized_blog() {
+
+	// Check for transient, if none, go count the categories.
 	if ( false === ( $all_the_cool_cats = get_transient( '_s_categories' ) ) ) {
+
 		// Create an array of all the categories that are attached to posts.
 		$all_the_cool_cats = get_categories( array(
-			'fields'     => 'ids',
-			'hide_empty' => 1,
-			// We only need to know if there is more than one category.
-			'number'     => 2,
+			'fields' => 'ids',
+			'number' => 2, // We only need to know if there is more than one category.
 		) );
 
 		// Count the number of categories that are attached to the posts.
 		$all_the_cool_cats = count( $all_the_cool_cats );
 
+		// Store the number of categories until the transient is flushed with _s_category_transient_flusher().
 		set_transient( '_s_categories', $all_the_cool_cats );
 	}
 
-	if ( $all_the_cool_cats > 1 ) {
-		// This blog has more than 1 category so _s_categorized_blog should return true.
-		return true;
-	} else {
-		// This blog has only 1 category so _s_categorized_blog should return false.
+	// This blog only has 1 category, so _s_categorized_blog and should return false.
+	if ( 1 >= $all_the_cool_cats ) {
 		return false;
 	}
+
+	// This blog has more than 1 category, so _s_categorized_blog should return true.
+	return true;
 }
 
 /**
