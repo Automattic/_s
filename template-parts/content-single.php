f9a65a2f4fe3854e9b2444fd485d92bfd95c4919
Andrew Saint <andrew.james.saint@icloud.com>
2015-12-02 15:12:06 +0000

    Add filter to append wp_link_pages to the content

--- template-parts/content-single.php
+++ template-parts/content-single.php
@@ -20,12 +20,6 @@
 
 	<div class="entry-content">
 		<?php the_content(); ?>
-		<?php
-			wp_link_pages( array(
-				'before' => '<div class="page-links">' . esc_html__( 'Pages:', '_s' ),
-				'after'  => '</div>',
-			) );
-		?>
 	</div><!-- .entry-content -->
 
 	<footer class="entry-footer">
