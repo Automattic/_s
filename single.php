60f7311f1977179822e83c95745fa6787894959a
Ulrich Pogson <grapplerulrich@gmail.com>
2015-12-04 15:51:58 +0100

    Add support for post formats in single post

--- single.php
+++ single.php
@@ -15,7 +15,7 @@ get_header(); ?>
 		<?php
 		while ( have_posts() ) : the_post();
 
-			get_template_part( 'template-parts/content', 'single' );
+			get_template_part( 'template-parts/content', get_post_format() );
 
 			the_post_navigation();
 
