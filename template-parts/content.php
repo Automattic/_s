944803c5d495d4599e5b6ec51c68b98be3d2088e
byjml <byjml@users.noreply.github.com>
2015-12-06 15:24:01 +0300

    Use consistent syntax for control structures
    Changed the opening brace to ':' (or colon) and the closing brace to `endif;`

--- template-parts/content.php
+++ template-parts/content.php
@@ -12,11 +12,11 @@
 <article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
 	<header class="entry-header">
 		<?php
-			if ( is_single() ) {
-				the_title( '<h1 class="entry-title">', '</h1>' );
-			} else {
-				the_title( '<h2 class="entry-title"><a href="' . esc_url( get_permalink() ) . '" rel="bookmark">', '</a></h2>' );
-			}
+		if ( is_single() ) :
+			the_title( '<h1 class="entry-title">', '</h1>' );
+		else :
+			the_title( '<h2 class="entry-title"><a href="' . esc_url( get_permalink() ) . '" rel="bookmark">', '</a></h2>' );
+		endif;
 
 		if ( 'post' === get_post_type() ) : ?>
 		<div class="entry-meta">
