e1f4b0add4e74007d4a3be638931c5d158711cfd
jrfnl <github_nospam@adviesenzo.nl>
2015-12-15 10:09:54 +0100

    Updated based on feedback.
    * Include skip-link-focus-fix.js in the js hint/lint tests & fix up the file.
    * Pull in .jshintrc from WP SVN.
    * Add PHPCompatibility Sniffs.
    * Sync the ignore statements to always exclude .min.js files.
    * Slim down the tested against PHP versions even more.

--- js/skip-link-focus-fix.js
+++ js/skip-link-focus-fix.js
@@ -1,16 +1,16 @@
 /**
- * skip-link-focus-fix.js
+ * File skip-link-focus-fix.js.
  *
  * Helps with accessibility for keyboard only users.
  *
  * Learn more: https://git.io/vWdr2
  */
 ( function() {
-	var is_webkit = navigator.userAgent.toLowerCase().indexOf( 'webkit' ) > -1,
-	    is_opera  = navigator.userAgent.toLowerCase().indexOf( 'opera' )  > -1,
-	    is_ie     = navigator.userAgent.toLowerCase().indexOf( 'msie' )   > -1;
+	var isWebkit = navigator.userAgent.toLowerCase().indexOf( 'webkit' ) > -1,
+	    isOpera  = navigator.userAgent.toLowerCase().indexOf( 'opera' )  > -1,
+	    isIe     = navigator.userAgent.toLowerCase().indexOf( 'msie' )   > -1;
 
-	if ( ( is_webkit || is_opera || is_ie ) && document.getElementById && window.addEventListener ) {
+	if ( ( isWebkit || isOpera || isIe ) && document.getElementById && window.addEventListener ) {
 		window.addEventListener( 'hashchange', function() {
 			var id = location.hash.substring( 1 ),
 				element;
