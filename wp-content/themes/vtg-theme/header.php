d2fd39c71ef1a03570993957e850c63879ff4a4f
Craig Freeman <craigfreeman@gmail.com>
2015-12-29 16:43:39 -0700

    Reworked repo to use NPM/browserify, removed Bower

--- wp-content/themes/vtg-theme/header.php
+++ wp-content/themes/vtg-theme/header.php
@@ -16,6 +16,7 @@
 <meta name="viewport" content="width=device-width, initial-scale=1">
 <link rel="profile" href="http://gmpg.org/xfn/11">
 <link rel="pingback" href="<?php bloginfo( 'pingback_url' ); ?>">
+<link rel="stylesheet" type="text/css" href="//cdnjs.cloudflare.com/ajax/libs/modernizr/2.8.3/modernizr.min.js">
 
 <?php wp_head(); ?>
 </head>
