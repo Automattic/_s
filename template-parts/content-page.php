1664139234fb1750c7c0ee9544d7e3df5c75f6d3
John Cionci <john@thinkoomph.com>
2016-04-05 11:23:56 -0400

    Add template tag for entry titles for better consistency

--- template-parts/content-page.php
+++ template-parts/content-page.php
@@ -11,7 +11,7 @@
 
 <article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
 	<header class="entry-header">
-		<?php the_title( '<h1 class="entry-title">', '</h1>' ); ?>
+		<?php _s_entry_title(); ?>
 	</header><!-- .entry-header -->
 
 	<div class="entry-content">
