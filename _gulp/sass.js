394de566ebac225bda7afcaee7fb1c7019ba66a6
Silvestar Bistrovic <Silvestar Bistrovic>
2016-04-24 21:38:23 +0200

    Fixing gulp and typography.

--- _gulp/sass.js
+++ _gulp/sass.js
@@ -4,14 +4,14 @@ var gulp = require('gulp');
 var sass = require('gulp-sass');
 
 gulp.task('sass', function () {
-	gulp.src('./sass/**/*.scss')
+	gulp.src('./sass/style.scss')
 		.pipe(sass()
 			.on('error', sass.logError))
 		.pipe(gulp.dest('./'));
 });
 
 gulp.task('sass-sync', function () {
-	gulp.src('./sass/**/*.scss')
+	gulp.src('./sass/style.scss')
 		.pipe(sass.sync()
 			.on('error', sass.logError))
 		.pipe(gulp.dest('./css'));
