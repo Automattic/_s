2316b1981c6c63bfa113949a83961dda506fccd1
Caroline Moore <calobee@gmail.com>
2016-06-13 16:39:54 -0400

    Removing post formats support and styles from _s

--- functions.php
+++ functions.php
@@ -59,18 +59,6 @@ function _s_setup() {
 		'caption',
 	) );
 
-	/*
-	 * Enable support for Post Formats.
-	 * See https://developer.wordpress.org/themes/functionality/post-formats/
-	 */
-	add_theme_support( 'post-formats', array(
-		'aside',
-		'image',
-		'video',
-		'quote',
-		'link',
-	) );
-
 	// Set up the WordPress core custom background feature.
 	add_theme_support( 'custom-background', apply_filters( '_s_custom_background_args', array(
 		'default-color' => 'ffffff',
