14a149cd19ac55ecb6c42565d5ceb106adba748b
ThierryA <thierry@getbeans.io>
2016-06-14 11:24:20 +0200

    Escape header attributes.

--- header.php
+++ header.php
@@ -12,10 +12,10 @@
 ?><!DOCTYPE html>
 <html <?php language_attributes(); ?>>
 <head>
-<meta charset="<?php bloginfo( 'charset' ); ?>">
+<meta charset="<?php esc_attr_e( get_bloginfo( 'charset' ) ); ?>">
 <meta name="viewport" content="width=device-width, initial-scale=1">
 <link rel="profile" href="http://gmpg.org/xfn/11">
-<link rel="pingback" href="<?php bloginfo( 'pingback_url' ); ?>">
+<link rel="pingback" href="<?php echo esc_url( get_bloginfo( 'pingback_url' ) ); ?>">
 
 <?php wp_head(); ?>
 </head>
