6f88d2b300605add9dfe82f12a53d21eee5e9485
Marius <funman@gmail.com>
2016-08-07 21:22:32 +0200

    Make the `content.php` more flexible
    replace `is_single()` with `is_singular()` to make sure custom post types are accounted for by default

--- template-parts/content.php
+++ template-parts/content.php
@@ -12,7 +12,7 @@
 <article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
 	<header class="entry-header">
 		<?php
-		if ( is_single() ) :
+		if ( is_singular() ) :
 			the_title( '<h1 class="entry-title">', '</h1>' );
 		else :
 			the_title( '<h2 class="entry-title"><a href="' . esc_url( get_permalink() ) . '" rel="bookmark">', '</a></h2>' );
