160e9f0301d8098fa5bca84f9bb86b05ac4eee73
Bappi <bappi.cs@gmail.com>
2016-09-27 12:16:03 +0600

    Remove period from file header

--- 404.php
+++ 404.php
@@ -1,6 +1,6 @@
 <?php
 /**
- * The template for displaying 404 pages (not found).
+ * The template for displaying 404 pages (not found)
  *
  * @link https://codex.wordpress.org/Creating_an_Error_404_Page
  *
