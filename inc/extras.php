c19b43fe3ac1fbea2432327a46864dcec01984bc
Bappi <bappi.cs@gmail.com>
2016-09-27 11:58:22 +0600

    Remove period from file header

--- inc/extras.php
+++ inc/extras.php
@@ -1,6 +1,6 @@
 <?php
 /**
- * Custom functions that act independently of the theme templates.
+ * Custom functions that act independently of the theme templates
  *
  * Eventually, some of the functionality here could be replaced by core features.
  *
