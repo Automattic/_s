8e1367bce304a459da5e3b47eb2d990d7f2f4200
Bjorn <bjorn2404@gmail.com>
2016-10-18 17:48:16 -0500

    _s_pingback_header should use get_bloginfo instead of bloginfo because echo is already being used to output the link tag (entire line)

--- inc/extras.php
+++ inc/extras.php
@@ -33,7 +33,7 @@ add_filter( 'body_class', '_s_body_classes' );
  */
 function _s_pingback_header() {
 	if ( is_singular() && pings_open() ) {
-		echo '<link rel="pingback" href="', bloginfo( 'pingback_url' ), '">';
+		echo '<link rel="pingback" href="', esc_url( get_bloginfo( 'pingback_url' ) ), '">';
 	}
 }
 add_action( 'wp_head', '_s_pingback_header' );
