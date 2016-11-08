a45b6ac1b96d904d2c8da100fcfe37ccf1068a01
SilverBack Studio <amministrazione@silverbackstudio.it>
2016-11-08 09:07:19 +0000

    added gulp installation to setup

--- setup.sh
+++ setup.sh
@@ -8,5 +8,6 @@ find ./ -type f -name "*.php"  -exec sed -i "s/_svbk-/$THEME_HANDLE-/g" {} \;
 sed -i "s/Text Domain: _svbk/Text Domain: $THEME_HANDLE/g" sass/style.scss 
 sed -i "s/Theme Name: _svbk/Theme Name: $THEME_NAME/g" sass/style.scss 
 
+npm install -g gulp
 npm update
 gulp compile-sass
