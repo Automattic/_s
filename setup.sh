16ebfbd66c93537f150fe5c78a13e2245778745f
SilverBack Studio <amministrazione@silverbackstudio.it>
2016-11-08 08:34:28 +0000

    added setup for theme name

--- setup.sh
+++ setup.sh
@@ -1,3 +1,4 @@
+read -p "Theme name: " THEME_NAME
 read -p "Theme handle: " THEME_HANDLE
 
 find ./ -type f -name "*.php"  -exec sed -i "s/_svbk/$THEME_HANDLE/g" {} \;
@@ -5,3 +6,4 @@ find ./ -type f -name "*.php"  -exec sed -i "s/_svbk_/$THEME_HANDLE_/g" {} \;
 find ./ -type f -name "*.php"  -exec sed -i "s/ _svbk/ $THEME_HANDLE/g" {} \;
 find ./ -type f -name "*.php"  -exec sed -i "s/_svbk-/$THEME_HANDLE-/g" {} \;
 sed -i "s/Text Domain: _svbk/Text Domain: $THEME_HANDLE/g" style.css 
+sed -i "s/Theme Name: _svbk/Theme Name: $THEME_NAME/g" style.css 
