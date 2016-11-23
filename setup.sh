49dc0a2dcc6ba2bc40cfdd4f203164f47afbaee3
Brando Meniconi <b.meniconi@silverbackstudio.it>
2016-11-23 14:17:43 +0100

    latest fixes

--- setup.sh
+++ setup.sh
@@ -1,12 +1,12 @@
 read -p "Theme name: " THEME_NAME
 read -p "Theme handle: " THEME_HANDLE
 
-find ./ -type f -name "*.php"  -exec sed -i "s/'_svbk'/$THEME_HANDLE/g" {} \;
+find ./ -type f -name "*.php"  -exec sed -i "s/'_svbk'/'$THEME_HANDLE'/g" {} \;
 find ./ -type f -name "*.php"  -exec sed -i "s/_svbk_/${THEME_HANDLE}_/g" {} \;
 find ./ -type f -name "*.php"  -exec sed -i "s/ _svbk/ $THEME_HANDLE/g" {} \;
 find ./ -type f -name "*.php"  -exec sed -i "s/_svbk-/$THEME_HANDLE-/g" {} \;
-sed -i "s/Text Domain: _svbk/Text Domain: $THEME_HANDLE/g" sass/style.scss 
-sed -i "s/Theme Name: _svbk/Theme Name: $THEME_NAME/g" sass/style.scss 
+sed -i "s/Text Domain: _svbk/Text Domain: $THEME_HANDLE/g" sass/style.scss
+sed -i "s/Theme Name: _svbk/Theme Name: $THEME_NAME/g" sass/style.scss
 
 npm install -g gulp
 npm update
