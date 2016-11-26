31a9e90437ace37e11a23a9f3f26126d49844031
Sami Keijonen <sami.keijonen@foxnet.fi>
2016-11-26 19:26:12 +0200

    Remove link around header image.

--- inc/custom-header.php
+++ inc/custom-header.php
@@ -5,9 +5,7 @@
  * You can add an optional custom header image to header.php like so ...
  *
 	<?php if ( get_header_image() ) : ?>
-	<a href="<?php echo esc_url( home_url( '/' ) ); ?>" rel="home">
-		<img src="<?php header_image(); ?>" width="<?php echo esc_attr( get_custom_header()->width ); ?>" height="<?php echo esc_attr( get_custom_header()->height ); ?>" alt="">
-	</a>
+	<img src="<?php header_image(); ?>" width="<?php echo esc_attr( get_custom_header()->width ); ?>" height="<?php echo esc_attr( get_custom_header()->height ); ?>" alt="">
 	<?php endif; // End header image check. ?>
  *
  * @link https://developer.wordpress.org/themes/functionality/custom-headers/
