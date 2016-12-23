fd701834ffeecf83238dbda2d391a0ca777c5b84
jrfnl <github_nospam@adviesenzo.nl>
2016-12-23 15:59:15 +0100

    Add translator comments as the strings contain variables.

--- comments.php
+++ comments.php
@@ -30,11 +30,13 @@ if ( post_password_required() ) {
 				$comment_count = get_comments_number();
 				if ( 1 === $comment_count ) {
 					printf(
+						/* translators: 1: title. */
 						esc_html_e( 'One thought on &ldquo;%1$s&rdquo;', '_s' ),
 						'<span>' . get_the_title() . '</span>'
 					);
 				} else {
 					printf( // WPCS: XSS OK.
+						/* translators: 1: comment count number, 2: title. */
 						esc_html( _nx( '%1$s thought on &ldquo;%2$s&rdquo;', '%1$s thoughts on &ldquo;%2$s&rdquo;', $comment_count, 'comments title', '_s' ) ),
 						number_format_i18n( $comment_count ),
 						'<span>' . get_the_title() . '</span>'
