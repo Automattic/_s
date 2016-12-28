7ee2c8774f73eb2588923ea5e083a51346cdfa7b
Sami Keijonen <sami.keijonen@foxnet.fi>
2016-12-28 15:29:51 +0200

    Add selective refresh for site title and description.

--- inc/customizer.php
+++ inc/customizer.php
@@ -14,8 +14,37 @@ function _s_customize_register( $wp_customize ) {
 	$wp_customize->get_setting( 'blogname' )->transport         = 'postMessage';
 	$wp_customize->get_setting( 'blogdescription' )->transport  = 'postMessage';
 	$wp_customize->get_setting( 'header_textcolor' )->transport = 'postMessage';
+
+	if ( isset( $wp_customize->selective_refresh ) ) {
+		$wp_customize->selective_refresh->add_partial( 'blogname', array(
+			'selector'        => '.site-title a',
+			'render_callback' => '_s_customize_partial_blogname',
+		) );
+		$wp_customize->selective_refresh->add_partial( 'blogdescription', array(
+			'selector'        => '.site-description',
+			'render_callback' => '_s_customize_partial_blogdescription',
+		) );
+	}
 }
 add_action( 'customize_register', '_s_customize_register' );
+
+/**
+ * Render the site title for the selective refresh partial.
+ *
+ * @return void
+ */
+function _s_customize_partial_blogname() {
+	bloginfo( 'name' );
+}
+
+/**
+ * Render the site tagline for the selective refresh partial.
+ *
+ * @return void
+ */
+function _s_customize_partial_blogdescription() {
+	bloginfo( 'description' );
+}
 
 /**
  * Binds JS handlers to make Theme Customizer preview reload changes asynchronously.
