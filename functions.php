26fec85c9978db54abed67fdc943f95f2a182fb5
SilverBack Studio <amministrazione@silverbackstudio.it>
2017-01-26 09:43:34 +0000

    added theme.js

--- functions.php
+++ functions.php
@@ -134,8 +134,8 @@ function _svbk_scripts() {
 	wp_enqueue_style( '_svbk-style', get_stylesheet_uri() );
 
 	wp_enqueue_script( '_svbk-navigation', get_template_directory_uri() . '/js/navigation.js', array(), '20151215', true );
-
 	wp_enqueue_script( '_svbk-skip-link-focus-fix', get_template_directory_uri() . '/js/skip-link-focus-fix.js', array(), '20151215', true );
+	wp_enqueue_script( 'ufficiobrevetti-theme', get_template_directory_uri() . '/js/theme.js', array(), '20170120', true );
 
 	if ( is_singular() && comments_open() && get_option( 'thread_comments' ) ) {
 		wp_enqueue_script( 'comment-reply' );
