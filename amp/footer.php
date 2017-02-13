1c75964940a46281d2f27aafaf48170d692db910
SilverBack Studio <amministrazione@silverbackstudio.it>
2017-02-13 13:04:45 +0000

    added AMP default footer

---
+++ amp/footer.php
@@ -0,0 +1,7 @@
+<footer class="amp-wp-footer">
+	<div>
+		<h2><?php echo esc_html( $this->get( 'blog_name' ) ); ?></h2>
+
+		<a href="#top" class="back-to-top"><?php _e( 'Back to top', 'amp' ); ?></a>
+	</div>
+</footer>
