4e06d7f6b14b15084f0feb261665ca72aa226620
Brando Meniconi <meniconi.brando@gmail.com>
2017-02-13 14:47:52 +0100

    Added ACF template function fallbacks

--- inc/template-tags.php
+++ inc/template-tags.php
@@ -135,3 +135,21 @@ function _svbk_the_custom_logo() {
 	}
 }
 endif;
+
+//ACF functions fallbacks
+if(!function_exists('get_field')){
+	function get_field($key, $post_id=null){
+		
+		if($post_id === null){
+			$post_id = get_the_ID();
+		}
+		
+		return get_post_meta( $post_id, $key, true );
+	}
+}
+if(!function_exists('the_field')){
+	function the_field($key, $post_id=null){
+		echo get_field( $key, $post_id );
+	}
+}
+
