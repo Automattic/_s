4949b36e71f717c5e7adfd02fce13a302323c880
SilverBack Studio <amministrazione@silverbackstudio.it>
2017-02-13 12:08:29 +0000

    updated 404 page content

--- functions.php
+++ functions.php
@@ -59,6 +59,7 @@ function _svbk_setup() {
 	// This theme uses wp_nav_menu() in one location.
 	register_nav_menus( array(
 		'menu-1' => esc_html__( 'Primary', '_svbk' ),
+		'menu-404' => esc_html__( 'Not Found (404) Page', '_svbk' ),
 	) );
 
 	/*
