9d1b8b28a9c423434e982d3591c10bc4a96fa131
Brando Meniconi <b.meniconi@silverbackstudio.it>
2017-02-16 17:45:39 +0100

    added thumb themplate part

---
+++ template-parts/thumb.php
@@ -0,0 +1,22 @@
+<?php
+/**
+ * Template part for displaying posts thumbs/previews
+ *
+ * @link https://codex.wordpress.org/Template_Hierarchy
+ *
+ * @package _svbk
+ */
+
+?>
+
+<div id="post-<?php the_ID(); ?>" <?php post_class(array('post-thumb')); ?>>
+
+	<a href="<?php the_permalink() ?>" ><?php the_post_thumbnail(); ?></a>
+
+	<div class="entry-header">
+		<?php the_title( '<h3 class="entry-title"><a href="' . esc_url( get_permalink() ) . '" rel="bookmark">', '</a></h3>' ); ?>
+		<p class="entry-excerpt"><?php the_excerpt(); ?></p>
+		<a class="readmore" href="<?php the_permalink() ?>" ><?php __('View More', '_svbk'); ?></a>
+	</div><!-- .entry-header -->
+
+</div><!-- #post-## -->
