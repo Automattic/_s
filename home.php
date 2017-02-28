458623cea6573fcac289b7cb2e8661e5cf10cc5f
Brando Meniconi <b.meniconi@silverbackstudio.it>
2017-02-28 18:02:31 +0100

    standardized ajax-filter, applied to blog by default

---
+++ home.php
@@ -0,0 +1,64 @@
+<?php
+/**
+ * The template for displaying all pages
+ *
+ * This is the template that displays all pages by default.
+ * Please note that this is the WordPress construct of pages
+ * and that other 'pages' on your WordPress site may use a
+ * different template.
+ *
+ * @link https://codex.wordpress.org/Template_Hierarchy
+ *
+ * @package _svbk
+ */
+
+get_header(); ?>
+
+	<div id="primary" class="content-area">
+		<main id="main" class="site-main" role="main">
+
+				<?php if ( have_posts() ) : ?>
+
+					<header>
+						<h1 class="page-title screen-reader-text"><?php single_post_title(); ?></h1>
+					</header>
+
+					<?php
+					if( $categories  = wp_list_categories(array('title_li'=>'', 'show_option_all'=>__('All categories', '_svbk'), 'echo'=>0 )) ): ?>
+					<ul id="category-filter" class="ajax-filter filter-list">
+						<?php echo $categories; ?>
+					</ul>
+					<?php endif; ?>
+
+					<div class="posts-list post-thumbs ajax-content">
+					<?php
+					endif;
+
+					/* Start the Loop */
+					while ( have_posts() ) : the_post();
+
+						/*
+						 * Include the Post-Format-specific template for the content.
+						 * If you want to override this in a child theme, then include a file
+						 * called content-___.php (where ___ is the Post Format name) and that will be used instead.
+						 */
+						get_template_part( 'template-parts/thumb', get_post_type() );
+
+					endwhile;
+					?>
+					</div>
+					<?php
+					the_posts_navigation();
+
+				else :
+
+					get_template_part( 'template-parts/content', 'none' );
+
+				endif; ?>
+
+		</main><!-- #main -->
+	</div><!-- #primary -->
+
+<?php
+get_sidebar();
+get_footer();
--- js/filter.js
+++ js/filter.js
@@ -1,67 +1,83 @@
-function updateNextPostLink(link){
-    if(link){
-        $('.posts-navigation .nav-previous a').attr('href', link);
-        $('.load-more').removeClass('no-more');
-    } else {
-        $('.load-more').addClass('no-more');
+(function($){
+
+    function updateNextPostLink(link){
+        if(link){
+            $('.posts-navigation .nav-previous a').attr('href', link);
+            $('.load-more').removeClass('no-more');
+        } else {
+            $('.load-more').addClass('no-more');
+        }
     }
-}
 
-function ajaxNavigate(newUrl, append,  callback){
+    function ajaxNavigate(newUrl, append, callback){
 
-    var postContainer = $('.posts-container');
+        var postContainer = $('.ajax-content');
+        var filterContainer = $('.ajax-filter');
 
-    $.get( newUrl, function(data){
+        filterContainer.addClass('loading');
+        postContainer.addClass('loading');
 
-        var page = $(data);
-        var contents = page.find(".posts-container").contents();
+        $.get( newUrl, function(data){
 
-        updateNextPostLink(page.find(".posts-navigation .nav-previous a").attr('href'));
+            var page = $(data);
+            var contents = page.find(".ajax-content").contents();
+            var filterItems = page.find(".ajax-filter").contents();
 
-        if(!append){
-            if(postContainer.hasClass('masonry')){
-                postContainer.masonry( 'remove', postContainer.contents() ).masonry('layout');
-            } else {
-                postContainer.empty();
+            filterContainer.empty().append(filterItems);
+
+            updateNextPostLink(page.find(".posts-navigation .nav-previous a").attr('href'));
+
+            if(!append){
+                if(postContainer.hasClass('masonry')){
+                    postContainer.masonry( 'remove', postContainer.contents() ).masonry('layout');
+                } else {
+                    postContainer.empty();
+                }
             }
-        }
 
-        postContainer.append(contents);
+            postContainer.append(contents);
 
-        if(postContainer.hasClass('masonry')){
-            postContainer.imagesLoaded( function() {
-                postContainer.masonry( 'appended', contents );
-            });
-        }
+            if(postContainer.hasClass('masonry')){
+                postContainer.imagesLoaded( function() {
+                    postContainer.masonry( 'appended', contents );
+                });
+            }
 
-        if(typeof callback === 'function'){
-            callback();
-        }
+            filterContainer.removeClass('loading');
+            postContainer.removeClass('loading');
 
+            $( document.body ).trigger( 'post-load' );
+
+            if(typeof callback === 'function'){
+                callback(data);
+            }
+
+        });
+
+    }
+
+    History.Adapter.bind(window,'statechange',function(){
+        var State = History.getState();
+        ajaxNavigate(State.url, State.data.append);
     });
 
-}
 
-History.Adapter.bind(window,'statechange',function(){
-    var State = History.getState();
-    ajaxNavigate(State.url, State.data.append);
-});
+    $( document.body ).on('click','.load-more', function(){
+        var nextPostLink = $('.posts-navigation .nav-previous a').attr('href');
 
+        if ( ! History.pushState({page: nextPostLink, append: true}, document.title, nextPostLink) ) {
+            ajaxNavigate( nextPostLink, true );
+        }
+    });
 
-$('body').on('click','.load-more', function(){
-    var nextPostLink = $('.posts-navigation .nav-previous a').attr('href');
+    $( document.body ).on('click', '.ajax-filter a', function(e){
+        var link = $(this).attr('href');
 
-    if ( ! History.pushState({page: nextPostLink, append: true}, document.title, nextPostLink) ) {
-        ajaxNavigate( nextPostLink, true );
-    }
-});
+        if ( ! History.pushState({page: link, append: false }, document.title, link) ) {
+            ajaxNavigate( link, false, function(data){ e.preventDefault(); } );
+        } else {
+            e.preventDefault();
+        }
+    });
 
-$('.category-filter').on('click', 'a', function(e){
-    var link = $(this).attr('href');
-
-    if ( ! History.pushState({page: link, append: false }, document.title, link) ) {
-        ajaxNavigate( link, false, function(){ e.preventDefault(); } );
-    } else {
-        e.preventDefault();
-    }
-});
+})(jQuery);
