49b7cbd6fdaad67ce48157ec1f96366200c85288
Brando Meniconi <b.meniconi@silverbackstudio.it>
2017-03-02 17:27:14 +0100

    changhed entry-excerpt tag to div in thumb.php

--- template-parts/thumb.php
+++ template-parts/thumb.php
@@ -17,7 +17,7 @@
 
 	<div class="entry-header">
 		<?php the_title( '<h3 class="entry-title"><a href="' . esc_url( get_permalink() ) . '" rel="bookmark">', '</a></h3>' ); ?>
-		<p class="entry-excerpt"><?php the_excerpt(); ?></p>
+		<div class="entry-excerpt"><?php the_excerpt(); ?></div>
 		<a class="readmore" href="<?php the_permalink() ?>" ><?php _e('View More', '_svbk'); ?></a>
 	</div><!-- .entry-header -->
 
