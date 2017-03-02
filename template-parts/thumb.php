a5a890e99771d2b6a30167a6795ab05225f41a73
Brando Meniconi <b.meniconi@silverbackstudio.it>
2017-03-02 16:26:35 +0100

    fixed traslation function output in thumb.php

--- template-parts/thumb.php
+++ template-parts/thumb.php
@@ -18,7 +18,7 @@
 	<div class="entry-header">
 		<?php the_title( '<h3 class="entry-title"><a href="' . esc_url( get_permalink() ) . '" rel="bookmark">', '</a></h3>' ); ?>
 		<p class="entry-excerpt"><?php the_excerpt(); ?></p>
-		<a class="readmore" href="<?php the_permalink() ?>" ><?php __('View More', '_svbk'); ?></a>
+		<a class="readmore" href="<?php the_permalink() ?>" ><?php _e('View More', '_svbk'); ?></a>
 	</div><!-- .entry-header -->
 
 </div><!-- #post-## -->
