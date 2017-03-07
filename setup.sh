bb9c0b07a6e1193fba28fb7d47ff65ad726e51e2
Brando Meniconi <b.meniconi@silverbackstudio.it>
2017-03-07 16:02:53 +0100

    fixed error in js file handle substitution

--- setup.sh
+++ setup.sh
@@ -5,7 +5,7 @@ find ./ -type f -name "*.php"  -exec sed -i "s/'_svbk'/'$THEME_HANDLE'/g" {} \;
 find ./ -type f -name "*.php"  -exec sed -i "s/_svbk_/${THEME_HANDLE}_/g" {} \;
 find ./ -type f -name "*.php"  -exec sed -i "s/ _svbk/ $THEME_HANDLE/g" {} \;
 find ./ -type f -name "*.php"  -exec sed -i "s/_svbk-/$THEME_HANDLE-/g" {} \;
-find ./ -type f -name "*.js"  -exec sed -i "s/_svbk/$THEME_HANDLE-/g" {} \;
+find ./ -type f -name "*.js"  -exec sed -i "s/_svbk/$THEME_HANDLE/g" {} \;
 
 sed -i "s/Text Domain: _svbk/Text Domain: $THEME_HANDLE/g" sass/style.scss
 sed -i "s/Theme Name: _svbk/Theme Name: $THEME_NAME/g" sass/style.scss
