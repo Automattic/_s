18cfff8a12eb2b9edc346548a27fc7f0a68189d3
Brando Meniconi <b.meniconi@silverbackstudio.it>
2017-03-22 15:22:57 +0100

    added smoothscroll

--- js/theme.js
+++ js/theme.js
@@ -5,4 +5,22 @@
     e.preventDefault();
   });
 
+  var smoothScroll = function(){
+    $('a[href*="#"]:not([href="#"])').click(function() {
+      if (location.pathname.replace(/^\//,'') == this.pathname.replace(/^\//,'') && location.hostname == this.hostname) {
+        var target = $(this.hash);
+        target = target.length ? target : $('[name=' + this.hash.slice(1) +']');
+        if (target.length) {
+          $('html, body').animate({
+            scrollTop: target.offset().top
+          }, 500);
+          return false;
+        }
+      }
+    });
+  };
+
+  $(document).ready( smoothScroll );
+  $(document.body).on( 'post-load', smoothScroll );
+
 })(jQuery)
