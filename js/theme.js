50fc970606cecc8081167e98af6e33b4d3a893f2
Brando Meniconi <b.meniconi@silverbackstudio.it>
2017-03-28 18:26:28 +0200

    smoothscroll listens events from body

--- js/theme.js
+++ js/theme.js
@@ -6,18 +6,18 @@
   });
 
   var smoothScroll = function(){
-    $('a[href*="#"]:not([href="#"])').click(function() {
-      if (location.pathname.replace(/^\//,'') == this.pathname.replace(/^\//,'') && location.hostname == this.hostname) {
-        var target = $(this.hash);
-        target = target.length ? target : $('[name=' + this.hash.slice(1) +']');
-        if (target.length) {
-          $('html, body').animate({
-            scrollTop: target.offset().top
-          }, 500);
-          return false;
-        }
-      }
-    });
+      $(document.body).on('click', 'a[href*="#"]:not([href="#"])', function() {
+            if (location.pathname.replace(/^\//,'') == this.pathname.replace(/^\//,'') && location.hostname == this.hostname) {
+              var target = $(this.hash);
+              target = target.length ? target : $('[name=' + this.hash.slice(1) +']');
+              if (target.length) {
+                  $('html, body').animate({
+                      scrollTop: target.offset().top
+                  }, 500);
+                  return false;
+              }
+            }
+      });
   };
 
   $(document).ready( smoothScroll );
