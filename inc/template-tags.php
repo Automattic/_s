23416a918dd5eaf1ffb7440d50ec3acc6cc8a11b
David A. Kennedy <me@davidakennedy.com>
2017-06-09 16:24:18 -0400

    Adjust location of placeholder text

--- inc/template-tags.php
+++ inc/template-tags.php
@@ -84,8 +84,8 @@ function _s_entry_footer() {
 
 	edit_post_link(
 		sprintf(
-			/* translators: %s: Name of current post. Only visible to screen readers */
 			wp_kses(
+				/* translators: %s: Name of current post. Only visible to screen readers */
 				__( 'Edit <span class="screen-reader-text">%s</span>', '_s' ),
 				array(
 					'span' => array(
