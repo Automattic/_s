007f792515fbe85bd91b7ccd58fe6d8ad1f340a3
David A. Kennedy <me@davidakennedy.com>
2017-06-09 16:10:50 -0400

    Fix build errors related to #1066

--- template-parts/content-page.php
+++ template-parts/content-page.php
@@ -31,7 +31,14 @@
 				edit_post_link(
 					sprintf(
 						/* translators: %s: Name of current post. Only visible to screen readers */
-						wp_kses( __( 'Edit <span class="screen-reader-text">%s</span>', '_s' ), array( 'span' => array( 'class' => array() ) ) ),
+						wp_kses(
+							__( 'Edit <span class="screen-reader-text">%s</span>', '_s' ),
+							array(
+								'span' => array(
+									'class' => array()
+								)
+							)
+						),
 						get_the_title()
 					),
 					'<span class="edit-link">',
