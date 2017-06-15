7b8db5678f21186da65945f7697a04745478cd6b
jrfnl <jrfnl@users.noreply.github.com>
2017-06-16 01:17:27 +0200

    Move pingback function back to the `template-functions` file.

--- inc/template-functions.php
+++ inc/template-functions.php
@@ -1,6 +1,6 @@
 <?php
 /**
- * Additional features to allow styling of the templates
+ * Functions which enhance the theme by hooking into WordPress
  *
  * @package _s
  */
@@ -25,3 +25,13 @@ function _s_body_classes( $classes ) {
 	return $classes;
 }
 add_filter( 'body_class', '_s_body_classes' );
+
+/**
+ * Add a pingback url auto-discovery header for singularly identifiable articles.
+ */
+function _s_pingback_header() {
+	if ( is_singular() && pings_open() ) {
+		echo '<link rel="pingback" href="', esc_url( get_bloginfo( 'pingback_url' ) ), '">';
+	}
+}
+add_action( 'wp_head', '_s_pingback_header' );
