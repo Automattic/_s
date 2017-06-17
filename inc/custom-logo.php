c509a9a3859bb5887d41a64e90b9f89b0077fb05
Laurence Bahiirwa <laurence@mediauganda.com>
2017-06-17 18:53:28 +0300

    Bug fix
    Calling the function_exists() from within the function it checks for,
    will always return true. Idea is merge
    https://github.com/Automattic/_s/issues/922 and
    https://github.com/Automattic/_s/pull/925/

--- inc/custom-logo.php
+++ inc/custom-logo.php
@@ -11,8 +11,9 @@
  * Get the custom Logo attachment link
  *
  */
-function _s_the_custom_logo() {
-	if ( function_exists( '_s_the_custom_logo' ) ) {
+
+if ( ! function_exists( '_s_the_custom_logo' ) ) {
+	function _s_the_custom_logo() {
 		$image = wp_get_attachment_image_src( get_theme_mod( 'custom_logo' ), 'full' );
 		echo esc_url( $image[0] );
 	}
