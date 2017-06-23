6a5ad8ee2f69a5a9b432bcbb6a090150fd905217
Thomas Guillot <thomas@automattic.com>
2017-06-23 09:51:26 +0100

    _s: Add basic support for Content Options Post Details
    This allows us to always display the category and author since it can
    be hidden via Customizer -> Content Options
    
    We don’t need to check for `is_multi_author` or `_s_categorized_blog()`
    anymore

--- inc/template-tags.php
+++ inc/template-tags.php
@@ -50,7 +50,7 @@ function _s_entry_footer() {
 	if ( 'post' === get_post_type() ) {
 		/* translators: used between list items, there is a space after the comma */
 		$categories_list = get_the_category_list( esc_html__( ', ', '_s' ) );
-		if ( $categories_list && _s_categorized_blog() ) {
+		if ( $categories_list ) {
 			/* translators: 1: list of categories. */
 			printf( '<span class="cat-links">' . esc_html__( 'Posted in %1$s', '_s' ) . '</span>', $categories_list ); // WPCS: XSS OK.
 		}
@@ -100,47 +100,3 @@ function _s_entry_footer() {
 	);
 }
 endif;
-
-/**
- * Returns true if a blog has more than 1 category.
- *
- * @return bool
- */
-function _s_categorized_blog() {
-	$all_the_cool_cats = get_transient( '_s_categories' );
-	if ( false === $all_the_cool_cats ) {
-		// Create an array of all the categories that are attached to posts.
-		$all_the_cool_cats = get_categories( array(
-			'fields'     => 'ids',
-			'hide_empty' => 1,
-			// We only need to know if there is more than one category.
-			'number'     => 2,
-		) );
-
-		// Count the number of categories that are attached to the posts.
-		$all_the_cool_cats = count( $all_the_cool_cats );
-
-		set_transient( '_s_categories', $all_the_cool_cats );
-	}
-
-	if ( $all_the_cool_cats > 1 || is_preview() ) {
-		// This blog has more than 1 category so _s_categorized_blog should return true.
-		return true;
-	} else {
-		// This blog has only 1 category so _s_categorized_blog should return false.
-		return false;
-	}
-}
-
-/**
- * Flush out the transients used in _s_categorized_blog.
- */
-function _s_category_transient_flusher() {
-	if ( defined( 'DOING_AUTOSAVE' ) && DOING_AUTOSAVE ) {
-		return;
-	}
-	// Like, beat it. Dig?
-	delete_transient( '_s_categories' );
-}
-add_action( 'edit_category', '_s_category_transient_flusher' );
-add_action( 'save_post',     '_s_category_transient_flusher' );
