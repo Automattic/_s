22f4409416ead5d27bd0943f071b6fe21e47dccb
Laurence Bahiirwa <laurence@mediauganda.com>
2017-06-27 10:31:49 +0300

    Travis Build errors fixing
    Empty strings removed for errors fixing

--- inc/custom-logo.php
+++ inc/custom-logo.php
@@ -12,10 +12,7 @@
 /**
  * Function to get the custom Logo attachment link
  */
-
-if ( ! function_exists( '_s_the_custom_logo' ) ) {
-	function _s_the_custom_logo() {
-		$image = wp_get_attachment_image_src( get_theme_mod( 'custom_logo' ), 'full' );
-		echo esc_url( $image[0] );
-	}
+function _s_the_custom_logo() {
+	$image = wp_get_attachment_image_src( get_theme_mod( 'custom_logo' ), 'full' );
+	echo esc_url( $image[0] );
 }
