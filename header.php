8f83cc46d148b694232ff8ff14e8a5abf0958b8a
Bappi <bappi.cs@gmail.com>
2017-07-06 14:37:16 +0600

    proper indent

--- header.php
+++ header.php
@@ -8,15 +8,15 @@
  *
  * @package _s
  */
-
-?><!doctype html>
+?>
+<!doctype html>
 <html <?php language_attributes(); ?>>
 <head>
-<meta charset="<?php bloginfo( 'charset' ); ?>">
-<meta name="viewport" content="width=device-width, initial-scale=1">
-<link rel="profile" href="http://gmpg.org/xfn/11">
+	<meta charset="<?php bloginfo( 'charset' ); ?>">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<link rel="profile" href="http://gmpg.org/xfn/11">
 
-<?php wp_head(); ?>
+	<?php wp_head(); ?>
 </head>
 
 <body <?php body_class(); ?>>
