feddbe049ff91c0429e8e84bb3d2bb4d76b71b38
Sidsector9 <siddharth.thevaril@gmail.com>
2017-07-07 11:52:15 +0530

    GH#1160 Replace underscore with hyphen

--- inc/customizer.php
+++ inc/customizer.php
@@ -50,6 +50,6 @@ function _s_customize_partial_blogdescription() {
  * Binds JS handlers to make Theme Customizer preview reload changes asynchronously.
  */
 function _s_customize_preview_js() {
-	wp_enqueue_script( '_s_customizer', get_template_directory_uri() . '/js/customizer.js', array( 'customize-preview' ), '20151215', true );
+	wp_enqueue_script( '_s-customizer', get_template_directory_uri() . '/js/customizer.js', array( 'customize-preview' ), '20151215', true );
 }
 add_action( 'customize_preview_init', '_s_customize_preview_js' );
