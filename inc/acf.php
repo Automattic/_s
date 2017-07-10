ab1cae591a156dd47da49b42706641754ce446a3
Brando Meniconi <b.meniconi@silverbackstudio.it>
2017-07-10 17:15:19 +0200

    fixed textdomain in acf file

--- inc/acf.php
+++ inc/acf.php
@@ -2,18 +2,20 @@
 use Svbk\WP\Helpers;
 use Svbk\WP\Plugins\PrivateArea;
 
-add_filter('acf/settings/show_admin', 'propertymanagers_acf_show_admin');
+add_filter('acf/settings/show_admin', '_svbk_acf_show_admin');
 
-function propertymanagers_acf_show_admin( $show ) {
+function _svbk_acf_show_admin( $show ) {
     return current_user_can('manage_options');
 }
 
-add_action( 'after_setup_theme', 'propertymanagers_acf_register_fields' );
+add_action( 'after_setup_theme', '_svbk_acf_register_fields' );
 
-function propertymanagers_acf_register_fields(){
+function _svbk_acf_register_fields(){
 
 	if( ! function_exists('acf_add_local_field_group') ){
 		return;
 	}
+
+  /* add fields definitions here */
 
 }
