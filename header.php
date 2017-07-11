be15cf96570f6a961087e6bd7716acf9d0e49484
Bappi <bappi.cs@gmail.com>
2017-07-11 14:10:30 +0600

    Remove blank line in head

--- header.php
+++ header.php
@@ -16,7 +16,6 @@
 	<meta charset="<?php bloginfo( 'charset' ); ?>">
 	<meta name="viewport" content="width=device-width, initial-scale=1">
 	<link rel="profile" href="http://gmpg.org/xfn/11">
-
 	<?php wp_head(); ?>
 </head>
 
