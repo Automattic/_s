4a2e99e6915ae93c380317f9149a74c767ea85c2
Juliette <jrfnl@users.noreply.github.com>
2017-07-14 18:37:12 +0200

    Conditionally include Jetpack compatibility file. (#858)
    Only include the Jetpack file if Jetpack is available on a site

--- functions.php
+++ functions.php
@@ -152,4 +152,6 @@ require get_template_directory() . '/inc/customizer.php';
 /**
  * Load Jetpack compatibility file.
  */
-require get_template_directory() . '/inc/jetpack.php';
+if ( defined( 'JETPACK__VERSION' ) ) {
+	require get_template_directory() . '/inc/jetpack.php';
+}
