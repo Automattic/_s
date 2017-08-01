b26b6ebfa4a92d82a69576e69aefe44192350635
Ulrich Pogson <grapplerulrich@gmail.com>
2017-08-01 22:27:46 +0200

    Move PHP tags on it’s own line & correct indention
    This fixes a few of the errors from the news sniffs in WPCS 0.12.0 and also standardizes the indention between the PHP and HTML

--- footer.php
+++ footer.php
@@ -15,15 +15,17 @@
 
 	<footer id="colophon" class="site-footer">
 		<div class="site-info">
-			<a href="<?php echo esc_url( __( 'https://wordpress.org/', '_s' ) ); ?>"><?php
+			<a href="<?php echo esc_url( __( 'https://wordpress.org/', '_s' ) ); ?>">
+				<?php
 				/* translators: %s: CMS name, i.e. WordPress. */
 				printf( esc_html__( 'Proudly powered by %s', '_s' ), 'WordPress' );
-			?></a>
+				?>
+			</a>
 			<span class="sep"> | </span>
-			<?php
+				<?php
 				/* translators: 1: Theme name, 2: Theme author. */
 				printf( esc_html__( 'Theme: %1$s by %2$s.', '_s' ), '_s', '<a href="https://automattic.com/">Automattic</a>' );
-			?>
+				?>
 		</div><!-- .site-info -->
 	</footer><!-- #colophon -->
 </div><!-- #page -->
