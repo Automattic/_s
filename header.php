69380c52819f663f72ca6c2979a4ebefc53e0369
William Patton <will@pattonwebz.com>
2017-10-02 17:21:01 +0100

    Re-add `banner` and `contentinfo` roles to header and footer elements.

--- header.php
+++ header.php
@@ -24,7 +24,7 @@
 <div id="page" class="site">
 	<a class="skip-link screen-reader-text" href="#content"><?php esc_html_e( 'Skip to content', '_s' ); ?></a>
 
-	<header id="masthead" class="site-header">
+	<header id="masthead" class="site-header" role="banner">
 		<div class="site-branding">
 			<?php
 			the_custom_logo();
