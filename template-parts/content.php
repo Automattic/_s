f99c9b5bd0aeedbec46d3b1991814b1107fac2eb
Ismail El Korchi <ismail.elkorchi@gmail.com>
2017-10-29 21:30:50 +0000

    Fix the indentation of the PHP tags

--- template-parts/content.php
+++ template-parts/content.php
@@ -21,8 +21,8 @@
 		if ( 'post' === get_post_type() ) : ?>
 		<div class="entry-meta">
 			<?php
-			_s_posted_on();
-			_s_posted_by();
+				_s_posted_on();
+				_s_posted_by();
 			?>
 		</div><!-- .entry-meta -->
 		<?php
