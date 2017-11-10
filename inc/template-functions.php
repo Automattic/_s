05b5b79c3d821c90951ee73ae3e2745ac31dd789
Brando Meniconi <amministrazione@silverbackstudio.it>
2017-11-10 10:45:53 +0000

    removed prefixes for categories and taGS

--- inc/template-functions.php
+++ inc/template-functions.php
@@ -35,3 +35,18 @@ function _svbk_pingback_header() {
 	}
 }
 add_action( 'wp_head', '_svbk_pingback_header' );
+
+add_filter( 'get_the_archive_title', '_svbk_archive_title');
+
+function _svbk_archive_title($title) {
+
+    if ( is_category() ) {
+        $title = single_cat_title( '', false ) ;
+    } elseif ( is_tag() ) {
+        $title = single_tag_title( '', false );
+    } elseif ( is_author() ) {
+        $title = '<span class="vcard">' . get_the_author() . '</span>' ;
+    }
+
+    return $title;
+}
