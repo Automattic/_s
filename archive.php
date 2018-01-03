e020eb63f3645a6a31c687ebc2501c0f5e310c83
Laurence Bahiirwa <laurence@mediauganda.com>
2018-01-03 08:31:42 +0300

    Css grid (#1)
    * Remove #content div (superfluous).
    
    * Remove #primary
    
    * Remove #primary, take 2
    
    * Change primary layout module to CSS grid.
    
    * Remove ::before / ::after based clearfixes. When using flex and grid, pseudo-elements are considered flex/grid elements and cause unexpected behavior. Clearfix is a tool to resolve clearing when using float-based layouts. Modern flex/grid-based layouts do not need them.
    
    * Main menu: Use flex as layout module.
    
    * Main menu cleanup.
    
    * Comments, Posts, and Post Navigation: Use flex for layout. Automatic RTL.

--- archive.php
+++ archive.php
@@ -9,42 +9,40 @@
 
 get_header(); ?>
 
-	<div id="primary" class="content-area">
-		<main id="main" class="site-main">
+	<main id="main" class="site-main">
+
+	<?php
+	if ( have_posts() ) : ?>
+
+		<header class="page-header">
+			<?php
+				the_archive_title( '<h1 class="page-title">', '</h1>' );
+				the_archive_description( '<div class="archive-description">', '</div>' );
+			?>
+		</header><!-- .page-header -->
 
 		<?php
-		if ( have_posts() ) : ?>
+		/* Start the Loop */
+		while ( have_posts() ) : the_post();
 
-			<header class="page-header">
-				<?php
-					the_archive_title( '<h1 class="page-title">', '</h1>' );
-					the_archive_description( '<div class="archive-description">', '</div>' );
-				?>
-			</header><!-- .page-header -->
+			/*
+				* Include the Post-Format-specific template for the content.
+				* If you want to override this in a child theme, then include a file
+				* called content-___.php (where ___ is the Post Format name) and that will be used instead.
+				*/
+			get_template_part( 'template-parts/content', get_post_format() );
 
-			<?php
-			/* Start the Loop */
-			while ( have_posts() ) : the_post();
+		endwhile;
 
-				/*
-				 * Include the Post-Format-specific template for the content.
-				 * If you want to override this in a child theme, then include a file
-				 * called content-___.php (where ___ is the Post Format name) and that will be used instead.
-				 */
-				get_template_part( 'template-parts/content', get_post_format() );
+		the_posts_navigation();
 
-			endwhile;
+	else :
 
-			the_posts_navigation();
+		get_template_part( 'template-parts/content', 'none' );
 
-		else :
+	endif; ?>
 
-			get_template_part( 'template-parts/content', 'none' );
-
-		endif; ?>
-
-		</main><!-- #main -->
-	</div><!-- #primary -->
+	</main><!-- #main -->
 
 <?php
 get_sidebar();
--- index.php
+++ index.php
@@ -14,42 +14,40 @@
 
 get_header(); ?>
 
-	<div id="primary" class="content-area">
-		<main id="main" class="site-main">
+	<main id="main" class="site-main">
+
+	<?php
+	if ( have_posts() ) :
+
+		if ( is_home() && ! is_front_page() ) : ?>
+			<header>
+				<h1 class="page-title screen-reader-text"><?php single_post_title(); ?></h1>
+			</header>
 
 		<?php
-		if ( have_posts() ) :
+		endif;
 
-			if ( is_home() && ! is_front_page() ) : ?>
-				<header>
-					<h1 class="page-title screen-reader-text"><?php single_post_title(); ?></h1>
-				</header>
+		/* Start the Loop */
+		while ( have_posts() ) : the_post();
 
-			<?php
-			endif;
+			/*
+				* Include the Post-Format-specific template for the content.
+				* If you want to override this in a child theme, then include a file
+				* called content-___.php (where ___ is the Post Format name) and that will be used instead.
+				*/
+			get_template_part( 'template-parts/content', get_post_format() );
 
-			/* Start the Loop */
-			while ( have_posts() ) : the_post();
+		endwhile;
 
-				/*
-				 * Include the Post-Format-specific template for the content.
-				 * If you want to override this in a child theme, then include a file
-				 * called content-___.php (where ___ is the Post Format name) and that will be used instead.
-				 */
-				get_template_part( 'template-parts/content', get_post_format() );
+		the_posts_navigation();
 
-			endwhile;
+	else :
 
-			the_posts_navigation();
+		get_template_part( 'template-parts/content', 'none' );
 
-		else :
+	endif; ?>
 
-			get_template_part( 'template-parts/content', 'none' );
-
-		endif; ?>
-
-		</main><!-- #main -->
-	</div><!-- #primary -->
+	</main><!-- #main -->
 
 <?php
 get_sidebar();
