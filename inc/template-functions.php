7546de2599d00dbc1fc0c088b848d46d7ebc7858
William Patton <will@pattonwebz.com>
2018-03-12 00:55:42 +0000

    Wrap more theme functions inside function_exists() check

--- inc/template-functions.php
+++ inc/template-functions.php
@@ -5,28 +5,32 @@
  * @package _s
  */
 
-/**
- * Adds custom classes to the array of body classes.
- *
- * @param array $classes Classes for the body element.
- * @return array
- */
-function _s_body_classes( $classes ) {
-	// Adds a class of hfeed to non-singular pages.
-	if ( ! is_singular() ) {
-		$classes[] = 'hfeed';
-	}
+if ( ! function_exists( '_s_body_classes' ) ) :
+	/**
+	 * Adds custom classes to the array of body classes.
+	 *
+	 * @param array $classes Classes for the body element.
+	 * @return array
+	 */
+	function _s_body_classes( $classes ) {
+		// Adds a class of hfeed to non-singular pages.
+		if ( ! is_singular() ) {
+			$classes[] = 'hfeed';
+		}
 
-	return $classes;
-}
+		return $classes;
+	}
+endif;
 add_filter( 'body_class', '_s_body_classes' );
 
-/**
- * Add a pingback url auto-discovery header for single posts, pages, or attachments.
- */
-function _s_pingback_header() {
-	if ( is_singular() && pings_open() ) {
-		echo '<link rel="pingback" href="', esc_url( get_bloginfo( 'pingback_url' ) ), '">';
+if ( ! function_exists( '_s_pingback_header' ) ) :
+	/**
+	 * Add a pingback url auto-discovery header for single posts, pages, or attachments.
+	 */
+	function _s_pingback_header() {
+		if ( is_singular() && pings_open() ) {
+			echo '<link rel="pingback" href="', esc_url( get_bloginfo( 'pingback_url' ) ), '">';
+		}
 	}
-}
+endif;
 add_action( 'wp_head', '_s_pingback_header' );
--- inc/wpcom.php
+++ inc/wpcom.php
@@ -7,25 +7,27 @@
  * @package _s
  */
 
-/**
- * Adds support for wp.com-specific theme functions.
- *
- * @global array $themecolors
- */
-function _s_wpcom_setup() {
-	global $themecolors;
+if ( ! function_exists( '_s_wpcom_setup' ) ) :
+	/**
+	 * Adds support for wp.com-specific theme functions.
+	 *
+	 * @global array $themecolors
+	 */
+	function _s_wpcom_setup() {
+		global $themecolors;
 
-	// Set theme colors for third party services.
-	if ( ! isset( $themecolors ) ) {
-		// Whitelist wpcom specific variable intended to be overruled.
-		// phpcs:ignore WordPress.NamingConventions.PrefixAllGlobals.NonPrefixedVariableFound
-		$themecolors = array(
-			'bg'     => '',
-			'border' => '',
-			'text'   => '',
-			'link'   => '',
-			'url'    => '',
-		);
+		// Set theme colors for third party services.
+		if ( ! isset( $themecolors ) ) {
+			// Whitelist wpcom specific variable intended to be overruled.
+			// phpcs:ignore WordPress.NamingConventions.PrefixAllGlobals.NonPrefixedVariableFound
+			$themecolors = array(
+				'bg'     => '',
+				'border' => '',
+				'text'   => '',
+				'link'   => '',
+				'url'    => '',
+			);
+		}
 	}
-}
+endif;
 add_action( 'after_setup_theme', '_s_wpcom_setup' );
