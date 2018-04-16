c1cde9c9a2976ec38da4f0f7e5739ee1c7838aa9
Brando Meniconi <amministrazione@silverbackstudio.it>
2018-04-16 13:37:28 +0000

    updated theme namespache schema

--- setup.sh
+++ setup.sh
@@ -1,6 +1,9 @@
 read -p "Theme name: " THEME_NAME
 read -p "Theme handle: " THEME_HANDLE
 
+# Upper case first svbk -> Svbk
+THEME_HANDLE_UCF="$(tr '[:lower:]' '[:upper:]' <<< ${THEME_HANDLE:0:1})${THEME_HANDLE:1}"
+
 find ./ -type f -name "*.php"  -exec sed -i "s/'_svbk'/'$THEME_HANDLE'/g" {} \;
 find ./ -type f -name "*.php"  -exec sed -i "s/_svbk_/${THEME_HANDLE}_/g" {} \;
 find ./ -type f -name "*.php"  -exec sed -i "s/ _svbk/ $THEME_HANDLE/g" {} \;
@@ -11,6 +14,7 @@ sed -i "s/Text Domain: _svbk/Text Domain: $THEME_HANDLE/g" sass/style.scss
 sed -i "s/Theme Name: _svbk/Theme Name: $THEME_NAME/g" sass/style.scss
 
 find ./ -type f -name "*.json"  -exec sed -i "s/_svbk/$THEME_HANDLE/g" {} \;
+find ./ -type f -name "*.json"  -exec sed -i "s/Svbk\\\\/$THEME_HANDLE_UCF\\\\/g" {} \;
 find ./ -type f -name "*.json"  -exec sed -i "s/Silverback Starter/Silverback $THEME_NAME/g" {} \;
 
 mv languages/_svbk.pot languages/$THEME_HANDLE.pot
