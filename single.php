8878f1ad7d260d8e25af988e66c0b0f6366c8d98
alaczek <ola@bodera.com>
2018-05-29 11:57:18 +1000

    Removing duplicate entry in the translation file. Switching the translation function for single post navigation to esc_html__

--- single.php
+++ single.php
@@ -18,8 +18,8 @@ get_header(); ?>
 			get_template_part( 'template-parts/content', get_post_type() );
 
 			the_post_navigation( array(
-				'prev_text' => '<span class="nav-subtitle">' . __( 'Previous:', '_s' ) . '</span> <span class="nav-title">%title</span>',
-				'next_text' => '<span class="nav-subtitle">' . __( 'Next:', '_s' ) . '</span> <span class="nav-title">%title</span>',
+				'prev_text' => '<span class="nav-subtitle">' . esc_html__( 'Previous:', '_s' ) . '</span> <span class="nav-title">%title</span>',
+				'next_text' => '<span class="nav-subtitle">' . esc_html__( 'Next:', '_s' ) . '</span> <span class="nav-title">%title</span>',
 			) );
 
 			// If comments are open or we have at least one comment, load up the comment template.
