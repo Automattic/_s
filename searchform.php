29577b51bcb6daecc393d89a54007301e74a2887
Brando Meniconi <amministrazione@silverbackstudio.it>
2018-10-26 12:14:16 +0000

    added context aware search, fixed search placeholder

--- searchform.php
+++ searchform.php
@@ -1,7 +1,17 @@
-<form role="search" method="get" class="search-form" action="<?php esc_url( home_url( '/' ) ) ?>">
+<?php 
+if ( 'page' === get_option( 'show_on_front' ) && ( is_home() || is_single() || is_archive() ) ) {
+	$action = get_permalink( get_option( 'page_for_posts' ) );
+} elseif ( is_post_type_archive() || ( is_singular() && get_post_type_archive_link( get_post_type() ) ) ) {
+	$action = get_post_type_archive_link( get_post_type() );
+} else {
+	$action = get_home_url( '/' );
+}
+?>
+
+<form role="search" method="get" class="search-form" action="<?php echo esc_url( $action ); ?>">
 	<label>
 		<span class="screen-reader-text"><?php _x( 'Search for:', 'label' ) ?></span>
-		<input type="search" class="search-field" placeholder="<?php esc_attr_x( 'Search &hellip;', 'placeholder' ) ?>" value="<?php get_search_query() ?>" name="s" />
+		<input type="search" class="search-field" placeholder="<?php echo esc_attr_x( 'Search &hellip;', 'placeholder' ) ?>" value="<?php get_search_query() ?>" name="s" />
 	</label>
 	<button type="submit" class="search-submit" ><span class="button-label"><?php esc_html_e( 'Search', 'submit button' ) ?></span></button>
 </form>
