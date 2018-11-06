42560ee8964d356cd5f75f3eeb6b01a120a68e62
Brando Meniconi <amministrazione@silverbackstudio.it>
2018-11-06 14:00:14 +0000

    added posts wrapper to archive template

--- archive.php
+++ archive.php
@@ -22,23 +22,25 @@ get_header();
 				?>
 			</header><!-- .page-header -->
 
-			<?php
-			/* Start the Loop */
-			while ( have_posts() ) :
-				the_post();
+			<div class="posts-list post-thumbs">
+				<?php
+				/* Start the Loop */
+				while ( have_posts() ) :
+					the_post();
+	
+					/*
+					 * Include the Post-Type-specific template for the content.
+					 * If you want to override this in a child theme, then include a file
+					 * called content-___.php (where ___ is the Post Type name) and that will be used instead.
+					 */
+					get_template_part( 'template-parts/preview', get_post_type() );
+	
+				endwhile; ?>
+			</div>
+			
+			<?php the_posts_navigation(); ?>
 
-				/*
-				 * Include the Post-Type-specific template for the content.
-				 * If you want to override this in a child theme, then include a file
-				 * called content-___.php (where ___ is the Post Type name) and that will be used instead.
-				 */
-				get_template_part( 'template-parts/preview', get_post_type() );
-
-			endwhile;
-
-			the_posts_navigation();
-
-		else :
+		<?php else :
 
 			get_template_part( 'template-parts/content', 'none' );
 
