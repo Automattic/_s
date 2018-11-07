a1459ccd8a7ca96e8cda71e8cbd9d2c6f33f1eb3
Brando Meniconi <amministrazione@silverbackstudio.it>
2018-11-07 13:39:46 +0000

    fixed the style.css path in setup.sh

--- setup.sh
+++ setup.sh
@@ -10,8 +10,8 @@ find ./ -type f -name "*.php"  -exec sed -i "s/ _svbk/ $THEME_HANDLE/g" {} \;
 find ./ -type f -name "*.php"  -exec sed -i "s/_svbk-/$THEME_HANDLE-/g" {} \;
 find ./ -type f -name "*.js"  -exec sed -i "s/_svbk/$THEME_HANDLE/g" {} \;
 
-sed -i "s/Text Domain: _svbk/Text Domain: $THEME_HANDLE/g" sass/style.scss
-sed -i "s/Theme Name: _svbk/Theme Name: $THEME_NAME/g" sass/style.scss
+sed -i "s/Text Domain: _svbk/Text Domain: $THEME_HANDLE/g" ./style.css
+sed -i "s/Theme Name: _svbk/Theme Name: $THEME_NAME/g" ./style.css
 
 find ./ -type f -name "*.json"  -exec sed -i "s/_svbk/$THEME_HANDLE/g" {} \;
 find ./ -type f -name "*.json"  -exec sed -i "s/Svbk\\\\/$THEME_HANDLE_UCF\\\\/g" {} \;
