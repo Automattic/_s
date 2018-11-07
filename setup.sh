dcdb2e0e27a0ad67a7a1d115dfeeb37ed4a7213a
Brando Meniconi <amministrazione@silverbackstudio.it>
2018-11-07 16:52:24 +0000

    replace <_svbk> in PHP files and rtl.css

--- setup.sh
+++ setup.sh
@@ -8,10 +8,11 @@ find ./ -type f -name "*.php"  -exec sed -i "s/'_svbk'/'$THEME_HANDLE'/g" {} \;
 find ./ -type f -name "*.php"  -exec sed -i "s/_svbk_/${THEME_HANDLE}_/g" {} \;
 find ./ -type f -name "*.php"  -exec sed -i "s/ _svbk/ $THEME_HANDLE/g" {} \;
 find ./ -type f -name "*.php"  -exec sed -i "s/_svbk-/$THEME_HANDLE-/g" {} \;
-find ./ -type f -name "*.js"  -exec sed -i "s/_svbk/$THEME_HANDLE/g" {} \;
+find ./ -type f -name "*.php"  -exec sed -i "s/_svbk/$THEME_HANDLE/g" {} \;
 
 sed -i "s/Text Domain: _svbk/Text Domain: $THEME_HANDLE/g" ./style.css
 sed -i "s/Theme Name: _svbk/Theme Name: $THEME_NAME/g" ./style.css
+sed -i "s/Theme Name: _svbk/Theme Name: $THEME_NAME/g" ./rtl.css
 
 find ./ -type f -name "*.json"  -exec sed -i "s/_svbk/$THEME_HANDLE/g" {} \;
 find ./ -type f -name "*.json"  -exec sed -i "s/Svbk\\\\/$THEME_HANDLE_UCF\\\\/g" {} \;
