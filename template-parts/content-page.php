38db20a49ea705e7a447a92d71411f2a47b71758
Brando Meniconi <amministrazione@silverbackstudio.it>
2018-11-20 16:09:37 +0000

    remove page title by default

--- template-parts/content-page.php
+++ template-parts/content-page.php
@@ -11,7 +11,7 @@
 
 <article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
 	<header class="page__header entry-header">
-		<?php the_title( '<h1 class="page__title entry-title">', '</h1>' ); ?>
+		<?php //the_title( '<h1 class="page__title entry-title">', '</h1>' ); ?>
 	</header><!-- .entry-header -->
 
 	<div class="page__content entry-content">
