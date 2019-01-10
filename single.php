8fa25074bbdb611fb9ebffd0c61943d8a4030462
Tazo Todua <tazotodua@gmail.com>
2019-01-10 21:37:47 +0400

    Update single.php
    Added hooks

--- single.php
+++ single.php
@@ -13,12 +13,16 @@ get_header();
 	<div id="primary" class="content-area">
 		<main id="main" class="site-main">
 
+		<?php do_action( "_s_single_head" ); ?>
+			
 		<?php
 		while ( have_posts() ) :
 			the_post();
 
 			get_template_part( 'template-parts/content', get_post_type() );
 
+			do_action( "_s_single_middle" );
+			
 			the_post_navigation();
 
 			// If comments are open or we have at least one comment, load up the comment template.
@@ -29,6 +33,8 @@ get_header();
 		endwhile; // End of the loop.
 		?>
 
+		<?php do_action( "_s_single_foot" ); ?>
+			
 		</main><!-- #main -->
 	</div><!-- #primary -->
 
