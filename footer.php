2c65e6e046e68c251e317687934b4fa4e92766be
Tazo Todua <tazotodua@gmail.com>
2019-01-10 21:18:26 +0400

    Update footer.php
    Added hooks

--- footer.php
+++ footer.php
@@ -14,19 +14,24 @@
 	</div><!-- #content -->
 
 	<footer id="colophon" class="site-footer">
-		<div class="site-info">
-			<a href="<?php echo esc_url( __( 'https://wordpress.org/', '_s' ) ); ?>">
+		
+		<?php do_action("_s_footer_middle");?>
+		
+		<div class="site-info"> 
+			
+			<a href="<?php echo esc_url( apply_filters( "footer_site_info_url", __( 'https://wordpress.org/', '_s' ) ) ); ?>">
 				<?php
 				/* translators: %s: CMS name, i.e. WordPress. */
-				printf( esc_html__( 'Proudly powered by %s', '_s' ), 'WordPress' );
+				echo apply_filters( "footer_site_info_text", sprintf( esc_html__( 'Proudly powered by %s', '_s' ), 'WordPress' ) );
 				?>
 			</a>
 			<span class="sep"> | </span>
 				<?php
 				/* translators: 1: Theme name, 2: Theme author. */
-				printf( esc_html__( 'Theme: %1$s by %2$s.', '_s' ), '_s', '<a href="https://automattic.com/">Automattic</a>' );
+				echo apply_filters( "footer_site_info_text", sprintf( esc_html__( 'Theme: %1$s by %2$s.', '_s' ), '_s', '<a href="https://automattic.com/">Automattic</a>' ) );
 				?>
-		</div><!-- .site-info -->
+		</div><!-- .site-info --> 
+		
 	</footer><!-- #colophon -->
 </div><!-- #page -->
 
