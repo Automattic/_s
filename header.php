e38914003231fbe0a75f6ed3690890d1dbca2513
Tazo Todua <tazotodua@gmail.com>
2019-01-10 21:24:42 +0400

    Update header.php
    Added hooks

--- header.php
+++ header.php
@@ -21,29 +21,40 @@
 </head>
 
 <body <?php body_class(); ?>>
+	
+<?php do_action( "_s_site_head" );?>
+	
 <div id="page" class="site">
+
+	<?php do_action( "_s_main_head" );?>
+	
 	<a class="skip-link screen-reader-text" href="#content"><?php esc_html_e( 'Skip to content', '_s' ); ?></a>
 
 	<header id="masthead" class="site-header">
+		
+		<?php do_action( "_s_masthead_head" );?>
+		
 		<div class="site-branding">
 			<?php
 			the_custom_logo();
 			if ( is_front_page() && is_home() ) :
 				?>
-				<h1 class="site-title"><a href="<?php echo esc_url( home_url( '/' ) ); ?>" rel="home"><?php bloginfo( 'name' ); ?></a></h1>
+				<h1 class="site-title"><a href="<?php echo esc_url( home_url( '/' ) ); ?>" rel="home"><?php echo apply_filter( "_s_head_bloginfo", get_bloginfo( 'name' ) ); ?></a></h1>
 				<?php
 			else :
 				?>
-				<p class="site-title"><a href="<?php echo esc_url( home_url( '/' ) ); ?>" rel="home"><?php bloginfo( 'name' ); ?></a></p>
+				<p class="site-title"><a href="<?php echo esc_url( home_url( '/' ) ); ?>" rel="home"><?php echo apply_filter( "_s_head_bloginfo", get_bloginfo( 'name' ) ); ?></a></p>
 				<?php
 			endif;
-			$_s_description = get_bloginfo( 'description', 'display' );
+			$_s_description =  echo apply_filter( "_s_head_description", get_bloginfo( 'description', 'display' ) );
 			if ( $_s_description || is_customize_preview() ) :
 				?>
 				<p class="site-description"><?php echo $_s_description; /* WPCS: xss ok. */ ?></p>
 			<?php endif; ?>
 		</div><!-- .site-branding -->
 
+		<?php do_action( "_s_masthead_middle" );?>
+		
 		<nav id="site-navigation" class="main-navigation">
 			<button class="menu-toggle" aria-controls="primary-menu" aria-expanded="false"><?php esc_html_e( 'Primary Menu', '_s' ); ?></button>
 			<?php
@@ -53,6 +64,11 @@
 			) );
 			?>
 		</nav><!-- #site-navigation -->
+		
+		<?php do_action( "_s_masthead_foot" );?>
+		
 	</header><!-- #masthead -->
 
 	<div id="content" class="site-content">
+
+		<?php do_action( "_s_content_head" );?>
