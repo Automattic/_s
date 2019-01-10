b40b252a5b6f2f2b0623abe4bb2343bd51463bd7
Tazo Todua <tazotodua@gmail.com>
2019-01-10 21:46:31 +0400

    Update content-none.php
    Added hooks

--- template-parts/content-none.php
+++ template-parts/content-none.php
@@ -10,11 +10,15 @@
 ?>
 
 <section class="no-results not-found">
+	
 	<header class="page-header">
 		<h1 class="page-title"><?php esc_html_e( 'Nothing Found', '_s' ); ?></h1>
 	</header><!-- .page-header -->
-
+	
 	<div class="page-content">
+
+		<?php do_action( "_s_content_none_head" );?>
+		
 		<?php
 		if ( is_home() && current_user_can( 'publish_posts' ) ) :
 
@@ -34,18 +38,22 @@
 		elseif ( is_search() ) :
 			?>
 
-			<p><?php esc_html_e( 'Sorry, but nothing matched your search terms. Please try again with some different keywords.', '_s' ); ?></p>
+			<p><?php echo apply_filter( "_s_content_none_nothing_matched", esc_html( 'Sorry, but nothing matched your search terms. Please try again with some different keywords.', '_s' ) ); ?></p>
 			<?php
 			get_search_form();
 
 		else :
 			?>
 
-			<p><?php esc_html_e( 'It seems we can&rsquo;t find what you&rsquo;re looking for. Perhaps searching can help.', '_s' ); ?></p>
+			<p><?php echo apply_filter( "_s_content_none_empty_page", esc_html( 'It seems we can&rsquo;t find what you&rsquo;re looking for. Perhaps searching can help.', '_s' ); ?></p>
 			<?php
 			get_search_form();
 
 		endif;
 		?>
+	
+		<?php do_action( "_s_content_none_foot" );?>
+		
 	</div><!-- .page-content -->
+	
 </section><!-- .no-results -->
