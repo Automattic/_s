54323a01817c971ca05ad5ac8541ef2fc3c0e598
Tazo Todua <tazotodua@gmail.com>
2019-01-10 21:40:38 +0400

    Update header.php
    Added hooks

--- header.php
+++ header.php
@@ -56,7 +56,7 @@
 		<?php do_action( "_s_masthead_middle" );?>
 		
 		<nav id="site-navigation" class="main-navigation">
-			<button class="menu-toggle" aria-controls="primary-menu" aria-expanded="false"><?php esc_html_e( 'Primary Menu', '_s' ); ?></button>
+			<button class="menu-toggle" aria-controls="primary-menu" aria-expanded="false"><?php echo apply_filter( "_s_primary_menu_text", esc_html( 'Primary Menu', '_s' ) ); ?></button>
 			<?php
 			wp_nav_menu( array(
 				'theme_location' => 'menu-1',
