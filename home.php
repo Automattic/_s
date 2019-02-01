1726300ba5128a658ec91a00f4fbb1b4407837b9
Brando Meniconi <amministrazione@silverbackstudio.it>
2019-02-01 11:16:19 +0000

    remove ajax container from blog

--- home.php
+++ home.php
@@ -39,23 +39,22 @@ get_header(); ?>
 					</ul>
 					<?php endif; ?>
 
-					<div class="ajax-content">
-						<div class="posts-list post-thumbs">
-					<?php
-
-					/* Start the Loop */
-					while ( have_posts() ) :
-						the_post();
-
-						/*
-						 * Include the Post-Format-specific template for the content.
-						 * If you want to override this in a child theme, then include a file
-						 * called content-___.php (where ___ is the Post Format name) and that will be used instead.
-						 */
-						get_template_part( 'template-parts/preview', get_post_type() );
-
-					endwhile;
-					?>
+					<div class="posts-list post-thumbs">
+						<?php
+	
+						/* Start the Loop */
+						while ( have_posts() ) :
+							the_post();
+	
+							/*
+							 * Include the Post-Format-specific template for the content.
+							 * If you want to override this in a child theme, then include a file
+							 * called content-___.php (where ___ is the Post Format name) and that will be used instead.
+							 */
+							get_template_part( 'template-parts/preview', get_post_type() );
+	
+						endwhile;
+						?>
 					</div>
 					<?php
 					the_posts_pagination(
@@ -65,13 +64,12 @@ get_header(); ?>
 						)
 					);
 					?>
-				</div>
 					<?php
-				else :
-					get_template_part( 'template-parts/content', 'none' );
-
-				endif;
-				?>
+					else :
+						get_template_part( 'template-parts/content', 'none' );
+	
+					endif;
+					?>
 
 		</main><!-- #main -->
 	</div><!-- #primary -->
