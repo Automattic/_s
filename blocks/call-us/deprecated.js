bca849c60b71e2f5fb50388e8d98039c74787993
Brando Meniconi <amministrazione@silverbackstudio.it>
2019-05-14 06:59:30 +0000

    add mobile-desktop call bottun to call-us blcok

---
+++ blocks/call-us/deprecated.js
@@ -0,0 +1,75 @@
+
+/**
+ * WordPress dependencies
+ */
+const { __ } = wp.i18n;
+
+const { 
+    Button,
+
+} = wp.components;
+
+const deprecated = [
+    {
+		attributes: {
+			callus_title: {
+				type: 'string',
+				source: 'text',
+				selector: '.callus__title',
+				default: __( 'Preferisci parlarne a voce?', '_svbk' )
+			},		
+			callus_text: {
+				type: 'string',
+				source: 'text',
+				selector: '.callus__text',
+				default: __( 'Chiamaci al numero', '_svbk' )
+			},
+			callus_number: {
+				type: 'string',
+				source: 'text',
+				selector: '.callus__number',
+				default: __( '(+39) 000 00000000', '_svbk' )
+			}
+		},      	
+
+        supports: {
+        	html: false,
+        },
+        
+        multiple: false,
+
+		migrate( attributes ) {
+			const { 
+				callus_title,
+				callus_text,
+				callus_number,
+			} = attributes;
+
+            const repls = /\(|\)|\+39|\s/gi;
+
+			return {
+				title: callus_title,
+				text: callus_text,
+				number: callus_number.replace(repls, ''),
+				action: __( 'Chiamaci', '_svbk' )
+			};
+		},
+		
+		save({ attributes }){
+			
+			const { callus_title, callus_text, callus_number, className } = attributes;
+			
+			var callus_href = callus_number.replace("+", "00").replace(/[\s\(\)]/g, "");
+			
+			return (
+				<div className={ className }>
+					<h3 className="callus__title">{callus_title}</h3>
+					<p className="callus__text">{callus_text}</p>
+					<a href={"tel:" + callus_href} className="callus__number">{callus_number}</a>
+				</div>
+			);
+		},
+    }
+];
+
+export default deprecated;
