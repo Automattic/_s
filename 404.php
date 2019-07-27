4537a61e798ae722d15fc09c2fbf6144550729cb
Hetvi Patel <hetvi.patel@espl.com>
2019-07-27 12:01:33 +0530

    404 change

--- 404.php
+++ 404.php
@@ -15,7 +15,7 @@ get_header();
 
 			<section class="error-404 not-found">
 				<header class="page-header">
-					<h1 class="page-title"><?php esc_html_e( 'Ooops! That page can&rsquo;t be found.', 'demo' ); ?></h1>
+					<h1 class="page-title"><?php esc_html_e( 'Oops! That page can&rsquo;t be found.', 'demo' ); ?></h1>
 				</header><!-- .page-header -->
 
 				<div class="page-content">
