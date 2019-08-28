93439be681c93eb8ae68344f0f106639af92f131
Kajal Dhameliya <test@test.mailcom>
2019-08-28 17:56:46 +0530

    index demo123

--- index.php
+++ index.php
@@ -16,7 +16,7 @@ get_header();
 ?>
 
 	<div id="primary" class="content-area">
-		<main id="main" class="site-main demo12">
+		<main id="main" class="site-main demo123">
 
 		<?php
 		if ( have_posts() ) :
