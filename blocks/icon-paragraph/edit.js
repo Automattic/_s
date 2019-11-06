beffc0b4b869633ea16ba890e1d8033aea97d837
Costanza <c.focardi@silverbackstudio.it>
2019-11-06 14:48:01 +0000

    icon-paragraph: add font-size control + css

--- blocks/icon-paragraph/edit.js
+++ blocks/icon-paragraph/edit.js
@@ -5,24 +5,37 @@
  * WordPress dependencies
  */
 
-const { Component } = wp.element;
+const { Fragment, Component } = wp.element;
 const { __ } = wp.i18n; // Import __() from wp.i18n
 const { 
 	RichText,
 	BlockControls,
 	AlignmentToolbar,
+	FontSizePicker,
+	withFontSizes,
+	InspectorControls
 } = wp.blockEditor;
+
+const {
+	PanelBody
+} = wp.components;
+
+const { compose } = wp.compose;
 
 
 import IconEditor from '../icon/editor';
+import classnames from 'classnames';
 
-class BulletEdit extends Component {
+class IconParagraphEdit extends Component {
 
     render() {
     	
 		const {
 			attributes,
 			setAttributes,
+			className,
+			fontSize,
+			setFontSize,
 		} = this.props;
 
 		const { 
@@ -30,33 +43,50 @@ class BulletEdit extends Component {
 			content, 
 		} = attributes;
 		
+
+		const classNames = classnames( className, {
+			[ fontSize.class ]: fontSize.class,			
+		} );
 		
 		const style = {
 			textAlign: align,
+			fontSize: fontSize.size ? fontSize.size + 'px' : undefined,
 		};
 		
 		return (
-			<div className={ 'wp-block-svbk-icon-paragraph' }>
-				<IconEditor { ...this.props } className={ 'wp-block-svbk-icon-paragraph__icon' } />
-				<RichText
-					tagName={ 'div' }
-					value={ content }
-					style={ style }
-					onChange={ ( value ) => setAttributes( { content: value } ) }
-					placeholder={ __( 'Content..', '_svbk' ) }
-					className={ 'wp-block-svbk-icon-paragraph__content' }
-				/>
-                <BlockControls>
-					<AlignmentToolbar
-						value={ align }
-						onChange={ ( nextAlign ) => {
-							setAttributes( { align: nextAlign } );
-						} }
-					/>				
-				</BlockControls>
-			</div>
+			<Fragment>
+				<div className={ 'wp-block-svbk-icon-paragraph' }>
+					<IconEditor { ...this.props } className={ 'wp-block-svbk-icon-paragraph__icon' } />
+					<RichText
+						tagName={ 'div' }
+						value={ content }
+						style={ style }
+						onChange={ ( value ) => setAttributes( { content: value } ) }
+						placeholder={ __( 'Content..', '_svbk' ) }
+						className={ 'wp-block-svbk-icon-paragraph__content' }
+					/>
+					<BlockControls>
+						<AlignmentToolbar
+							value={ align }
+							onChange={ ( nextAlign ) => {
+								setAttributes( { align: nextAlign } );
+							} }
+						/>				
+					</BlockControls>
+					<InspectorControls>
+						<PanelBody title={ __( 'Text Settings' ) } className="blocks-font-size">
+							<FontSizePicker
+								value={ fontSize.size }
+								onChange={ setFontSize }
+							/>
+						</PanelBody>				
+					</InspectorControls>	
+				</div>
+			</Fragment>
 		);
 	}
 }
 
-export default BulletEdit;
+export default compose( [
+	withFontSizes( 'fontSize' )
+] )( IconParagraphEdit );
