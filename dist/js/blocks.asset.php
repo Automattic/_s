e613d18aa40ded7421e98d5e3a4ee9221e628c85
Costanza <c.focardi@silverbackstudio.it>
2020-02-03 13:27:49 +0000

    hero block: set responsive width

--- dist/js/blocks.asset.php
+++ dist/js/blocks.asset.php
@@ -1,5 +1,1 @@
-<<<<<<< HEAD
-<?php return array('dependencies' => array('react', 'react-dom', 'wp-element', 'wp-polyfill'), 'version' => 'ff963ba2b929e58c3295f4e6ad451480');
-=======
-<?php return array('dependencies' => array('react', 'react-dom', 'wp-element', 'wp-polyfill'), 'version' => '57c9dcd67ea36cc2097922f26a2b5912');
->>>>>>> e0ebf45b587ac289aa3d16bea93c8861fbe66b08
+<?php return array('dependencies' => array('react', 'react-dom', 'wp-element', 'wp-polyfill'), 'version' => '9f53776d106bf1f5f9e1f22493699ae4');
