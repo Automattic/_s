d4ef6775d137592618d81cc01e6dd3d31541bda8
Ismail El Korchi <ismail.elkorchi@gmail.com>
2020-04-06 22:26:57 +0100

    Add instruction to replace the _S_VERSION in the readme file

--- functions.php
+++ functions.php
@@ -8,8 +8,8 @@
  */
 
 if ( ! defined( '_S_VERSION' ) ) {
-	// Replace #.# with the version number of the theme on each release.
-	define( '_S_VERSION', '#.#' );
+	// Replace the version number of the theme on each release.
+	define( '_S_VERSION', '1.0.0' );
 }
 
 if ( ! function_exists( '_s_setup' ) ) :
