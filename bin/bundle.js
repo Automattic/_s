ff15c77bd871fc999fc93c63901a843cb358e733
Ismail El Korchi <ismail.elkorchi@gmail.com>
2020-04-29 22:04:59 +0000

    Output a zip file instead of a directory

--- bin/bundle.js
+++ bin/bundle.js
@@ -2,8 +2,9 @@
 
 const path = require( 'path' );
 const fs = require( 'fs' );
+const archiver = require('archiver');
 
-const distinationDirectory = 'bundle';
+// Contains the excluded files and folders.
 const excludes = [
 	'.DS_Store',
 	'.stylelintrc.json',
@@ -24,39 +25,66 @@ const excludes = [
 	'sass',
 ];
 
+// The path of the zip file.
+const zipPath = path.join(
+	__dirname,
+	'/../../',
+	path.basename(path.dirname(__dirname))
+) + '.zip';
+
+
+// Create a file to stream archive data to.
+let output = fs.createWriteStream( zipPath );
+let archive = archiver('zip', {
+  zlib: { level: 9 }
+});
+
+/**
+ * Recursively traverse the directory tree and append the files to the archive. 
+ * @param {string} directoryPath - The path of the directory being looped through.
+ */
 function traverseDirectoryTree( directoryPath ) {
 	const files = fs.readdirSync( directoryPath );
 	for ( const i in files ) {
 		const currentPath = directoryPath + '/' + files[i];
 		const stats = fs.statSync( currentPath );
+		let relativePath = path.relative(process.cwd(), currentPath);
 		if ( stats.isFile() && ! excludes.includes( files[i] ) ) {
-			copyFile( currentPath );
+			archive.file(currentPath, {
+				name: `${relativePath}`
+			});
 		} else if ( stats.isDirectory() && ! excludes.includes( files[i] ) ) {
 			traverseDirectoryTree( currentPath );
 		}
 	}
 }
 
-function copyFile( file ) {
-	const distinationPath = path.join(
-		__dirname,
-		'/../../',
-		distinationDirectory,
-		file
-	);
+// Listen for all archive data to be written.
+output.on('close', function () {
+	console.log(`Created ${path.basename(path.dirname(__dirname))}.zip of ${archive.pointer()} bytes`);
+});
 
-	if ( ! fs.existsSync( path.dirname( distinationPath ) ) ) {
-		fs.mkdirSync( path.dirname( distinationPath ) );
+// Catch warnings during archiving.
+archive.on('warning', function(err) {
+	if (err.code === 'ENOENT') {
+		// log warning
+		console.log(err);
+	} else {
+		// throw error
+		throw err;
 	}
+});
 
-	fs.readFile( file, 'utf8', function( err, data ) {
-		fs.writeFile( distinationPath, data, 'utf8', function( err ) {
-			console.log( 'Copying' + file );
-			if ( err ) {
-				return console.log( err );
-			}
-		} );
-	} );
-}
+// Catch errors during archiving.
+archive.on('error', function(err){
+	throw err;
+});
 
+// Pipe archive data to the file.
+archive.pipe(output);
+
+// Append the files to the archive.
 traverseDirectoryTree( '.' );
+
+// Finalize the archive.
+archive.finalize();
