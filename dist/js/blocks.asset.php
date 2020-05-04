267fb593e46870c82533be541ca5192ffa5f3fce
Costanza <c.focardi@silverbackstudio.it>
2020-05-04 13:50:43 +0000

    Blog: fix post-banner style

--- dist/js/blocks.asset.php
+++ dist/js/blocks.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react', 'react-dom', 'wp-element', 'wp-polyfill'), 'version' => '0907ebcb3d136d6129bf6d740eea38bf');
+<?php return array('dependencies' => array('react', 'react-dom', 'wp-element', 'wp-polyfill'), 'version' => '894275e458c44a2365fab25ef3436633');
