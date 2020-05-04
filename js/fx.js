ed656f632900dfa0e242700e6a4815fbc32ecd27
Costanza <c.focardi@silverbackstudio.it>
2020-05-04 13:52:51 +0000

    JS: add smooth scroll offset

--- js/fx.js
+++ js/fx.js
@@ -2,18 +2,20 @@
 
   /* Scroll smoothly betweeen anchors */
   var smoothScroll = function(){
-      $(document.body).on('click', 'a[href*="#"]:not([href="#"])', function() {
-            if (location.pathname.replace(/^\//,'') == this.pathname.replace(/^\//,'') && location.hostname == this.hostname) {
-              var target = $(this.hash);
-              target = target.length ? target : $('[name=' + this.hash.slice(1) +']');
-              if (target.length) {
-                  $('html, body').animate({
-                      scrollTop: target.offset().top
-                  }, 500);
-                  return false;
-              }
-            }
-      });
+    $(document.body).on('click', 'a[href*="#"]:not([href="#"])', function() {
+        if (location.pathname.replace(/^\//,'') == this.pathname.replace(/^\//,'') && location.hostname == this.hostname) {
+          var target = $(this.hash);
+          target = target.length ? target : $('[name=' + this.hash.slice(1) +']');
+          if (target.length) {
+            var headerHeight = $("#site-header-content").height();
+            var scrollOffset = $(target).offset().top - headerHeight - 100;
+            $('html, body').animate({
+                scrollTop: scrollOffset
+            }, 500);
+            return false;
+          }
+        }
+    });
   };
 
   $(document).ready( smoothScroll );
