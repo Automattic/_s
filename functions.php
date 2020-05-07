3351fcd44850c7f61b9f195d604d6fb94265aa5d
Ismail El Korchi <ismail.elkorchi@gmail.com>
2020-05-07 04:59:19 +0000

    Remove the skip link focus fix

--- functions.php
+++ functions.php
@@ -148,8 +148,6 @@ function _s_scripts() {
 
 	wp_enqueue_script( '_s-navigation', get_template_directory_uri() . '/js/navigation.js', array(), _S_VERSION, true );
 
-	wp_enqueue_script( '_s-skip-link-focus-fix', get_template_directory_uri() . '/js/skip-link-focus-fix.js', array(), _S_VERSION, true );
-
 	if ( is_singular() && comments_open() && get_option( 'thread_comments' ) ) {
 		wp_enqueue_script( 'comment-reply' );
 	}
