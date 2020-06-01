aa8a16553826f2cd5122a9a35ca11dca034a3b92
李二庆（Amos Lee） <iwillhappy1314@gmail.com>
2020-06-01 16:41:22 +0800

    Footer 布局优化

--- footer.php
+++ footer.php
@@ -16,13 +16,17 @@
 <footer id="colophon" class="site__footer">
 
     <?php if ( is_active_sidebar('sidebar-footer')): ?>
-        <section class="container">
-            <?php dynamic_sidebar('sidebar-footer'); ?>
+        <section class="container pt-8">
+            <div class="flex -mx-4">
+                <?php dynamic_sidebar('sidebar-footer'); ?>
+            </div>
         </section>
     <?php endif; ?>
 
-    <div class="site__info">
-        Copyright &copy; <?= date('Y'); ?> <a href="<?= home_url(); ?>"><?php bloginfo('name'); ?></a>
+    <div class="border-t border-white py-4 site__info">
+        <div class="container">
+            Copyright &copy; <?= date('Y'); ?> <a href="<?= home_url(); ?>"><?php bloginfo('name'); ?></a>
+        </div>
     </div>
 </footer>
 </div><!-- #page -->
