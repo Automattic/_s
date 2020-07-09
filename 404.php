6e73bc37ad2a2d4aeac483f72bc513ad290a7682
李二庆（Amos Lee） <iwillhappy1314@gmail.com>
2020-07-09 18:44:21 +0800

    整理 WooCommerce 按钮

--- 404.php
+++ 404.php
@@ -2,7 +2,7 @@
 /**
  * The template for displaying 404 pages (not found)
  *
- * @link https://codex.wordpress.org/Creating_an_Error_404_Page
+ * @link    https://codex.wordpress.org/Creating_an_Error_404_Page
  *
  * @package _s
  */
@@ -10,39 +10,41 @@
 get_header();
 ?>
 
-	<div id="primary" class="content__primary">
-		<main id="main" class="site__main">
+    <div id="content" class="site__content">
+        <div id="primary" class="content__primary">
+            <main id="main" class="site__main">
 
-			<section class="error-404 not-found">
-				<header class="page-header">
-					<h1 class="page-title"><?php esc_html_e( 'Oops! That page can&rsquo;t be found.', '_s' ); ?></h1>
-				</header><!-- .page-header -->
+                <section class="error-404 not-found">
+                    <header class="page-header">
+                        <h1 class="page-title"><?php esc_html_e('Oops! That page can&rsquo;t be found.', '_s'); ?></h1>
+                    </header><!-- .page-header -->
 
-				<div class="page-content">
-					<p><?php esc_html_e( 'It looks like nothing was found at this location. Maybe try one of the links below or a search?', '_s' ); ?></p>
+                    <div class="page-content">
+                        <p><?php esc_html_e('It looks like nothing was found at this location. Maybe try one of the links below or a search?', '_s'); ?></p>
 
-					<?php get_search_form(); ?>
+                        <?php get_search_form(); ?>
 
-					<div class="widget widget_categories">
-						<h2 class="widget-title"><?php esc_html_e( 'Most Used Categories', '_s' ); ?></h2>
-						<ul>
-							<?php
-							wp_list_categories( array(
-								'orderby'    => 'count',
-								'order'      => 'DESC',
-								'show_count' => 1,
-								'title_li'   => '',
-								'number'     => 10,
-							) );
-							?>
-						</ul>
-					</div>
+                        <div class="widget widget_categories">
+                            <h2 class="widget-title"><?php esc_html_e('Most Used Categories', '_s'); ?></h2>
+                            <ul>
+                                <?php
+                                wp_list_categories([
+                                    'orderby'    => 'count',
+                                    'order'      => 'DESC',
+                                    'show_count' => 1,
+                                    'title_li'   => '',
+                                    'number'     => 10,
+                                ]);
+                                ?>
+                            </ul>
+                        </div>
 
-				</div>
-			</section>
+                    </div>
+                </section>
 
-		</main><!-- #main -->
-	</div><!-- #primary -->
+            </main><!-- #main -->
+        </div><!-- #primary -->
+    </div><!-- #content -->
 
 <?php
 get_footer();
