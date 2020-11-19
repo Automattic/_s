c09568e9c3a5df7a8c565c397276d27143187b7c
Krishna Kant <lushkant@gmail.com>
2020-11-19 12:07:43 +0530

    Fix deprecated `get_the_author()` function call

--- inc/template-tags.php
+++ inc/template-tags.php
@@ -44,7 +44,7 @@ if ( ! function_exists( '_s_posted_by' ) ) :
 		$byline = sprintf(
 			/* translators: %s: post author. */
 			esc_html_x( 'by %s', 'post author', '_s' ),
-			'<span class="author vcard"><a class="url fn n" href="' . esc_url( get_author_posts_url( get_the_author_meta( 'ID' ) ) ) . '">' . esc_html( get_the_author() ) . '</a></span>'
+			'<span class="author vcard"><a class="url fn n" href="' . esc_url( get_author_posts_url( get_the_author_meta( 'ID' ) ) ) . '">' . esc_html( get_the_author_meta( 'display_name' ) ) . '</a></span>'
 		);
 
 		echo '<span class="byline"> ' . $byline . '</span>'; // phpcs:ignore WordPress.Security.EscapeOutput.OutputNotEscaped
