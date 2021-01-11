517a93abfb455c6f5f0509db441861ad8e438f17
Marcos Rego <web@marcosrego.com>
2021-01-11 00:41:48 +0000

    Add possibility to add content to Error 404 page

--- 404.php
+++ 404.php
@@ -10,46 +10,20 @@
 get_header();
 ?>
 
-	<main id="primary" class="site-main">
+	<main id="primary" class="mr-main site-main">
 
-		<section class="error-404 not-found">
-			<header class="page-header">
+		<section class="mr-section error-404 not-found">
+			<header class="mr-header page-header">
 				<h1 class="page-title"><?php esc_html_e( 'Oops! That page can&rsquo;t be found.', '_s' ); ?></h1>
 			</header><!-- .page-header -->
 
 			<div class="page-content">
-				<p><?php esc_html_e( 'It looks like nothing was found at this location. Maybe try one of the links below or a search?', '_s' ); ?></p>
-
-					<?php
-					get_search_form();
-
-					the_widget( 'WP_Widget_Recent_Posts' );
-					?>
-
-					<div class="widget widget_categories">
-						<h2 class="widget-title"><?php esc_html_e( 'Most Used Categories', '_s' ); ?></h2>
-						<ul>
-							<?php
-							wp_list_categories(
-								array(
-									'orderby'    => 'count',
-									'order'      => 'DESC',
-									'show_count' => 1,
-									'title_li'   => '',
-									'number'     => 10,
-								)
-							);
-							?>
-						</ul>
-					</div><!-- .widget -->
-
-					<?php
-					/* translators: %1$s: smiley */
-					$_s_archive_content = '<p>' . sprintf( esc_html__( 'Try looking in the monthly archives. %1$s', '_s' ), convert_smilies( ':)' ) ) . '</p>';
-					the_widget( 'WP_Widget_Archives', 'dropdown=1', "after_title=</h2>$_s_archive_content" );
-
-					the_widget( 'WP_Widget_Tag_Cloud' );
-					?>
+				
+				<?php
+				if ( is_active_sidebar( 'error' ) ) {
+					dynamic_sidebar( 'error' );
+				}
+				?>
 
 			</div><!-- .page-content -->
 		</section><!-- .error-404 -->
