7c036b9f82fc6a4afdae966f7862c2b0ccf1df3d
iamdavidabayomi <iamdavidabayomi@gmail.com>
2021-04-05 13:11:18 +0100

    fix post date bug

--- inc/template-tags.php
+++ inc/template-tags.php
@@ -14,15 +14,17 @@ if ( ! function_exists( '_s_posted_on' ) ) :
 	function _s_posted_on() {
 		$time_string = '<time class="entry-date published updated" datetime="%1$s">%2$s</time>';
 		if ( get_the_time( 'U' ) !== get_the_modified_time( 'U' ) ) {
-			$time_string = '<time class="entry-date published" datetime="%1$s">%2$s</time><time class="updated" datetime="%3$s">%4$s</time>';
+			$time_string = sprintf(
+				$time_string,
+				esc_attr( get_the_modified_date( DATE_W3C ) ),
+				esc_html( get_the_modified_date() )
+			);
 		}
 
 		$time_string = sprintf(
 			$time_string,
 			esc_attr( get_the_date( DATE_W3C ) ),
-			esc_html( get_the_date() ),
-			esc_attr( get_the_modified_date( DATE_W3C ) ),
-			esc_html( get_the_modified_date() )
+			esc_html( get_the_date() )
 		);
 
 		$posted_on = sprintf(
