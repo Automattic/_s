8c74939002e63e27c3f517d9aacdac6d41ddfae1
Marcos Rego <web@marcosrego.com>
2021-04-16 20:42:03 +0100

    Mr.Dev.'s Framework - Default styles

--- README.md
+++ README.md
@@ -4,7 +4,6 @@ The easiest way to start creating with Mr.Dev.'s Framework!
 
 Mr.Dev. grabbed Underscores, the famous starter theme for Wordpress, and made it fully compatible with his Framework.
 
-**Some key differences:**
-- No default widgets/sidebars/sections - This is to better follow the new workflow on Wordpress *(using blocks and reusable blocks per page instead of widgets)* and to prepare for the full site editor. You can still create new sidebars/sections using "layout control" on Mr.Dev.'s Framework or set your defaults on a child theme by uncommenting *add_action( 'widgets_init', '_s_widgets_init' )* on *functions.php*.
-- No forced logo or menu in the header - You can use widgets instead. Create new sidebars/sections for those widgets using "layout control" on Mr.Dev.'s Framework.
-- Auto dropdown menu - With Mr.Dev_s any default menu widget located inside ".mr-header" will automatically become a structured dropdown menu.
+**Some key differences vs Underscores:**
+- No default widgets/sidebars and no forced logo or menu in the header - This is to better follow the new workflow on Wordpress *(using blocks and reusable blocks per page instead of widgets)* and to prepare for the full site editor. You can still create new sidebars/sections using "layout control" on Mr.Dev.'s Framework or set your defaults on a child theme from *Mr.Dev_s*, by going to *functions.php* and uncommenting *add_action( 'widgets_init', '_s_widgets_init' )*.
+- Auto dropdown menu - With *Mr.Dev_s* any default menu widget located inside ".mr-header" will automatically become a structured dropdown menu.
---
+++ mrdev-framework/styles.php
@@ -0,0 +1,164 @@
+
+<?php
+defined('ABSPATH') or die;
+
+$mrdev_styles_colors_number_default = 20;
+
+${"mrdev_style_color_name1"} = 'Background Color';
+${"mrdev_style_color_value1"} = '#FFFFFF';
+${"mrdev_style_color_opacity1"} = 1;
+
+${"mrdev_style_color_name2"} = 'Text Color';
+${"mrdev_style_color_value2"} = '#434b5c';
+${"mrdev_style_color_opacity2"} = 1;
+
+${"mrdev_style_color_name3"} = 'Header Background Color';
+${"mrdev_style_color_value3"} = '#757d98';
+${"mrdev_style_color_opacity3"} = 1;
+
+${"mrdev_style_color_name4"} = 'Header Text Color';
+${"mrdev_style_color_value4"} = '#8cc7c2';
+${"mrdev_style_color_opacity4"} = 1;
+
+${"mrdev_style_color_name5"} = 'Main Background Color';
+${"mrdev_style_color_value5"} = '#dfdfd0';
+${"mrdev_style_color_opacity5"} = 1;
+
+${"mrdev_style_color_name6"} = 'Main Text Color';
+${"mrdev_style_color_value6"} = '#434b5c';
+${"mrdev_style_color_opacity6"} = 1;
+
+${"mrdev_style_color_name7"} = 'Aside Background Color';
+${"mrdev_style_color_value7"} = '#8cc7c2';
+${"mrdev_style_color_opacity7"} = 1;
+
+${"mrdev_style_color_name8"} = 'Aside Text Color';
+${"mrdev_style_color_value8"} = '#434b5c';
+${"mrdev_style_color_opacity8"} = 1;
+
+${"mrdev_style_color_name9"} = 'Footer Background Color';
+${"mrdev_style_color_value9"} = '#757d98';
+${"mrdev_style_color_opacity9"} = 1;
+
+${"mrdev_style_color_name10"} = 'Footer Text Color';
+${"mrdev_style_color_value10"} = '#FFFFFF';
+${"mrdev_style_color_opacity10"} = 0.5;
+
+${"mrdev_style_color_name11"} = 'Offcanvas Background Color';
+${"mrdev_style_color_value11"} = '#23282d';
+${"mrdev_style_color_opacity11"} = 1;
+
+${"mrdev_style_color_name12"} = 'Offcanvas Text Color';
+${"mrdev_style_color_value12"} = '#FFFFFF';
+${"mrdev_style_color_opacity12"} = 1;
+
+${"mrdev_style_color_name13"} = 'Offcanvas Toggle Color';
+${"mrdev_style_color_value13"} = '#888888';
+${"mrdev_style_color_opacity13"} = 1;
+
+${"mrdev_style_color_name14"} = 'Overlay Color';
+${"mrdev_style_color_value14"} = '#000000';
+${"mrdev_style_color_opacity14"} = 0.5;
+
+${"mrdev_style_color_name15"} = 'Primary Color';
+${"mrdev_style_color_value15"} = '#0073aa';
+${"mrdev_style_color_opacity15"} = 1;
+
+${"mrdev_style_color_name16"} = 'Secondary Color';
+${"mrdev_style_color_value16"} = '#23282d';
+${"mrdev_style_color_opacity16"} = 1;
+
+${"mrdev_style_color_name17"} = 'Info Color';
+${"mrdev_style_color_value17"} = '#00a0d2';
+${"mrdev_style_color_opacity17"} = 1;
+
+${"mrdev_style_color_name18"} = 'Success Color';
+${"mrdev_style_color_value18"} = '#46b450';
+${"mrdev_style_color_opacity18"} = 1;
+
+${"mrdev_style_color_name19"} = 'Warning Color';
+${"mrdev_style_color_value19"} = '#ffb900';
+${"mrdev_style_color_opacity19"} = 1;
+
+${"mrdev_style_color_name20"} = 'Error Color';
+${"mrdev_style_color_value20"} = '#dc3232';
+${"mrdev_style_color_opacity20"} = 1;
+
+$mrdev_styles_fonts_number_default = 2;
+
+${"mrdev_style_font_name1"} = 'Heading Font';
+${"mrdev_style_font_value1"} = 'TimesNewRoman,Times New Roman,Times,Baskerville,Georgia,serif';
+${"mrdev_style_font_url1"} = '';
+
+${"mrdev_style_font_name2"} = 'Text Font';
+${"mrdev_style_font_value2"} = 'Arial,Helvetica Neue,Helvetica';
+${"mrdev_style_font_url2"} = '';
+
+$mrdev_styles_sizes_number_default = 16;
+
+${"mrdev_style_size_name1"} = 'Base';
+${"mrdev_style_size_value1"} = 100;
+${"mrdev_style_size_unit1"} = '%';
+
+${"mrdev_style_size_name2"} = 'Text';
+${"mrdev_style_size_value2"} = 1;
+${"mrdev_style_size_unit2"} = 'rem';
+
+${"mrdev_style_size_name3"} = 'Heading 1';
+${"mrdev_style_size_value3"} = 2;
+${"mrdev_style_size_unit3"} = 'rem';
+
+${"mrdev_style_size_name4"} = 'Heading 2';
+${"mrdev_style_size_value4"} = 1.5;
+${"mrdev_style_size_unit4"} = 'rem';
+
+${"mrdev_style_size_name5"} = 'Heading 3';
+${"mrdev_style_size_value5"} = 1.17;
+${"mrdev_style_size_unit5"} = 'rem';
+
+${"mrdev_style_size_name6"} = 'Heading 4';
+${"mrdev_style_size_value6"} = 1;
+${"mrdev_style_size_unit6"} = 'rem';
+
+${"mrdev_style_size_name7"} = 'Heading 5';
+${"mrdev_style_size_value7"} = 0.83;
+${"mrdev_style_size_unit7"} = 'rem';
+
+${"mrdev_style_size_name8"} = 'Heading 6';
+${"mrdev_style_size_value8"} = 0.67;
+${"mrdev_style_size_unit8"} = 'rem';
+
+${"mrdev_style_size_name9"} = 'Line-height';
+${"mrdev_style_size_value9"} = 1.2;
+${"mrdev_style_size_unit9"} = '';
+
+${"mrdev_style_size_name10"} = 'Letter Spacing';
+${"mrdev_style_size_value10"} = 0.05;
+${"mrdev_style_size_unit10"} = 'rem';
+
+${"mrdev_style_size_name11"} = 'Container';
+${"mrdev_style_size_value11"} = 75;
+${"mrdev_style_size_unit11"} = 'rem';
+
+${"mrdev_style_size_name12"} = 'Padding';
+${"mrdev_style_size_value12"} = 0.75;
+${"mrdev_style_size_unit12"} = 'rem';
+
+${"mrdev_style_size_name13"} = 'Margin';
+${"mrdev_style_size_value13"} = 0.75;
+${"mrdev_style_size_unit13"} = 'rem';
+
+${"mrdev_style_size_name14"} = 'Transition Duration';
+${"mrdev_style_size_value14"} = 0.5;
+${"mrdev_style_size_unit14"} = 's';
+
+${"mrdev_style_size_name15"} = 'Border';
+${"mrdev_style_size_value15"} = 2;
+${"mrdev_style_size_unit15"} = 'rem';
+
+${"mrdev_style_size_name16"} = 'Border Radius';
+${"mrdev_style_size_value16"} = 0;
+${"mrdev_style_size_unit16"} = 'rem';
+
+$mrdev_styles_breakpoints_number_default = 4;
+
