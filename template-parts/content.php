94234e5038b27bd8240035ad602bb6bbda08b8c5
Marcos Rego <43761910+marcosrego-web@users.noreply.github.com>
2021-05-17 16:41:51 +0100

    Add option for excerpt
    Underscores only supports the use of "read more" inside of the content. But I think enough devs would like to have the option of the "classic" excerpt from the start. Also for some clients might be easier to understand/learn the use of an excerpt that is outside of the content.

--- template-parts/content.php
+++ template-parts/content.php
@@ -33,20 +33,24 @@
 
 	<div class="entry-content">
 		<?php
-		the_content(
-			sprintf(
-				wp_kses(
-					/* translators: %s: Name of current post. Only visible to screen readers */
-					__( 'Continue reading<span class="screen-reader-text"> "%s"</span>', '_s' ),
-					array(
-						'span' => array(
-							'class' => array(),
-						),
-					)
-				),
-				wp_kses_post( get_the_title() )
-			)
-		);
+		if ( has_excerpt() ) :
+			the_excerpt();
+		else :
+			the_content(
+				sprintf(
+					wp_kses(
+						/* translators: %s: Name of current post. Only visible to screen readers */
+						__( 'Continue reading<span class="screen-reader-text"> "%s"</span>', '_s' ),
+						array(
+							'span' => array(
+								'class' => array(),
+							),
+						)
+					),
+					wp_kses_post( get_the_title() )
+				)
+			);
+		endif;
 
 		wp_link_pages(
 			array(
