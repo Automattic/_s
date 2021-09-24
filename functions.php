b142319cd440a240b15e6ab183581b0b7756c198
Md Rashed Hossain <wprashed@icloud.com>
2021-09-24 13:04:11 +0600

    Update functions.php
    We can add post reading time feature

--- functions.php
+++ functions.php
@@ -184,3 +184,30 @@ if ( defined( 'JETPACK__VERSION' ) ) {
 if ( class_exists( 'WooCommerce' ) ) {
 	require get_template_directory() . '/inc/woocommerce.php';
 }
+
+/**
+ * Add Post Reading time.
+ */
+function post_read_time() {
+ 
+// get the post content
+$content = get_post_field( 'post_content', $post-&gt;ID );
+ 
+// count the words
+$word_count = str_word_count( strip_tags( $content ) );
+ 
+// reading time itself
+$readingtime = ceil($word_count / 200);
+ 
+if ($readingtime == 1) {
+ $timer = " minute read";
+} else {
+ $timer = " minute read"; // or your version :) I use the same wordings for 1 minute of reading or more
+}
+ 
+// I'm going to print 'X minute read' above my post
+$totalreadingtime = $readingtime . $timer;
+echo $totalreadingtime;
+return $totalreadingtime;
+ 
+}
